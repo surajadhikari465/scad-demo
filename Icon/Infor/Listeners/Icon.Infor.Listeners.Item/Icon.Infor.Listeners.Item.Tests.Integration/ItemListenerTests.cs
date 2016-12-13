faf6618c0523a36f91afb7b4ec688b9f7232df39
Ed McNab <emcnab@gmail.com>
2016-12-13 21:58:39 +0000

    Merged PR 89: Merge 19338_IrmaAndInforItemListener_OrganicFlagDatabaseSproc to master
     - Organic flag in Irma to match Infor
     - Minor revisions - helper variables and removing commented-out line
     - adding integration test for item listener
     - Added integration test  for Infor Item Listener - confirms organic trait is
    
    Related work items: #19338

--- Icon/Infor/Listeners/Icon.Infor.Listeners.Item/Icon.Infor.Listeners.Item.Tests.Integration/ItemListenerTests.cs
+++ Icon/Infor/Listeners/Icon.Infor.Listeners.Item/Icon.Infor.Listeners.Item.Tests.Integration/ItemListenerTests.cs
@@ -15,12 +15,34 @@ using System.IO;
 using System.Linq;
 using Icon.Infor.Listeners.Item.Validators;
 using Icon.Infor.Listeners.Item.Notifiers;
+using System.Xml.Linq;
 
 namespace Icon.Infor.Listeners.Item.Tests.Integration
 {
     [TestClass]
     public class ItemListenerTests
     {
+        protected ItemListener CreateItemListenerForTest(Mock<IRenewableContext<IconContext>> mockGlobalContext)
+        {
+            ItemListener il = new ItemListener(
+                              new ItemMessageParser(new NLogLogger<ItemMessageParser>()),
+                              new ItemModelValidator(new ValidateItemsCommandHandler(mockGlobalContext.Object)),
+                              mockGlobalContext.Object,
+                              new ItemService(
+                                      new ItemAddOrUpdateCommandHandler(mockGlobalContext.Object),
+                                      new GenerateItemMessagesCommandHandler(mockGlobalContext.Object),
+                                      new ArchiveItemsCommandHandler(),
+                                      new ArchiveMessageCommandHandler(mockGlobalContext.Object)
+                                  ),
+                                  ListenerApplicationSettings.CreateDefaultSettings("Infor Item Listener"),
+                                  EsbConnectionSettings.CreateSettingsFromConfig(),
+                                  new EsbSubscriber(EsbConnectionSettings.CreateSettingsFromConfig()),
+                                  EmailClient.CreateFromConfig(),
+                                  new ItemListenerNotifier(EmailClient.CreateFromConfig()),
+                                  new NLogLogger<ItemListener>());
+            return il;
+        }
+
         [TestMethod]
         public void HandleMessage_GivenAProductMessageFromInforWithASingleItem_ShouldSaveItemToDatabase()
         {
@@ -31,22 +53,8 @@ namespace Icon.Infor.Listeners.Item.Tests.Integration
 
             using (var transaction = context.Database.BeginTransaction())
             {
-                ItemListener il = new ItemListener(
-                            new ItemMessageParser(new NLogLogger<ItemMessageParser>()),
-                            new ItemModelValidator(new ValidateItemsCommandHandler(mockGlobalContext.Object)),
-                            mockGlobalContext.Object,
-                            new ItemService(
-                                    new ItemAddOrUpdateCommandHandler(mockGlobalContext.Object),
-                                    new GenerateItemMessagesCommandHandler(mockGlobalContext.Object),
-                                    new ArchiveItemsCommandHandler(),
-                                    new ArchiveMessageCommandHandler(mockGlobalContext.Object)
-                                ),
-                                ListenerApplicationSettings.CreateDefaultSettings("Infor Item Listener"),
-                                EsbConnectionSettings.CreateSettingsFromConfig(),
-                                new EsbSubscriber(EsbConnectionSettings.CreateSettingsFromConfig()),
-                                EmailClient.CreateFromConfig(),
-                                new ItemListenerNotifier(EmailClient.CreateFromConfig()),
-                                new NLogLogger<ItemListener>());
+                ItemListener il = CreateItemListenerForTest(mockGlobalContext);
+
                 Mock<IEsbMessage> mockMessage = new Mock<IEsbMessage>();
                 mockMessage.SetupGet(m => m.MessageText)
                     .Returns(File.ReadAllText(@"TestMessages/ProductMessageFromInfor.xml"));
@@ -66,6 +74,46 @@ namespace Icon.Infor.Listeners.Item.Tests.Integration
                 var messageArchiveProduct = context.MessageArchiveProduct.FirstOrDefault(p => p.ItemId == 4000110);
 
                 Assert.IsNotNull(messageArchiveProduct, "Item update was not successfully archived to the database.");
+            }
+        }
+
+
+        [TestMethod]
+        public void HandleMessage_ProductMessageFromInforWithItemThatHasOrganicTrait_SavseItemSignAttributeAsOrganic()
+        {
+            //Given
+            IconContext context = new IconContext();
+            Mock<IRenewableContext<IconContext>> mockGlobalContext = new Mock<IRenewableContext<IconContext>>();
+            mockGlobalContext.SetupGet(m => m.Context).Returns(context);
+
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                ItemListener il = CreateItemListenerForTest(mockGlobalContext);
+
+                Mock<IEsbMessage> mockMessage = new Mock<IEsbMessage>();
+
+                //load the test message as an xml doc
+                var xmlTestData = XDocument.Load(@"TestMessages/ProductMessageFromInfor.xml");
+                // set the "OG" element's <trt:value> element to have our desired value
+                const string organicAgencyName = "xyz123";
+                xmlTestData.Root.Descendants().Where(trait => trait.Value.ToString() == "OG").First()
+                    .ElementsAfterSelf().First().Descendants().Last().Value = organicAgencyName;
+
+                mockMessage.SetupGet(m => m.MessageText)
+                    .Returns(xmlTestData.ToString());
+                mockMessage.Setup(m => m.GetProperty("IconMessageID"))
+                    .Returns("ec339683-14e6-4142-8de9-7b5c00960d62");
+
+                //When
+                il.HandleMessage(null, new EsbMessageEventArgs { Message = mockMessage.Object });
+
+                //Then
+                var itemSignAttribute = context.ScanCode.AsNoTracking()
+                    .FirstOrDefault(sc => sc.scanCode == "1000397" && sc.itemID == 4000110)
+                    .Item.ItemSignAttribute.FirstOrDefault();
+
+                Assert.IsNotNull(itemSignAttribute, "Item was not successfully saved to the database.");
+                Assert.AreEqual(organicAgencyName, itemSignAttribute.OrganicAgencyName);
             }
         }
     }
