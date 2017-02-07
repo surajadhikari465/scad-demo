abc3f71b7d67418adcefffe7594c0c3dc22d8503
Blake Jones <blake.l.jones@avanade.com>
2017-02-07 19:30:00 +0000

    Merged PR 129: Merge 20411_SharedCode_DbContextFactory to master
    - Created IDbContextFactory to use to refactor services
    
    Related work items: #20411

---
+++ Icon/Shared/Icon.DbContextFactory/IDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Icon.ApiController.Common
+{
+    public interface IDbContextFactory<T>
+        where T : DbContext, new()
+    {
+        T CreateContext();
+        T CreateContext(string connectionStringName);
+        T CreateContext(object settings);
+    }
+}
