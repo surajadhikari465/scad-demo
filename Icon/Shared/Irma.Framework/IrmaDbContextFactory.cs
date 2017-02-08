4f680e6293bf44afa28342121a3a9b2ceedc0306
Blake Jones <blake.l.jones@avanade.com>
2017-02-08 16:59:02 +0000

    Merged PR 132: Added an IconDbContextFactory and IrmaDbContextFactory
    Added an IconDbContextFactory and IrmaDbContextFactory

---
+++ Icon/Shared/Irma.Framework/IrmaDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Icon.DbContextFactory;
+using System;
+
+namespace Irma.Framework
+{
+    public class IrmaDbContextFactory : IDbContextFactory<IrmaContext>
+    {
+        public IrmaContext CreateContext()
+        {
+            return new IrmaContext();
+        }
+
+        public IrmaContext CreateContext(object settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IrmaContext CreateContext(string connectionStringName)
+        {
+            return new IrmaContext(connectionStringName);
+        }
+    }
+}
