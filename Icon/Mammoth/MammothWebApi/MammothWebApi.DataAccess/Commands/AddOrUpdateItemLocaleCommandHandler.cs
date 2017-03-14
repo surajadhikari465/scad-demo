2306e584c316178e898b29903022e1fe35140f96
Ed McNab <emcnab@gmail.com>
2017-03-14 15:51:55 +0000

    Merged PR 164: Merge 20722_Mammoth_Web_API_stop_using_MERGE_for_ItemLocale to master
    Related work items: #20722

--- Icon/Mammoth/MammothWebApi/MammothWebApi.DataAccess/Commands/AddOrUpdateItemLocaleCommandHandler.cs
+++ Icon/Mammoth/MammothWebApi/MammothWebApi.DataAccess/Commands/AddOrUpdateItemLocaleCommandHandler.cs
@@ -16,102 +16,159 @@ namespace MammothWebApi.DataAccess.Commands
 
         public void Execute(AddOrUpdateItemLocaleCommand data)
         {
-            string sql = @"MERGE dbo.ItemAttributes_Locale_{0} WITH (updlock, rowlock) il
-		                    USING
-		                    (
-			                    SELECT DISTINCT
-				                    sil.Region,
-				                    i.ItemID,
-				                    l.BusinessUnitID,
-				                    sil.Discount_Case,
-				                    sil.Discount_TM,
-				                    sil.Restriction_Age,
-				                    sil.Restriction_Hours,
-				                    sil.Authorized,
-				                    sil.Discontinued,
-				                    sil.LabelTypeDesc,
-				                    sil.LocalItem,  
-				                    sil.Product_Code,
-				                    sil.RetailUnit,
-				                    sil.Sign_Desc,
-				                    sil.Locality,
-				                    sil.Sign_RomanceText_Long,
-				                    sil.Sign_RomanceText_Short,
-                                    sil.Msrp
-			                    FROM 
-				                    stage.ItemLocale	        sil
-				                    INNER JOIN dbo.Items		i	on sil.ScanCode = i.ScanCode
-				                    INNER JOIN dbo.Locales_{0}  l   on sil.BusinessUnitID = l.BusinessUnitID  -- to ensure the Locale exists in Mammoth
-			                    WHERE 
-				                    sil.Region = @Region
-				                    AND sil.TransactionId = @TransactionId
-		                    )	s
-		                    ON
-			                    il.Region = s.Region
-			                    AND il.ItemID = s.ItemID
-			                    AND il.BusinessUnitID = s.BusinessUnitID
-		                    WHEN MATCHED THEN
-			                    UPDATE
-			                    SET
-				                    il.Discount_Case			= s.Discount_Case,
-				                    il.Discount_TM				= s.Discount_TM,
-				                    il.Restriction_Age			= s.Restriction_Age,
-				                    il.Restriction_Hours		= s.Restriction_Hours,
-				                    il.Authorized				= s.Authorized,
-				                    il.Discontinued				= s.Discontinued,
-				                    il.LocalItem				= s.LocalItem,
-				                    il.LabelTypeDesc			= s.LabelTypeDesc,
-				                    il.Product_Code				= s.Product_Code,
-				                    il.RetailUnit				= s.RetailUnit,
-				                    il.Sign_Desc				= s.Sign_Desc,
-				                    il.Locality					= s.Locality,
-				                    il.Sign_RomanceText_Long	= s.Sign_RomanceText_Long,
-				                    il.Sign_RomanceText_Short	= s.Sign_RomanceText_Short,
-                                    il.MSRP                     = s.Msrp,
-				                    il.ModifiedDate				= @Timestamp
-		                    WHEN NOT MATCHED THEN
-			                    INSERT
-			                    (
-				                    ItemID,
-				                    BusinessUnitID,
-				                    Discount_Case,
-				                    Discount_TM,
-				                    Restriction_Age,
-				                    Restriction_Hours,
-				                    Authorized,
-				                    Discontinued,
-				                    LocalItem,
-				                    LabelTypeDesc,
-				                    Product_Code,
-				                    RetailUnit,
-				                    Sign_Desc,
-				                    Locality,
-				                    Sign_RomanceText_Long,
-				                    Sign_RomanceText_Short,
-                                    MSRP,
-				                    AddedDate
-			                    ) 
-			                    VALUES
-			                    (
-				                    s.ItemID,
-				                    s.BusinessUnitID,
-				                    s.Discount_Case,
-				                    s.Discount_TM,
-				                    s.Restriction_Age,
-				                    s.Restriction_Hours,
-				                    s.Authorized,
-				                    s.Discontinued,
-				                    s.LocalItem,
-				                    s.LabelTypeDesc,
-				                    s.Product_Code,
-				                    s.RetailUnit,
-				                    s.Sign_Desc,
-				                    s.Locality,
-				                    s.Sign_RomanceText_Long,
-				                    s.Sign_RomanceText_Short,
-                                    s.Msrp,
-				                    @Timestamp
-			                    );";
+            string sql = @"
+    --declare temp table with index
+    CREATE TABLE #tmpStagedItemLocale
+    (
+        [Region]                 NVARCHAR (2)     NOT NULL,
+        [ItemID]				 INT              NOT NULL,
+        [BusinessUnitID]         INT              NOT NULL,
+        [Discount_Case]          BIT              NOT NULL,
+        [Discount_TM]            BIT              NOT NULL,
+        [Restriction_Age]        INT              NULL,
+        [Restriction_Hours]      BIT              NOT NULL,
+        [Authorized]             BIT              NOT NULL,
+        [Discontinued]           BIT              NULL,
+        [LabelTypeDesc]          NVARCHAR (4)     NULL,
+        [LocalItem]              BIT              NULL,
+        [Product_Code]           NVARCHAR (15)    NULL,
+        [RetailUnit]             NVARCHAR (25)    NULL,
+        [Sign_Desc]              NVARCHAR (60)    NULL,
+        [Locality]               NVARCHAR (50)    NULL,
+        [Sign_RomanceText_Long]  NVARCHAR (300)   NULL,
+        [Sign_RomanceText_Short] NVARCHAR (255)   NULL,
+        [Msrp]                   SMALLMONEY       NOT NULL,
+        [InsertOrUpdate]		 CHAR			  NOT NULL,
+        PRIMARY KEY ([Region], [ItemID], [BusinessUnitID])
+    )
+
+    --copy staging data into temp table
+    INSERT #tmpStagedItemLocale
+    SELECT DISTINCT
+	    stage.[Region],
+	    item.[ItemID],
+	    stage.[BusinessUnitID],
+	    stage.[Discount_Case],
+	    stage.[Discount_TM],
+	    stage.[Restriction_Age],
+	    stage.[Restriction_Hours],
+	    stage.[Authorized],
+	    stage.[Discontinued],
+	    stage.[LabelTypeDesc],
+	    stage.[LocalItem],  
+	    stage.[Product_Code],
+	    stage.[RetailUnit],
+	    stage.[Sign_Desc],
+	    stage.[Locality],
+	    stage.[Sign_RomanceText_Long],
+	    stage.[Sign_RomanceText_Short],
+        stage.[Msrp],
+	    CASE WHEN att.[ItemAttributeLocaleID] is null THEN 'I' ELSE 'U' END As [InsertOrUpdate]
+    FROM 
+	    [stage].[ItemLocale]	        AS stage
+	    INNER JOIN [dbo].[Items]		AS item ON
+            item.[ScanCode] = [stage].[ScanCode]
+	    INNER JOIN [dbo].[Locales_{0}]   AS loc ON
+            loc.[BusinessUnitID] = [stage].[BusinessUnitID]   -- (to ensure the Locale exists in Mammoth)
+	    LEFT OUTER JOIN [dbo].[ItemAttributes_Locale_{0}] AS att ON
+            att.[ItemID]        = item.[ItemID] AND
+            att.[Region]        = loc.[Region] AND
+            att.[BusinessUnitID] = stage.[BusinessUnitID]
+    WHERE 
+	    stage.[Region] =        @Region AND
+        stage.[TransactionId] = @TransactionId AND
+        stage.[Timestamp] =     @Timestamp
+			
+    DECLARE @staged_row_count int = @@ROWCOUNT;
+
+    BEGIN TRY
+        BEGIN TRAN
+
+        --perform updates
+        UPDATE [dbo].[ItemAttributes_Locale_{0}]
+	        SET
+			    [Discount_Case]			    = tmp.[Discount_Case],
+			    [Discount_TM]				= tmp.[Discount_TM],
+			    [Restriction_Age]			= tmp.[Restriction_Age],
+			    [Restriction_Hours]		    = tmp.[Restriction_Hours],
+			    [Authorized]				= tmp.[Authorized],
+			    [Discontinued]			    = tmp.[Discontinued],
+			    [LocalItem]				    = tmp.[LocalItem],
+			    [LabelTypeDesc]			    = tmp.[LabelTypeDesc],
+			    [Product_Code]			    = tmp.[Product_Code],
+			    [RetailUnit]				= tmp.[RetailUnit],
+			    [Sign_Desc]				    = tmp.[Sign_Desc],
+			    [Locality]				    = tmp.[Locality],
+			    [Sign_RomanceText_Long]	    = tmp.[Sign_RomanceText_Long],
+			    [Sign_RomanceText_Short]    = tmp.[Sign_RomanceText_Short],
+                [MSRP]                      = tmp.[Msrp],
+			    [ModifiedDate]              = @Timestamp
+	        FROM
+		       [dbo].[ItemAttributes_Locale_{0}] att
+			        JOIN #tmpStagedItemLocale tmp ON
+                        tmp.[Region] = att.[Region] AND
+                        tmp.[ItemID] = att.[ItemID] AND
+                        tmp.[BusinessUnitID] = att.[BusinessUnitID]
+	        WHERE tmp.[InsertOrUpdate]= 'U'
+				
+	    IF @@ROWCOUNT < @staged_row_count
+	    BEGIN
+		    --perform inserts
+            INSERT INTO [dbo].[ItemAttributes_Locale_{0}]
+            (
+				[Region],
+				[ItemID],
+				[BusinessUnitID],
+				[Discount_Case],
+				[Discount_TM],
+				[Restriction_Age],
+				[Restriction_Hours],
+				[Authorized],
+				[Discontinued],
+				[LocalItem],
+				[LabelTypeDesc],
+				[Product_Code],
+				[RetailUnit],
+				[Sign_Desc],
+				[Locality],
+				[Sign_RomanceText_Long],
+				[Sign_RomanceText_Short],
+				[MSRP],
+				[AddedDate]
+            )
+			SELECT 
+				tmp.[Region],
+				tmp.[ItemID],
+				tmp.[BusinessUnitID],
+				tmp.[Discount_Case],
+				tmp.[Discount_TM],
+				tmp.[Restriction_Age],
+				tmp.[Restriction_Hours],
+				tmp.[Authorized],
+				tmp.[Discontinued],
+				tmp.[LocalItem],
+				tmp.[LabelTypeDesc],
+				tmp.[Product_Code],
+				tmp.[RetailUnit],
+				tmp.[Sign_Desc],
+				tmp.[Locality],
+				tmp.[Sign_RomanceText_Long],
+				tmp.[Sign_RomanceText_Short],
+				tmp.[MSRP],
+				@Timestamp
+			FROM #tmpStagedItemLocale tmp
+			WHERE tmp.[InsertOrUpdate] = 'I'
+	    END
+
+        COMMIT TRAN
+    END TRY
+    BEGIN CATCH
+	    ROLLBACK TRAN;
+        IF OBJECT_ID('tempdb..#tmpStagedItemLocale') IS NOT NULL DROP TABLE #tmpStagedItemLocale;
+        THROW
+    END CATCH
+            ";
+	
 
             int affectedRows = this.db.Connection.Execute(String.Format(sql, data.Region),
                 new { Region = new DbString { Value = data.Region, Length = 2 }, Timestamp = data.Timestamp, TransactionId = data.TransactionId },
