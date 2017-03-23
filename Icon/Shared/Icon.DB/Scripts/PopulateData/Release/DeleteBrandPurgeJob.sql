42d30ab681d89dea04f1712f66f4c4f41915911a
Blake Jones <blake.l.jones@avanade.com>
2017-03-23 09:11:21 +0000

    Merged PR 175: Added a script to delete the Brand Purge job in Icon
    Added a script to delete the Brand Purge job in Icon
    
    Related work items: #21039

---
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/Release/DeleteBrandPurgeJob.sql
@@ -0,0 +1,7 @@
+﻿IF EXISTS (
+		SELECT job_id
+		FROM msdb.dbo.sysjobs_view
+		WHERE NAME = N'Brand Purge - Icon'
+		)
+	EXEC msdb.dbo.sp_delete_job @job_name = N'Brand Purge - Icon'
+		,@delete_unused_schedule = 1
