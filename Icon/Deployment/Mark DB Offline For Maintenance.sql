db3fe9dea6620b2b378dc83d0107a04084e29f26
Tom Lux <tom.lux@hotmail.com>
2017-04-13 09:27:23 -0600

    Adding script for IRMA/Icon/Mamm to set maint flag to mark DB as offline for maintenance.

---
+++ Icon/Deployment/Mark DB Offline For Maintenance.sql
@@ -0,0 +1,18 @@
+declare @dbName nvarchar(256), @schemaName nvarchar(64)
+select @dbName = db_name(), @schemaName = 'app'
+if @dbname like 'itemcat%'
+	select @schemaName = 'dbo'
+
+-- Ensure the feature of the maint flag is in place before we try to use it.
+-- For Icon/Mamm schema=app, for IRMA
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' and table_schema = @schemaName AND TABLE_NAME='dbstatus')
+begin
+	if @schemaName = 'app'
+		update app.dbstatus set statusFlagValue = 1, lastUpdateDate = getdate() where StatusFlagName = 'IsOfflineForMaintenance'
+	else
+		update dbo.dbstatus set statusFlagValue = 1, lastUpdateDate = getdate() where StatusFlagName = 'IsOfflineForMaintenance'
+end
+else
+	print 'DB-Status table does not exist... yet?'
+
