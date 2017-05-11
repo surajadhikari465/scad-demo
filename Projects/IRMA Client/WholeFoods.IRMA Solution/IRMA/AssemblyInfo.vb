0c008006bb388658be398f9a387188de1766c864
Tom Lux <tom.lux@hotmail.com>
2017-05-11 11:12:31 -0600

    Incrementing client version to 8.8.0.

--- Projects/IRMA Client/WholeFoods.IRMA Solution/IRMA/AssemblyInfo.vb
+++ Projects/IRMA Client/WholeFoods.IRMA Solution/IRMA/AssemblyInfo.vb
@@ -28,10 +28,10 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("8.6.0.0")>
+<Assembly: AssemblyVersion("8.8.0.0")>
 
 
-<Assembly: AssemblyFileVersionAttribute("8.6.0.0")>
+<Assembly: AssemblyFileVersionAttribute("8.8.0.0")>
 
 ' Specify that the logger is configured using the app.config file 
 <Assembly: log4net.Config.XmlConfigurator()> 
