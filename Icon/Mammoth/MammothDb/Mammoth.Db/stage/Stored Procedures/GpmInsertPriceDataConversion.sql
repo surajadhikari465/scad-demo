4e4be0b337bb7cdc7ec5846c20ac5db77e6f6dc2
Ben Sims <benjamin.sims@wholefoods.com>
2017-05-26 21:58:05 +0000

    Merged PR 238: Merge 20475_GpmMammoth_SSISDataConversion to master
    Much of this might be gobbledygook since it's just xml files, but here's the gist of what I did:
    Added a few objects in mammoth db:
    1. [stage].[GpmDataConversion] table.  This is the staging table for what we get from IRMA.  Doing it this way allowed for me to specify a region's price table in Mammoth
    2. [stage].[GpmInsertPriceDataConversion] sproc - this sproc inserts from staging table (above) into the gpm.Price_XX table.
    
    SSIS Package Steps:
    1.  Truncate Mammoth Staging Table
    2.  Run the Price Extract sproc in IRMA
    3.  Do a few other steps like map or lookup some other necessary columns
    4.  Then do two steps:
    -      Insert into the mammoth staging table
    -      Write data to a flat file to a configured location
    5.  Execute the sproc to take data from staging into gpm.Price_XX table (I truncate the gpm.Price_XX table inside this sproc).
    6.  Tranfer file from file location to the location specified by WFM Infor Team
    7.  Truncate the mammoth staging table.
    
    I've submitted this to DBAs to create a Sql Agent Job in Dev/Test.  It will read from a configuration file where you specify the IRMA connection, file locations, and Mammoth connection.  You also have to specify the correct region inside this file.
    
    Related work items: #22051, #20475

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/stage/Stored Procedures/GpmInsertPriceDataConversion.sql
@@ -0,0 +1,86 @@
+﻿CREATE PROCEDURE [stage].[GpmInsertPriceDataConversion]
+	@BatchSize int = 0
+AS
+
+IF @BatchSize = 0
+	SET @BatchSize = 50000;
+
+-- Iterate through each region returned in stage.GpmDataConversion table
+DECLARE @Region nvarchar(2);
+DECLARE db_cursor CURSOR FOR  
+	SELECT DISTINCT Region 
+	FROM [stage].[GpmDataConversion]
+
+OPEN db_cursor   
+FETCH NEXT FROM db_cursor INTO @Region   
+
+WHILE @@FETCH_STATUS = 0   
+BEGIN   
+    
+	DECLARE @sql nvarchar(max);
+
+	-- Truncate gpm price table for that region
+	SET @sql = N'TRUNCATE TABLE [gpm].[Price_' + @Region + '];';
+	EXEC sp_executesql @sql;
+
+	-- Insert into gpm price table from staging table for that region
+	SET @sql = N'
+	SET IDENTITY_INSERT [gpm].[Price_'+ @Region + '] ON
+	WHILE 1 = 1
+	BEGIN
+		INSERT INTO [gpm].[Price_' + @Region + ']
+		(
+			[Region],
+			[PriceID],
+			[GpmID],
+			[ItemID],
+			[BusinessUnitID],
+			[StartDate],
+			[EndDate],
+			[Price],
+			[PriceType],
+			[PriceTypeAttribute],
+			[SellableUOM],
+			[CurrencyCode],
+			[Multiple],
+			[NewTagExpiration],
+			[InsertDateUtc]
+		)
+		SELECT TOP(@BatchSize)
+			@Region as Region,
+			s.PriceID,
+			s.GpmID,
+			s.ItemID,
+			s.BusinessUnitID,
+			s.StartDate,
+			s.EndDate,
+			s.Price,
+			s.PriceType,
+			s.PriceTypeAttribute,
+			s.SellableUOM,
+			s.CurrencyCode,
+			s.Multiple,
+			s.NewTagExpiration,
+			s.InsertDateUtc
+		FROM [stage].[GpmDataConversion] s
+		WHERE s.Region = @Region
+			AND NOT EXISTS (
+			SELECT 1
+			FROM [gpm].[Price_' + @Region + ']
+			WHERE PriceID = s.PriceID
+				AND Region = s.Region
+		)
+
+		IF @@ROWCOUNT < @BatchSize
+			BREAK
+	END
+	SET IDENTITY_INSERT [gpm].[Price_'+ @Region + '] OFF';
+
+	PRINT @sql;
+	EXEC sp_executesql @sql, N'@BatchSize int, @Region nvarchar(2)', @BatchSize = @BatchSize, @Region = @Region;
+
+	FETCH NEXT FROM db_cursor INTO @Region
+END  
+
+CLOSE db_cursor   
+DEALLOCATE db_cursor
---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/stage/Tables/GpmDataConversion.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE [stage].[GpmDataConversion]
+(
+	[Region] [nchar](2) NOT NULL,
+    [PriceID] [bigint] IDENTITY(1,1) NOT NULL,
+    [GpmID] [uniqueidentifier] NOT NULL,
+    [ItemID] [int] NOT NULL,
+    [BusinessUnitID] [int] NOT NULL,
+    [StartDate] [datetime2](7) NOT NULL,
+    [EndDate] [datetime2](7) NULL,
+    [Price] [smallmoney] NOT NULL,
+    [PriceType] [nvarchar](3) NOT NULL,
+    [PriceTypeAttribute] [nvarchar](10) NOT NULL,
+    [SellableUOM] [nvarchar](3) NOT NULL,
+    [CurrencyCode] [nvarchar](3) NOT NULL,
+    [Multiple] [tinyint] NOT NULL,
+    [NewTagExpiration] [datetime2](7) NULL,
+    [InsertDateUtc] [datetime2](7) NOT NULL DEFAULT (SYSUTCDATETIME())
+)
+GO
+
+CREATE NONCLUSTERED INDEX [CIX_StageGpmDataConversion]
+    ON [stage].[GpmDataConversion]([Region] ASC, [PriceID] ASC)
+	INCLUDE
+	(
+		GpmID,
+		ItemID,
+		BusinessUnitID,
+		StartDate,
+		EndDate,
+		Price,
+		PriceType,
+		PriceTypeAttribute,
+		SellableUOM,
+		CurrencyCode,
+		Multiple,
+		NewTagExpiration,
+		InsertDateUtc
+	) WITH (FILLFACTOR = 100);
+GO
