f65c545716dcdf32ff449e4b40d44dd247e3ba93
Amit Uppal <auppal80@gmail.com>
2017-06-06 13:41:26 -0500

    latest changes after code review.

---
+++ Icon/Shared/Icon.DB/infor/Tables/MessageArchiveLocale.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE infor.MessageArchiveLocale (
+	MessageArchiveId INT NOT NULL identity(1, 1)
+	,LocaleId INT NULL
+	,BusinessUnitId INT NULL
+	,LocaleName NVARCHAR(255) NOT NULL
+	,LocaleTypeCode NVARCHAR(3) NOT NULL
+	,InforMessageId UNIQUEIDENTIFIER NULL
+	,Action NVARCHAR(30) NOT NULL
+	,Context NVARCHAR(max) NOT NULL
+	,ErrorCode NVARCHAR(30) NULL
+	,ErrorDetails NVARCHAR(max) NULL
+	,InsertDate DATETIME2(7) CONSTRAINT DF_MessageArchiveLocale_InsertDate DEFAULT(sysdatetime()) NOT NULL
+	)
+GO
+ALTER TABLE infor.MessageArchiveLocale ADD CONSTRAINT MessageArchiveLocale_PK PRIMARY KEY CLUSTERED (MessageArchiveId)
