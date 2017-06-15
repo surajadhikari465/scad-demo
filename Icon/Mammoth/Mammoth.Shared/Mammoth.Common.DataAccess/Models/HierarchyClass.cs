ff63905b32b2b0732840d16f08c17b90ce6d2a44
Ben Sims <benjamin.sims@wholefoods.com>
2017-06-15 18:12:08 +0000

    Merged PR 294: Adding some Db models to Mammoth.Common.DataAccess so they are available in
    Adding some Db models to Mammoth.Common.DataAccess so they are available in nuget package

---
+++ Icon/Mammoth/Mammoth.Shared/Mammoth.Common.DataAccess/Models/HierarchyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mammoth.Common.DataAccess.Models
+{
+    public partial class HierarchyClass
+    {
+        public int HierarchyClassID { get; set; }
+        public Nullable<int> HierarchyID { get; set; }
+        public string HierarchyClassName { get; set; }
+        public System.DateTime AddedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public virtual Hierarchy Hierarchy { get; set; }
+    }
+}
---
+++ Icon/Mammoth/Mammoth.Shared/Mammoth.Common.DataAccess/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mammoth.Common.DataAccess.Models
+{
+    public partial class Item
+    {
+        public int ItemID { get; set; }
+        public Nullable<int> ItemTypeID { get; set; }
+        public string ScanCode { get; set; }
+        public Nullable<int> HierarchyMerchandiseID { get; set; }
+        public Nullable<int> HierarchyNationalClassID { get; set; }
+        public Nullable<int> BrandHCID { get; set; }
+        public Nullable<int> TaxClassHCID { get; set; }
+        public Nullable<int> PSNumber { get; set; }
+        public string Desc_Product { get; set; }
+        public string Desc_POS { get; set; }
+        public string PackageUnit { get; set; }
+        public string RetailSize { get; set; }
+        public string RetailUOM { get; set; }
+        public Nullable<bool> FoodStampEligible { get; set; }
+        public System.DateTime AddedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+    }
+}
