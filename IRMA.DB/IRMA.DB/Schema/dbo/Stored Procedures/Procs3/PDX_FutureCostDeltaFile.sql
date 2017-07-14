260915bca3b980e4185bc49309b778552b1bc368
Min Zhao <min.zhao@wholefoods.com>
2017-07-14 17:34:50 +0000

    Merged PR 335: Updated the Future Cost package and related code
    Updated the Future Cost package and related code so that it can be deployed to SQL server 2014+.
    
    Related work items: #20350

--- IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs3/PDX_FutureCostDeltaFile.sql
+++ IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs3/PDX_FutureCostDeltaFile.sql
@@ -3,13 +3,13 @@ AS
 BEGIN
 	SET NOCOUNT ON
 	
-	DECLARE @today DATETIME = CONVERT(DATE, getdate(), 102)
-			,@yesterday DATETIME = CONVERT(DATE, getdate() - 1, 102)
+	DECLARE @today DATETIME = CONVERT(DATE, GETDATE(), 102)
+			,@yesterday DATETIME = CONVERT(DATE, GETDATE() - 1, 102)
 			,@ExcludedStoreNo varchar(250) = (SELECT dbo.fn_GetAppConfigValue('LabAndClosedStoreNo','IRMA Client'))
 
 	;WITH VendorCostHistoryMostCurrent 
 	AS (
-		SELECT max(VendorCostHistoryID) AS VendorCostHistoryID, vch.StoreItemVendorID
+		SELECT MAX(VendorCostHistoryID) AS VendorCostHistoryID, vch.StoreItemVendorID
 		  FROM VendorCostHistory vch
 		  JOIN StoreItemVendor siv ON vch.StoreItemVendorID = siv.StoreItemVendorID
 		 WHERE vch.StartDate <= @today
@@ -20,9 +20,9 @@ BEGIN
 
 	VendorCostHistoryFuture
 	AS (
-		SELECT Max(vch.VendorCostHistoryID) AS VendorCostHistoryID
+		SELECT MAX(vch.VendorCostHistoryID) AS VendorCostHistoryID
 		  FROM VendorCostHistory vch
-		  JOIN VendorCostHistoryMostCurrent vchmc on vch.StoreItemVendorID = vchmc.StoreItemVendorID 
+		  JOIN VendorCostHistoryMostCurrent vchmc ON vch.StoreItemVendorID = vchmc.StoreItemVendorID 
 		 WHERE vch.VendorCostHistoryID > vchmc.VendorCostHistoryID
 		   AND vch.InsertDate < @today
 		   AND vch.InsertDate > @yesterday
@@ -33,7 +33,7 @@ BEGIN
 	AS (
 		SELECT vchf.VendorCostHistoryID AS VendorCostHistoryID
 		  FROM VendorCostHistoryFuture vchf
-		  JOIN VendorCostHistory vch on vchf.VendorCostHistoryID = vch.VendorCostHistoryID
+		  JOIN VendorCostHistory vch ON vchf.VendorCostHistoryID = vch.VendorCostHistoryID
 		 WHERE not exists (SELECT vchr.VendorCostHistoryID 
 	                 FROM VendorCostHistory vchr
 	                WHERE vchf.VendorCostHistoryID <> vchr.VendorCostHistoryID
@@ -42,16 +42,20 @@ BEGIN
 					  AND vchr.UnitCost = vch.UnitCost
 					  AND vchr.InsertDate < @yesterday)
 	)
-	SELECT convert(varchar, vch.StartDate, 112) as EFF_DATE, ii.identifier as NAT_UPC, convert(varchar,s.BusinessUnit_ID) as STORE_NUMBER, cast(vch.UnitCost as decimal(16,2)) as COST
+	SELECT	
+		 CONVERT(VARCHAR, vch.StartDate, 112) AS EFF_DATE
+		,RIGHT('0000000000000'+ISNULL(ii.Identifier,''),13) AS NAT_UPC
+		,CONVERT(varchar,s.BusinessUnit_ID) AS STORE_NUMBER
+		,CAST(vch.UnitCost AS decimal(16,2)) AS COST
 	FROM VendorCostHistory vch
-	JOIN UniqueFutureCost vchf on vch.VendorCostHistoryID = vchf.VendorCostHistoryID 
-	JOIN StoreItemVendor siv on vch.StoreItemVendorID = siv.StoreItemVendorID
-	JOIN Item i on siv.Item_Key = i.Item_Key
-	JOIN ItemIdentifier ii on siv.Item_Key = ii.Item_Key
-	JOIN Store s on siv.Store_No = s.Store_No
+	JOIN UniqueFutureCost vchf ON vch.VendorCostHistoryID = vchf.VendorCostHistoryID 
+	JOIN StoreItemVendor siv ON vch.StoreItemVendorID = siv.StoreItemVendorID
+	JOIN Item i ON siv.Item_Key = i.Item_Key
+	JOIN ItemIdentifier ii ON siv.Item_Key = ii.Item_Key
+	JOIN Store s ON siv.Store_No = s.Store_No
 	WHERE i.Deleted_Item = 0 AND i.Remove_Item = 0
 	  AND ii.Deleted_Identifier = 0 AND ii.Remove_Identifier = 0
 	  AND ii.Default_Identifier = 1
-	  AND s.Store_No not in (select Key_Value from dbo.fn_Parse_List(@ExcludedStoreNo, '|')) 
-	order by ii.identifier, s.BusinessUnit_ID
+	  AND s.Store_No not in (SELECT Key_Value FROM dbo.fn_Parse_List(@ExcludedStoreNo, '|')) 
+	ORDER BY ii.identifier, s.BusinessUnit_ID
 END
--- IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs3/PDX_FutureCostFullFile.sql
+++ IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs3/PDX_FutureCostFullFile.sql
@@ -2,13 +2,13 @@
 AS
 BEGIN
 	SET NOCOUNT ON
-	set transaction isolation level read uncommitted
-	DECLARE @today DATETIME = CONVERT(DATE, getdate())
-			,@ExcludedStoreNo varchar(250) = (SELECT dbo.fn_GetAppConfigValue('LabAndClosedStoreNo','IRMA Client'))
+	
+	DECLARE @today DATETIME = CONVERT(DATE, GETDATE())
+			,@ExcludedStoreNo VARCHAR(250) = (SELECT dbo.fn_GetAppConfigValue('LabAndClosedStoreNo','IRMA Client'))
 
 	;WITH VendorCostHistoryMostCurrent 
 	AS (
-		SELECT max(VendorCostHistoryID) AS VendorCostHistoryID, vch.StoreItemVendorID
+		SELECT MAX(VendorCostHistoryID) AS VendorCostHistoryID, vch.StoreItemVendorID
 		  FROM VendorCostHistory vch
 		  JOIN StoreItemVendor siv ON vch.StoreItemVendorID = siv.StoreItemVendorID
 		 WHERE vch.StartDate <= @today
@@ -18,22 +18,26 @@ BEGIN
 	),
 	VendorCostHistoryFuture
 	AS (
-		SELECT Max(vch.VendorCostHistoryID) AS VendorCostHistoryID
+		SELECT MAX(vch.VendorCostHistoryID) AS VendorCostHistoryID
 		  FROM VendorCostHistory vch
 		  JOIN VendorCostHistoryMostCurrent vchmc on vch.StoreItemVendorID = vchmc.StoreItemVendorID 
 		 WHERE vch.VendorCostHistoryID > vchmc.VendorCostHistoryID
 	  GROUP BY vch.StoreItemVendorID, StartDate
 	)
-	SELECT convert(varchar, vch.StartDate, 112) as EFF_DATE, ii.identifier as NAT_UPC, convert(varchar,s.BusinessUnit_ID) as STORE_NUMBER, cast(vch.UnitCost as decimal(16,2)) as COST
+	SELECT 
+		 CONVERT(VARCHAR, vch.StartDate, 112) AS EFF_DATE
+		,RIGHT('0000000000000'+ISNULL(ii.Identifier,''),13) AS NAT_UPC
+		,CONVERT(VARCHAR,s.BusinessUnit_ID) AS STORE_NUMBER
+		,CAST(vch.UnitCost AS DECIMAL(16,2)) AS COST
 	FROM VendorCostHistory vch
-	JOIN VendorCostHistoryFuture vchf on vch.VendorCostHistoryID = vchf.VendorCostHistoryID 
-	JOIN StoreItemVendor siv on vch.StoreItemVendorID = siv.StoreItemVendorID
-	JOIN Item i on siv.Item_Key = i.Item_Key
-	JOIN ItemIdentifier ii on siv.Item_Key = ii.Item_Key
-	JOIN Store s on siv.Store_No = s.Store_No
+	JOIN VendorCostHistoryFuture vchf ON vch.VendorCostHistoryID = vchf.VendorCostHistoryID 
+	JOIN StoreItemVendor siv ON vch.StoreItemVendorID = siv.StoreItemVendorID
+	JOIN Item i ON siv.Item_Key = i.Item_Key
+	JOIN ItemIdentifier ii ON siv.Item_Key = ii.Item_Key
+	JOIN Store s ON siv.Store_No = s.Store_No
 	WHERE i.Deleted_Item = 0 AND i.Remove_Item = 0
 	  AND ii.Deleted_Identifier = 0 AND ii.Remove_Identifier = 0
 	  AND ii.Default_Identifier = 1
-	  AND s.Store_No not in (select Key_Value from dbo.fn_Parse_List(@ExcludedStoreNo, '|')) 
-	order by ii.identifier, s.BusinessUnit_ID
+	  AND s.Store_No NOT IN (SELECT Key_Value FROM dbo.fn_Parse_List(@ExcludedStoreNo, '|')) 
+	ORDER BY ii.identifier, s.BusinessUnit_ID
 END
