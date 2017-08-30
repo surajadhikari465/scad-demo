f47ad8dd583b4761190af0f881dfdca2207e6289
Amit Uppal <auppal80@gmail.com>
2017-08-30 08:38:42 -0500

    added BW to insert data into irma queue

---
+++ Icon/Shared/Icon.DB/app/Tables/JobSchedule.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [app].[JobSchedule]
+(	JobScheduleId int  IDENTITY (1, 1) NOT NULL,
+	JobName nvarchar(50) NOT NULL,
+	Region nvarchar(2) NULL,
+	DestinationQueueName nvarchar(50) NULL,
+	StartDateTimeUtc datetime2(7) NOT NULL,		
+	LastRunDateTimeUtc datetime2(7) NULL,	
+	IntervalInSeconds int NOT NULL,
+	Enabled bit NOT NULL,	
+	XmlObject nvarchar(max) NULL,
+    CONSTRAINT [JobSchedule_PK] PRIMARY KEY CLUSTERED (JobScheduleId ASC) WITH (FILLFACTOR = 80)
+)
