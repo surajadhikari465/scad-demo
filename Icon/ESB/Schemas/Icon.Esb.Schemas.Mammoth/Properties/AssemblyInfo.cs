6252f9fd12a41854f924f1824392c11a91c4c9e1
Ben Sims <benjamin.sims@wholefoods.com>
2017-09-26 21:46:48 +0000

    Merged PR 439: Merge 23002_SchemaNugetPkg_AddSchema to master
    Changed project to a WCF class library for xsd code generation

--- Icon/ESB/Schemas/Icon.Esb.Schemas.Mammoth/Properties/AssemblyInfo.cs
+++ Icon/ESB/Schemas/Icon.Esb.Schemas.Mammoth/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Icon.Esb.Schemas.Mammoth")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1653a95a-89c9-406e-b976-e930ac36b382")]
+[assembly: Guid("a2763a43-d9ea-402e-b46b-95ad46c121d4")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
