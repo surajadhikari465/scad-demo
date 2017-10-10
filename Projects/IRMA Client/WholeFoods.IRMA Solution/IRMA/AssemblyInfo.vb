877a981a3528ea351f7980167fcd80b542bb3a7f
Tom Lux <tom.lux@hotmail.com>
2017-10-10 18:40:53 +0000

    Merged PR 459: Incremented IRMA Client to 9.3.0 for deploy to QA in sprint 103
    Incremented IRMA Client to 9.3.0 for deploy to QA in sprint 103

--- Projects/IRMA Client/WholeFoods.IRMA Solution/IRMA/AssemblyInfo.vb
+++ Projects/IRMA Client/WholeFoods.IRMA Solution/IRMA/AssemblyInfo.vb
@@ -28,10 +28,10 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("9.2.0.0")>
+<Assembly: AssemblyVersion("9.3.0.0")>
 
 
-<Assembly: AssemblyFileVersionAttribute("9.2.0.0")>
+<Assembly: AssemblyFileVersionAttribute("9.3.0.0")>
 
 ' Specify that the logger is configured using the app.config file 
 <Assembly: log4net.Config.XmlConfigurator()> 
