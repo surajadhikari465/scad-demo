f5d54d580f3fbeffa4f2e79f8191022735910ad2
Ben Sims <benjamin.sims@wholefoods.com>
2017-10-27 21:51:29 +0000

    Merged PR 496: Removed 'Database' column from RetentionPolicy table.  Updated Pre and Post
    These changes should fix the dacpac
    - Removed 'Database' column from RetentionPolicy table.  The data purge sproc didn't reference them anyway.
    - Updated Pre and Post Deployment scripts to no longer point to different databases.  This is ultimately what caused the dacpac to fail.
    - Removed refactorlog.
    
    Related work items: #23951

--- Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PopulateRetentionPolicyForStagingTables.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PopulateRetentionPolicyForStagingTables.sql
@@ -5,80 +5,20 @@ set @scriptKey = 'Re-PopulateRetentionPolicyForStagingTables'
 IF(NOT exists(Select * from app.PostDeploymentScriptHistory where ScriptKey = @scriptKey))
 BEGIN
 	Print 'running script ' + @scriptKey 
-
-	DECLARE @serverName nvarchar(50);
-	SET @serverName = @@SERVERNAME;
-
-	-- DEV and TEST Instance
-	IF @serverName = 'CEWD6587\MAMMOTH'
-	BEGIN
-		-- DEV DB
-		USE Mammoth_Dev
-		INSERT INTO [app].[RetentionPolicy]([Database], [Schema], [Table], [ReferenceColumn], [DaysToKeep], [TimeToStart], [TimeToEnd], [IncludedInDailyPurge], [DailyPurgeCompleted], [PurgeJobName], [LastPurgedDateTime])
-		VALUES 
-		('Mammoth_Dev', 'app', 'AppLog', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'esb', 'MessageQueueItemLocale', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'esb', 'MessageQueuePrice', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'esb', 'MessageQueueItemLocaleArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'esb', 'MessageQueuePriceArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'gpm', 'MessageArchivePrice', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'esb', 'MessageArchive', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth_Dev', 'gpm', 'MessageArchivePriceR10', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL)
-
-
-		-- TEST DB
-		USE Mammoth
-
-		INSERT INTO [app].[RetentionPolicy]([Database], [Schema], [Table], [ReferenceColumn], [DaysToKeep], [TimeToStart], [TimeToEnd], [IncludedInDailyPurge], [DailyPurgeCompleted], [PurgeJobName], [LastPurgedDateTime])
-		VALUES 
-		('Mammoth', 'app', 'AppLog', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueueItemLocale', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueuePrice', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueueItemLocaleArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueuePriceArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'gpm', 'MessageArchivePrice', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageArchive', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'gpm', 'MessageArchivePriceR10', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL)
-	END
-
-	-- QA Instance
-	IF @serverName = 'QA-01-MAMMOTH\MAMMOTH'
-	BEGIN
-		USE Mammoth
-
-		INSERT INTO [app].[RetentionPolicy]([Database], [Schema], [Table], [ReferenceColumn], [DaysToKeep], [TimeToStart], [TimeToEnd], [IncludedInDailyPurge], [DailyPurgeCompleted], [PurgeJobName], [LastPurgedDateTime])
-		VALUES 
-		('Mammoth', 'app', 'AppLog', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueueItemLocale', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueuePrice', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueueItemLocaleArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueuePriceArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'gpm', 'MessageArchivePrice', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageArchive', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'gpm', 'MessageArchivePriceR10', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL)
-	END
-
-	-- PRD Instance
-	IF @serverName = 'PRD-01-MAMMOTH\MAMMOTH'
-	BEGIN
-		USE Mammoth
-
-		INSERT INTO [app].[RetentionPolicy]([Database], [Schema], [Table], [ReferenceColumn], [DaysToKeep], [TimeToStart], [TimeToEnd], [IncludedInDailyPurge], [DailyPurgeCompleted], [PurgeJobName], [LastPurgedDateTime])
-		VALUES 
-		('Mammoth', 'app', 'AppLog', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueueItemLocale', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueuePrice', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueueItemLocaleArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageQueuePriceArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'gpm', 'MessageArchivePrice', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'esb', 'MessageArchive', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
-		('Mammoth', 'gpm', 'MessageArchivePriceR10', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL)
-	END
-
+	INSERT INTO [app].[RetentionPolicy]([Schema], [Table], [ReferenceColumn], [DaysToKeep], [TimeToStart], [TimeToEnd], [IncludedInDailyPurge], [DailyPurgeCompleted], [PurgeJobName], [LastPurgedDateTime])
+	VALUES 
+	('app', 'AppLog', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('esb', 'MessageQueueItemLocale', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('esb', 'MessageQueuePrice', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('esb', 'MessageQueueItemLocaleArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('esb', 'MessageQueuePriceArchive', 'InsertDate', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('gpm', 'MessageArchivePrice', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('esb', 'MessageArchive', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL),
+	('gpm', 'MessageArchivePriceR10', 'InsertDateUtc', 10, 21, 24, 1, 0, 'Data History Purge', NULL)
 	insert into app.PostDeploymentScriptHistory values(@scriptKey, getdate())
 END
 ELSE
 BEGIN
-	print 'Skipping script ' + @scriptKey
+	PRINT '[' + convert(NVARCHAR, getdate(), 121) + '] ' + 'Pop-data already applied: ' + @scriptKey
 END
 GO
--- Icon/Mammoth/MammothDb/Mammoth.Db/app/Tables/RetentionPolicy.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/app/Tables/RetentionPolicy.sql
@@ -1,35 +1,15 @@
 ﻿CREATE TABLE [app].[RetentionPolicy](
-
-[RetentionPolicyId] [int] IDENTITY(1,1) NOT NULL,
-
-[Database] [nvarchar](16) NOT NULL,
-
-[Schema] [nvarchar](50) NOT NULL,
-
-[Table] [nvarchar](64) NOT NULL,
-
-[ReferenceColumn] [nvarchar](50) NOT NULL,
-
-[DaysToKeep] [int] NOT NULL,
-
-[TimeToStart] [int] NOT NULL,
-
-[TimeToEnd] [int] NOT NULL,
-
-[IncludedInDailyPurge] [bit] NOT NULL,
-
-[DailyPurgeCompleted] [bit] NOT NULL,
-
-[PurgeJobName] [nvarchar](50) NOT NULL,
-
-[LastPurgedDateTime] [datetime] NULL,
-
- CONSTRAINT [PK_RetentionPolicy] PRIMARY KEY CLUSTERED 
-
-(
-
-[RetentionPolicyId] ASC
-
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
-
+	[RetentionPolicyId] [int] IDENTITY(1,1) NOT NULL,
+	[Schema] [nvarchar](50) NOT NULL,
+	[Table] [nvarchar](64) NOT NULL,
+	[ReferenceColumn] [nvarchar](50) NOT NULL,
+	[DaysToKeep] [int] NOT NULL,
+	[TimeToStart] [int] NOT NULL,
+	[TimeToEnd] [int] NOT NULL,
+	[IncludedInDailyPurge] [bit] NOT NULL,
+	[DailyPurgeCompleted] [bit] NOT NULL,
+	[PurgeJobName] [nvarchar](50) NOT NULL,
+	[LastPurgedDateTime] [datetime] NULL,
+	CONSTRAINT [PK_RetentionPolicy] PRIMARY KEY CLUSTERED
+		([RetentionPolicyId] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
 ) ON [PRIMARY]
