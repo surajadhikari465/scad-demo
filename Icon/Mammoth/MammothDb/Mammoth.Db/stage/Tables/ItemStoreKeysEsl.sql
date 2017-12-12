6c52a0bd9ebcafa330420c1dcb3f20e9d98eebf1
Ben Sims <benjamin.sims@wholefoods.com>
2017-12-12 21:18:46 +0000

    Merged PR 581: Adding Esl ItemStore Key staging table for the DDS team to use.
    Adding Esl ItemStore Key staging table for the DDS team to use.
    
    - DDS team is blocked by us to progress with their testing.  This will most likely unblock them.  Nonetheless, it's one of the objects we're adding for the ESL project.  I created this branch only for this table.
    
    Related work items: #22816

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/stage/Tables/ItemStoreKeysEsl.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [stage].[ItemStoreKeysEsl]
+(
+	[BusinessUnitID] INT NOT NULL,
+	[ItemID] INT NOT NULL,
+	[InsertDateUtc] DATETIME2(7) NOT NULL
+)
+GO
+
+CREATE CLUSTERED INDEX [CX_ItemStoreKeysEsl_BusinessUnit_InsertDateUtc] ON [stage].[ItemStoreKeysEsl] ([BusinessUnitID] ASC, [InsertDateUtc] ASC)
+GO
+
+GRANT SELECT, UPDATE, INSERT ON [stage].[ItemStoreKeysEsl] TO dds_esl_role
+GO
