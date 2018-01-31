b6cf8670f38a219002bb4a7f4b8b125a1f23b5dc
Min Zhao <min.zhao@wholefoods.com>
2018-01-31 20:03:48 +0000

    Merged PR 686: Updated the data pop script from date table update since it encountered error
    Updated the data pop script from date table update since it encountered error when running against the SO region.
    
    Related work items: #25500

--- IRMA.DB/IRMA.DB/Scripts/PopulateData/PostDeploy.Scripts/PBI25134 - PostDeploly Schema PopData - Update And Repopulate Date Table.sql
+++ IRMA.DB/IRMA.DB/Scripts/PopulateData/PostDeploy.Scripts/PBI25134 - PostDeploly Schema PopData - Update And Repopulate Date Table.sql
@@ -8,11 +8,6 @@ select * into Date_Backup from date where Date_Key > '2017-09-24'
 
 GO
 
-delete Date
-where Date_key > '2017-12-31'
-
-GO
-
 update Date
 set Year = '2017',
 	Quarter = 5,
@@ -23,12 +18,42 @@ and Year = '2018'
 
 GO
 
-declare @date as datetime = '2018-01-01'
+update Date
+set [Year] = YEAR(Date_Key),
+	[Quarter] = DATEPART(QUARTER, Date_Key),
+	[Period] = MONTH(Date_Key), 
+	[Week] = DATEPART(WEEK, Date_Key) - DATEPART(WEEK, DATEADD(MM, DATEDIFF(MM,0,Date_Key), 0))+ 1,
+	[Day_Name] = DATENAME(weekday, Date_Key), 
+	[Day_Of_Week] = DATEPART(weekday,Date_Key),
+	[Day_Of_Month] = DATEPART(DAY,Date_Key),
+	[Day_Of_Year] = DATEPART(dayofyear, Date_Key)
+where Date_Key >= '2018-01-01'
+
+GO
+
+declare @date as datetime = '2020-09-28 00:00:00'
 
 while @date < '2023-01-01'
 BEGIN
-	insert into Date
-	select @date, YEAR(@date), DATEPART(QUARTER, @date), MONTH(@date), DATEPART(WEEK, @DATE)  -
-    DATEPART(WEEK, DATEADD(MM, DATEDIFF(MM,0,@DATE), 0))+ 1, DATENAME(weekday, @date), DATEPART(weekday,@date), DATEPART(DAY,@date), DATEPART(dayofyear, @date)
+	insert into Date 
+           ([Date_Key]
+           ,[Year]
+           ,[Quarter]
+           ,[Period]
+           ,[Week]
+           ,[Day_Name]
+           ,[Day_Of_Week]
+           ,[Day_Of_Month]
+           ,[Day_Of_Year])
+	select  @date, 
+			YEAR(@date), 
+			DATEPART(QUARTER, @date), 
+			MONTH(@date), 
+			DATEPART(WEEK, @DATE) - DATEPART(WEEK, DATEADD(MM, DATEDIFF(MM,0,@DATE), 0))+ 1, 
+			DATENAME(weekday, @date), 
+			DATEPART(weekday,@date), 
+			DATEPART(DAY,@date), 
+			DATEPART(dayofyear, @date)
+
 	set @date = DATEADD(d, 1, @date)
 END 
