7223a79a7c7a4a285ce1e06067dc4c60edc1c5df
Min Zhao <min.zhao@wholefoods.com>
2018-02-19 15:57:52 +0000

    Merged PR 719: Updated the code for item subscription extract to accomodate the Order by Infor
    Updated the code for item subscription extract to accomodate the Order by Infor (PDX) field change (value stored in a different table in IRMA).
    
    Related work items: #25106

--- IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs3/PDX_ItemSubscriptionFile.sql
+++ IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs3/PDX_ItemSubscriptionFile.sql
@@ -19,7 +19,7 @@ join   [dbo].[fn_Parse_List]([dbo].[fn_GetAppConfigValue]('WFMBannerStoresForOrd
 select convert(varchar, getdate(), 112) as EFFECTIVE_DATE, RIGHT('0000000000000'+ISNULL(ii.Identifier,''),13) as UPC, 
 convert(varchar,s.BusinessUnit_ID) as STORE_NUMBER, v.PS_Vendor_ID as VENDOR_NUMBER,
 case si.AUTHORIZED when 1 then 'Y' else 'N' end as AUTHORIZED, 
-case isnull(ia.Check_Box_20,0) when 1 then 'Y' else 'N' end as ORDER_BY_PDX
+case isnull(sie.OrderedByInfor,0) when 1 then 'Y' else 'N' end as ORDER_BY_PDX
 from StoreItem si
 join @IncludedStores ist on si.Store_no = ist.Store_No
 join Store s on si.Store_No = s.Store_No
@@ -28,7 +28,7 @@ join Vendor v on siv.Vendor_ID = v.Vendor_ID
 join ItemIdentifier ii on si.Item_Key = ii.Item_key
 join item i on i.item_key = ii.Item_Key
 join ValidatedScanCode vsc on vsc.ScanCode = ii.Identifier 
-left join ItemAttribute ia on ia.Item_key = i.Item_key
+left join StoreItemExtended sie on sie.Item_key = i.Item_key and sie.Store_No = s.Store_No
 where i.retail_sale = 1
 and i.Deleted_Item = 0 and i.Remove_Item = 0
 and ii.Deleted_Identifier = 0 and Remove_Identifier = 0
