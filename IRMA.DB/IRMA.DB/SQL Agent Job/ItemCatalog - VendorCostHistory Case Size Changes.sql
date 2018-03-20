5074ec37928f60078561368333906c5031e7c1ee
Amit Uppal <amit.uppal@wholefoods.com>
2018-03-20 14:01:35 -0500

    added a SP to get case size changes, create a new job that calls this SP.

---
+++ IRMA.DB/IRMA.DB/SQL Agent Job/ItemCatalog - VendorCostHistory Case Size Changes.sql
@@ -0,0 +1,82 @@
+USE [msdb]
+GO
+
+IF  EXISTS (SELECT job_id FROM msdb.dbo.sysjobs_view WHERE name = N'ItemCatalog - VendorCostHistory Case Size Changes')
+EXEC msdb.dbo.sp_delete_job @job_name=N'ItemCatalog - VendorCostHistory Case Size Changes', @delete_unused_schedule = 1
+GO
+
+DECLARE @Environment varchar(10)
+DECLARE @DBNAME VARCHAR(200)
+IF EXISTS (SELECT * FROM master.dbo.sysdatabases WHERE Name = 'ItemCatalog_TEST')
+	BEGIN
+		SET @DBNAME = 'ItemCatalog_TEST'
+	END
+ELSE
+	BEGIN
+		SET @DBNAME = 'ItemCatalog'
+	END
+
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 3/19/2018 2:21:31 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ItemCatalog - VendorCostHistory Case Size Changes', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'checks for vendor case size changes and inserts a record in mammoth item local queue', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'IRMA Developers', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Run SP CheckForVendorCostHistoryCaseSizeChanges]    Script Date: 3/19/2018 2:21:31 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run SP CheckForVendorCostHistoryCaseSizeChanges', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'exec CheckForVendorCostHistoryCaseSizeChanges', 
+		@database_name=@DBNAME, 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Daily At 5 A.M', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20180319, 
+		@active_end_date=99991231, 
+		@active_start_time=50000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'd848862d-1b6c-45df-a4d6-a97e9b276fae'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
