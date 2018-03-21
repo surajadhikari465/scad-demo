0018272c8b7487c06fdff1b25c89d9c3946678f8
Ed McNab <emcnab@gmail.com>
2018-03-21 15:52:15 +0000

    Merged PR 776: adding RegionGpmStatus class to Mammoth.Framework (precursor check-in for NuGet
    adding RegionGpmStatus class to Mammoth.Framework (precursor check-in for NuGet package for using object in web support project)
    
    Related work items: #25992

---
+++ Icon/Mammoth/Mammoth.Shared/Mammoth.Framework/RegionGpmStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace Mammoth.Framework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("dbo.RegionGpmStatus")]
+    public partial class RegionGpmStatus
+    {
+        [Required]
+        [StringLength(2)]
+        public string Region { get; set; }
+
+        public bool IsGpmEnabled { get; set; }
+    }
+}
