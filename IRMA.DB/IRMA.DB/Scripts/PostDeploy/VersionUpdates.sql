ce667a291cae78dee37f6d5c4269f3f7df67fb83
Tom Lux <tom.lux@hotmail.com>
2018-04-03 20:39:34 +0000

    Merged PR 822: Merge IrmaClientVersionIncrement to master
    DB and client incrementalizationistics.

--- IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
+++ IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
@@ -1,8 +1,8 @@
 print '[' + convert(nvarchar, getdate(), 121) + '] ' + 'Start: [UpdateVersion.sql]'
 go
 
-RAISERROR ('Updating Version table to 9.9.0', 10,1) with nowait
+RAISERROR ('Updating Version table to 10.0.0', 10,1) with nowait
 UPDATE [Version]
-SET [Version] = '9.9.0'
+SET [Version] = '10.0.0'
 where applicationname in ('IRMA CLIENT', 'DATABASE', 'SYSTEM')
 GO
