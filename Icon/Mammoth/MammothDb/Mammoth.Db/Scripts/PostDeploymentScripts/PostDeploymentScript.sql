8e0a60573ccef71447f51ef9f679cea38f01e0bb
Amit Uppal <amit.uppal@wholefoods.com>
2018-04-12 13:37:07 -0500

    shelve in latest changes

--- Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PostDeploymentScript.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PostDeploymentScript.sql
@@ -13,6 +13,6 @@ Post-Deployment Script Template
 -- Please add a date-added comment to the end of your line, to help with tracking, maintenance, and archiving.
 
 :r AlterItemAttributes_ExtAttributeValueColumnSize.sql --2018.03.16 PBI 25777
-
+:r AddIrmaPriceMessageType.sql --2018.04.11 PBI 25853
 -- Run every time
 :r ..\..\Security\SecurityGrants.sql
---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/esb/Tables/MessageArchiveDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE esb.PriceMessageArchiveDetail
+(
+	PriceMessageArchiveDetailID INT IDENTITY (1, 1),
+	MessageAction NVARCHAR(20) NOT NULL,
+	Region NVARCHAR(2) NOT NULL,
+	GpmID  uniqueidentifier NOT NULL,
+	ItemID INT NOT NULL,
+	BusinessUnitID INT NOT NULL,
+	MessageID NVARCHAR(50) NOT NULL,
+	JsonObject NVARCHAR(MAX) NOT NULL,
+	ErrorCode NVARCHAR(50) NULL,
+	ErrorDetails NVARCHAR(MAX) NULL,
+	InsertDateUtc datetime2(7) CONSTRAINT DF_PriceMessageArchiveDetail_InsertDate DEFAULT (SYSUTCDATETIME()) NOT NULL,
+    CONSTRAINT [PK_PriceMessageArchiveDetailID] PRIMARY KEY CLUSTERED (PriceMessageArchiveDetailID ASC) WITH (FILLFACTOR = 100),
+)
