097c7baf1b59eea74d5420534fdd8b5b37b2cef5
Ed McNab <emcnab@gmail.com>
2018-05-11 15:54:46 +0000

    Merged PR 905: Correction for Mammoth currency table column nullability
    added columns to mammoth dbo.Currency table & accidentally made one NOT NULL when it should be nullable - this corrects it
    
    Related work items: #26344

--- Icon/Mammoth/MammothDb/Mammoth.Db/dbo/Tables/Currency.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/dbo/Tables/Currency.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Currency] (
-    [CurrencyID]	INT        IDENTITY (1, 1) NOT NULL,
-    [CurrencyCode]	NVARCHAR (5)  NOT NULL,
+    [CurrencyID]	INT IDENTITY (1, 1) NOT NULL,
+    [CurrencyCode]	NVARCHAR (5) NOT NULL,
     [CurrencyDesc]	NVARCHAR (25) NOT NULL,
-    [IssuingEntity] NVARCHAR(255)  NULL,
-    [NumericCode]	INT  NULL,
-    [MinorUnit]		INT  NULL,
-    [Symbol]		NVARCHAR(3)  NOT NULL, 
+    [IssuingEntity] NVARCHAR(255) NULL,
+    [NumericCode]	INT NULL,
+    [MinorUnit]		INT NULL,
+    [Symbol]		NVARCHAR(3) NULL, 
     CONSTRAINT [PK_Currency] PRIMARY KEY CLUSTERED ([CurrencyID] ASC) WITH (FILLFACTOR = 100)
 );
 GO
