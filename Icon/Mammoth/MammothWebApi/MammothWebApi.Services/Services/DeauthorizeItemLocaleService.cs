9856db743192d9300eaa5cbe6e3d4d9693ab84e6
Amit Uppal <amit.uppal@wholefoods.com>
2018-05-25 11:36:25 -0500

    code to delete price from mammoth when item is deuthorized/deleted

---
+++ Icon/Mammoth/MammothWebApi/MammothWebApi.Services/Services/DeauthorizeItemLocaleService.cs
@@ -0,0 +1,33 @@
+﻿using Mammoth.Common.DataAccess.CommandQuery;
+using Mammoth.Logging;
+using MammothWebApi.DataAccess.Commands;
+using MammothWebApi.Service.Services;
+
+public class DeauthorizeItemLocaleService : IUpdateService<DeauthorizeItemLocale>
+{
+    private ICommandHandler<DeleteItemLocalePriceCommand> deleteItemLocalePriceCommandHandler;
+    private ILogger logger;
+
+    public DeauthorizeItemLocaleService(
+        ICommandHandler<DeleteItemLocalePriceCommand> deleteItemLocalePriceCommandHandler,
+        ILogger logger)
+    {
+        this.deleteItemLocalePriceCommandHandler = deleteItemLocalePriceCommandHandler;
+        this.logger = logger;
+    }
+
+    public void Handle(DeauthorizeItemLocale data)
+    {
+        foreach (var itemLocale in data.ItemLocaleServiceModelList)
+        {
+            deleteItemLocalePriceCommandHandler.Execute(new DeleteItemLocalePriceCommand
+            {
+                Region = itemLocale.Region,
+                BusinessUnitId = itemLocale.BusinessUnitId,
+                ScanCode = itemLocale.ScanCode
+            });
+
+            logger.Info("Price Records Deleted for Region:" + itemLocale.Region + ", ScanCode:" + itemLocale.ScanCode + ", BusinessUnitId:" + itemLocale.BusinessUnitId.ToString());
+        }
+    }
+}
