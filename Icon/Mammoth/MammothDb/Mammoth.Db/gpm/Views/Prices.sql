ab7c2a617c3d1d690799d1c2d83e48b6b7a0bfd7
Amit Uppal <amit.uppal@wholefoods.com>
2018-06-12 11:26:18 -0500

    updated mammoth schema-removed gpmid updated mammothpricelistener to read attribute for gpmid updated active price service

--- Icon/Mammoth/MammothDb/Mammoth.Db/gpm/Views/Prices.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/gpm/Views/Prices.sql
@@ -1,42 +1,42 @@
 ﻿CREATE VIEW [gpm].[Prices]
 	AS 
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_FL]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_MA]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_MW]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_NA]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_NC]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_NE]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_PN]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_RM]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_SO]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_SP]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_SW]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_TS]
 	UNION ALL
-	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, InsertDateUtc, ModifiedDateUtc
+	SELECT Region, PriceID, GpmID, ItemID, BusinessUnitID, StartDate, EndDate, Price, PriceType, PriceTypeAttribute, SellableUOM, CurrencyCode, Multiple, TagExpirationDate, PercentOff, InsertDateUtc, ModifiedDateUtc
 	FROM [gpm].[Price_UK]
 GO
 
