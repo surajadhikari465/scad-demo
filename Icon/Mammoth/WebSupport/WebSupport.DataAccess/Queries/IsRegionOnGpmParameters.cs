efe55bf41a84ab02dc30ac0d9d5faea4b441574c
Amit Uppal <amit.uppal@wholefoods.com>
2018-07-11 14:57:03 -0500

    if region on gpm check gpm tables for item sale and item type. based on that send addupdate or delete PSG.

---
+++ Icon/Mammoth/WebSupport/WebSupport.DataAccess/Queries/IsRegionOnGpmParameters.cs
@@ -0,0 +1,9 @@
+﻿using Icon.Common.DataAccess;
+
+namespace WebSupport.DataAccess.Queries
+{
+    public class IsRegionOnGpmParameters:IQuery<bool>
+    {
+        public string Region { get; set; }
+    }
+}
