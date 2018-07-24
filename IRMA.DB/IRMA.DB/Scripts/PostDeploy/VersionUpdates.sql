f6c229127c3c1f7f1b14a59bff779aead247e284
Tom Lux <tom.lux@hotmail.com>
2018-07-24 15:18:46 +0000

    Merged PR 1033: Archiving 10.2 pop-data and incrementing IRMA to 10.3, but no other DB updates
    Archiving 10.2 pop-data and incrementing IRMA to 10.3, but no other DB updates for this release.

--- IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
+++ IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
@@ -1,8 +1,8 @@
 print '[' + convert(nvarchar, getdate(), 121) + '] ' + 'Start: [UpdateVersion.sql]'
 go
 
-RAISERROR ('Updating Version table to 10.2.0', 10,1) with nowait
+RAISERROR ('Updating Version table to 10.3.0', 10,1) with nowait
 UPDATE [Version]
-SET [Version] = '10.2.0'
+SET [Version] = '10.3.0'
 where applicationname in ('IRMA CLIENT', 'DATABASE', 'SYSTEM')
 GO
