8e9da6d0b47e9ce2b8c2ca17d4b82a8042a4048d
Ben Sims <benjamin.sims@wholefoods.com>
2018-08-10 19:56:02 +0000

    Merged PR 1077: Created AddOrUpdateMessageSequence sproc that is called by PriceListener now.
    Created AddOrUpdateMessageSequence sproc that is called by PriceListener now.  Updated some logic in transitions to be appropriate logic.  Put all logic related to ResetFlag in the GetLastMessageSequenceQuery process
    
    Logic should be:
    1. When ResetFlag="1" we always treat it as `IsInSequence="true"`
    2. When ResetFlag="0" we still need to check normal sequence logic where `SequenceID = LastProcessedSequenceID + 1`
    3. If SequenceID is less than LastProcessedSequenceID we will treat it as if we already processed and no action is taken (unless ResetFlag = "1")
    
    Related work items: #28538

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/gpm/Stored Procedures/AddOrUpdateMessageSequence.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [gpm].[AddOrUpdateMessageSequence]
+	@ItemID int,
+	@BusinessUnitID int,
+	@PatchFamilyID nvarchar(50),
+	@PatchFamilySequenceID int,
+	@MessageID nvarchar(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE [gpm].[MessageSequence]
+	SET 
+		PatchFamilySequenceID = @PatchFamilySequenceID,
+		MessageID = @MessageID,
+		ModifiedDateUtc = SYSUTCDATETIME()
+	WHERE PatchFamilyID = @PatchFamilyID
+
+	IF @@ROWCOUNT = 0
+	BEGIN
+		INSERT INTO [gpm].[MessageSequence] (ItemID, BusinessUnitID, PatchFamilyID, PatchFamilySequenceID, MessageID)
+		VALUES (@ItemID, @BusinessUnitID, @PatchFamilyID, @PatchFamilySequenceID, @MessageID)
+	END
+	
+END
+GO
+
+GRANT EXEC ON [gpm].[AddOrUpdateMessageSequence] TO TibcoRole
+GO
