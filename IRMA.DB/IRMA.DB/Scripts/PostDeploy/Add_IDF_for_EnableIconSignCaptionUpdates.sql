948a866e9cad8be58916e8e7d04c47a5ec424c96
Ed McNab <emcnab@gmail.com>
2018-08-14 20:52:30 +0000

    Merged PR 1088: 28448 Pass the Customer Friendly Description (CFD) Value to the Existing Sign Caption Field in IRMA
    removing unneeded portion of db script (read-only not controlled by attribute values)
    
    Related work items: #25133, #28448

--- IRMA.DB/IRMA.DB/Scripts/PostDeploy/Add_IDF_for_EnableIconSignCaptionUpdates.sql
+++ IRMA.DB/IRMA.DB/Scripts/PostDeploy/Add_IDF_for_EnableIconSignCaptionUpdates.sql
@@ -8,11 +8,3 @@ BEGIN
 	INSERT INTO dbo.InstanceDataFlags (FlagKey, FlagValue, CanStoreOverride)
     VALUES ('EnableIconSignCaptionUpdates', 0, 0)
 END
-
--- Part 2: update EIM attribute to make sign caption readonly
-UPDATE UTA
-SET IsReadOnlyForNewItems = 1 , IsReadOnlyForExistingItems = 1
-FROM dbo.UploadTypeAttribute UTA
-	INNER JOIN dbo.UploadAttribute UA ON UA.UploadAttribute_ID = UTA.UploadAttribute_ID
-WHERE UA.[Name] = 'Sign Caption'
-	AND (UTA.IsReadOnlyForExistingItems=0 OR UTA.IsReadOnlyForExistingItems=0)
