03e56454ddde7ec09a6eb8fd26a636c06c0f3ddc
Robin Eudy <robin.eudy@wholefoods.com>
2018-08-28 19:02:09 -0500

    added swagger and modelvalidation to framework.

---
+++ Icon/KitBuilder/KitBuilderWebApi/KitBuilderWebApi/KitBuilderWebApi/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace KitBuilderWebApi.Filters
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
