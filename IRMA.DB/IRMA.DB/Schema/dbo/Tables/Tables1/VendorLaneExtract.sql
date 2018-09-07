317cdfc189f8f1f83a6ea9ef3effcb5039939e8a
Amit Uppal <amit.uppal@wholefoods.com>
2018-09-07 17:00:37 +0000

    Merged PR 1140: added a new package to make item vendor lane run faster.
    added a new package to make item vendor lane run faster.
    
    Related work items: #28027

---
+++ IRMA.DB/IRMA.DB/Schema/dbo/Tables/Tables1/VendorLaneExtract.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[VendorLaneExtract](
+	[UPC] [varchar](13) NULL,
+	[STORE_NUMBER] [varchar](20) NULL,
+	[VENDOR_NUMBER] [varchar](20) NULL,
+	[VENDOR_NAME] [varchar](50) NULL,
+	[CASE_SIZE] [varchar](15) NULL,
+	[CASE_UOM] [varchar](25) NULL,
+	[VENDOR_COST_UOM] [varchar](25) NULL,
+	[REG_COST] [varchar](25) NULL,
+	[RETAIL_UOM] [varchar](25) NULL,
+	[RETAIL_PACK] [varchar](15) NULL
+) ON [PRIMARY];
