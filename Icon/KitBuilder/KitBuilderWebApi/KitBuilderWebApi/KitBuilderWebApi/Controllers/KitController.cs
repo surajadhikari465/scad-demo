8760fb7c2db8d9a70b7bbfe5cc7347997f57dd0b
Robin Eudy <robin.eudy@wholefoods.com>
2018-09-13 14:04:06 -0500

    KitController

---
+++ Icon/KitBuilder/KitBuilderWebApi/KitBuilderWebApi/KitBuilderWebApi/Controllers/KitController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KitBuilderWebApi.DataAccess.Repository;
+using KitBuilderWebApi.DatabaseModels;
+using KitBuilderWebApi.QueryParameters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KitBuilderWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Kit")]
+    public class KitController : Controller
+    {
+        private IRepository<Kit> kitRepository;
+        private IRepository<Status> statusRespository;
+
+        private ILogger<KitController> logger;
+
+        public KitController(IRepository<Kit> kitRepository,
+            IRepository<Status> statusRespository,
+            ILogger<KitController> logger)
+        {
+            this.kitRepository = kitRepository;
+            this.statusRespository = statusRespository;
+            this.logger = logger;
+        }
+
+
+
+        public IActionResult GetKits(KitParameters kitParameters)
+        {
+            if (!ModelState.IsValid || kitParameters == null)
+                return BadRequest(ModelState);
+
+
+            return Ok();
+        }
+
+        
+    }
+
+    public class KitDto
+    {
+
+    }
+}
