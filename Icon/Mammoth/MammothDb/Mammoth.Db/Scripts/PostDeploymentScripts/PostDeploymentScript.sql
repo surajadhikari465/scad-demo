d61be4a903945189f4039e74fcf07c19e4aeb7d3
Tom Lux <tom.lux@hotmail.com>
2018-09-25 18:43:36 +0000

    Merged PR 1176: Archiving Mamm 3.5 post-deploy.
    Archiving Mamm 3.5 post-deploy.
    
    Related work items: #25429

--- Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PostDeploymentScript.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PostDeploymentScript.sql
@@ -12,9 +12,11 @@ Post-Deployment Script Template
 
 -- Please add a date-added comment to the end of your line, to help with tracking, maintenance, and archiving.
 
--- Run every time
-:r ..\..\Security\SecurityGrants.sql
-:r PopulateItemPriceTypeWithGsp.sql
 :r AddIrmaItemKeyAttributes.sql --PBI 28239 Aug. 15, 2018
 :r UpdateAppLogRetention.sql -- PBI 28749 8/27/2018
 :r AddNumberOfDigitsForCFSItems.sql -- PBI 28686 09/13/2018
+
+
+-- Run every time (add your script above; keep this at bottom, as last post-deploy script).
+:r ..\..\Security\SecurityGrants.sql
+
