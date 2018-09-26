45666d73622062ea9bfa72b196c19072a303c7a1
Vic Lusenko <2172772@wholefoods.com>
2018-09-26 14:04:34 -0500

    Change schema; Added fields to dbo.Item table.   HospitalityItem bit not null default(0),   KitchenItem bit not null default(0),   KitchenDescription varchar(15),   URL1 varchar(255),

--- Icon/Shared/Icon.DB/dbo/Tables/Item.sql
+++ Icon/Shared/Icon.DB/dbo/Tables/Item.sql
@@ -1,19 +1,13 @@
 ﻿CREATE TABLE [dbo].[Item] (
-	[itemID] INT NOT NULL IDENTITY,
-	[itemTypeID] INT NOT NULL,
-	[productKey] INT NOT NULL CONSTRAINT DF_Item_productKey DEFAULT app.fn_GetNextProductKey()
+	itemID INT IDENTITY     constraint Item_PK primary key clustered,
+	itemTypeID INT NOT NULL constraint ItemType_Item_FK1 foreign key references dbo.ItemType(itemTypeID),
+	productKey INT NOT NULL constraint DF_Item_productKey DEFAULT app.fn_GetNextProductKey(),
+  HospitalityItem bit not null default(0),
+  KitchenItem bit not null default(0),
+  KitchenDescription varchar(15),
+  URL1 varchar(255),
 )
 GO
-ALTER TABLE [dbo].[Item] WITH CHECK ADD CONSTRAINT [ItemType_Item_FK1] FOREIGN KEY (
-[itemTypeID]
-)
-REFERENCES [dbo].[ItemType] (
-[itemTypeID]
-)
-GO
-ALTER TABLE [dbo].[Item] ADD CONSTRAINT [Item_PK] PRIMARY KEY CLUSTERED (
-[itemID]
-)
-GO
+
 CREATE NONCLUSTERED INDEX IX_Item_productKey on [dbo].[Item] (productKey)
 GO
