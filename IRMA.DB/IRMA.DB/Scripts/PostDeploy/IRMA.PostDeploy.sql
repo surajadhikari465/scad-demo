1a81a9a9bff6931bfbe84dfc6165ff65c6215260
Alpesh Patel <alpesh.patel@wholefoods.com>
2018-10-15 16:20:06 +0000

    Merged PR 1244: IRMA Db updates for 10.6 release
    IRMA Db updates for 10.6 release
    changed version to 10.6
    Moved 'PopulateAmzEventTypeTable' to archive folder
    
    Cherry-picked from commit `c6c1ed1b`.
    
    Related work items: #25432

--- IRMA.DB/IRMA.DB/Scripts/PostDeploy/IRMA.PostDeploy.sql
+++ IRMA.DB/IRMA.DB/Scripts/PostDeploy/IRMA.PostDeploy.sql
@@ -13,7 +13,6 @@ Post-Deployment Script Template
 -- This post-deploy script is in the base post-deploy folder, so you do not need to add a path or folder reference (you just specify your script name).
 -- Please add a date-added comment to the end of your line, to help with tracking, maintenance, and archiving.
 -- Example: :r Cool_Script_Stuff.sql -- 2018.01.01
-:r PopulateAmzEventTypeTable.sql -- 2018.08.15 - Honeycrisp Project - Populate new table called [amz].[EventType]
 :r InsertOrderTypes.sql
 :r AddEnableAmazonEventGenerationIdf.sql
 :r HC_PurgeMessageArchive.sql
