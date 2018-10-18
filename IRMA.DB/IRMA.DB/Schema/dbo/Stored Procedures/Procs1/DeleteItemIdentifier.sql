5b938abe69f4a53700b3042ee7edcb1693017d8b
Vic <2172772@wholefoods.com>
2018-10-18 18:07:35 +0000

    Merged PR 1264: Alternate item identifier: Refactored proc dbo.DeleteItemIdentifier to generate 'ItemDeauthorization' event.
    Refactored proc dbo.DeleteItemIdentifier.
    As per our meeting with Bobbie , event 'ItemDeauthorization' should be generated immediately (not waiting for Push).
    
    Related work items: #29514

--- IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs1/DeleteItemIdentifier.sql
+++ IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs1/DeleteItemIdentifier.sql
@@ -1,48 +1,30 @@
 ﻿CREATE PROCEDURE [dbo].[DeleteItemIdentifier]
 	@Identifier_ID int
 AS 
-
 BEGIN
-  DECLARE @Error_No int = 0,
-          @Item_Key int,
-          @Identifier varchar(13);
-	
-	SELECT 
-		@Item_Key = Item_Key,  
-		@Identifier = Identifier 
-	FROM ItemIdentifier 
-	WHERE Identifier_ID = @Identifier_ID
-
   BEGIN TRAN
-	  --Send Mammoth Events first because if the Add_Identifier = 1 then the Identifier will be 
-	  --deleted and the no event will be generated
-    --18-10-16: Seems like the the query below has been obsoleted but let's keep it here for awhile just in case
-		--EXEC [mammoth].[InsertItemLocaleChangeQueue] @Item_Key, NULL, 'ItemDelete', @Identifier
-			
-	    DECLARE @IdentifiersType dbo.IdentifiersType
-	    INSERT INTO  @IdentifiersType(Identifier) values(@Identifier)
-	
-	    EXEC [mammoth].[GenerateEvents] @IdentifiersType, 'ItemDeauthorization'
+    set nocount on;
+    declare @Error_No int = 0;
+    declare @IdentifiersType dbo.IdentifiersType;
 
-		  SELECT @Error_No = @@ERROR
-    
-	IF @Error_No = 0
-	BEGIN
-		DELETE 
-		FROM ItemIdentifier
-		WHERE Identifier_ID = @Identifier_ID AND Add_Identifier = 1
+    insert into @IdentifiersType(Identifier)
+      select Identifier from ItemIdentifier where Identifier_ID = @Identifier_ID;
 
-		SELECT @Error_No = @@ERROR
-	END
+    exec mammoth.GenerateEvents @IdentifiersType, 'ItemDeauthorization';
+    set @Error_No = @@ERROR;
 
-    IF @Error_No = 0
-    BEGIN
+    if(@Error_No = 0)
+    begin
+	    if(exists(select 1 from ItemIdentifier where Identifier_ID = @Identifier_ID and Add_Identifier = 1)) --Delete if it's a new Identifier
+		    delete from ItemIdentifier
+        where Identifier_ID = @Identifier_ID;
+      else                                                                                                 --Set Remove_Identifier flag 
         UPDATE ItemIdentifier
         SET Remove_Identifier = 1
-        WHERE Identifier_ID = @Identifier_ID
+        WHERE Identifier_ID = @Identifier_ID;
 
-        SELECT @Error_No = @@ERROR
-    END
+      set @Error_No = @@ERROR;
+    end
 
     IF @Error_No = 0
         COMMIT TRAN
@@ -53,18 +35,10 @@ BEGIN
         RAISERROR ('DeleteItemIdentifier failed with @@ERROR: %d', @Severity, 1, @Error_No)
     END
 END
-
 GO
-GRANT EXECUTE
-    ON OBJECT::[dbo].[DeleteItemIdentifier] TO [IRMAClientRole]
-    AS [dbo];
 
+GRANT EXECUTE ON OBJECT::[dbo].[DeleteItemIdentifier] TO [IRMAClientRole] AS [dbo];
 GO
-GRANT EXECUTE
-    ON OBJECT::[dbo].[DeleteItemIdentifier] TO [IRMASchedJobsRole]
-    AS [dbo];
-
+GRANT EXECUTE ON OBJECT::[dbo].[DeleteItemIdentifier] TO [IRMASchedJobsRole] AS [dbo];
 GO
-GRANT EXECUTE
-    ON OBJECT::[dbo].[DeleteItemIdentifier] TO [IRMAReportsRole]
-    AS [dbo];
+GRANT EXECUTE ON OBJECT::[dbo].[DeleteItemIdentifier] TO [IRMAReportsRole] AS [dbo];
