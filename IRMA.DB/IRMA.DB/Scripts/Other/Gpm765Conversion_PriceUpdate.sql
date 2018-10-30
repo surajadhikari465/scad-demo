ce734656c658330f08c4d829270cc58de53006b1
Ben Sims <benjamin.sims@wholefoods.com>
2018-10-30 15:30:50 +0000

    Merged PR 1277: Added table for Gpm / 765 conversion for all the prices to be cancelled.  Added
    Added table for Gpm / 765 conversion for all the prices to be cancelled.  Added two scripts. One for Cancelling the TPRs and one for updating the IRMA Price table to REG for the long running TPRs turning to REG
    
    For context, the flow of how we will convert all new regions to GPM (with 765 turned on) will be:
    1. Receive a flat file that will have all the TPRs that need to be cancelled in IRMA
    2. Populate the IconPOSPushPublish table (I use the `Replenishment_POSPush_PopulateNonBatchIconPOSPushStaging` for reference).
    3. POS Push Controller (Pusheen) will pick up those rows and messages will eventually get generated from the Api Controller (in Icon).
    4. Update IRMA with all the REG prices that used to be "TPRs" in IRMA (price types like DIS, EDV, CLR, etc)
    
    Related work items: #29333, #29953

---
+++ IRMA.DB/IRMA.DB/Scripts/Other/Gpm765Conversion_PriceUpdate.sql
@@ -0,0 +1,51 @@
+﻿DECLARE @RegPriceTypeID INT = (SELECT TOP 1 PriceChgTypeID FROM PriceChgType WHERE PriceChgTypeDesc = 'REG');
+DECLARE @RegularPromoPricingMethodId INT = (SELECT TOP 1 PricingMethod_ID FROM PricingMethod WHERE PricingMethod_Name = 'Regular Promo')
+
+IF OBJECT_ID('tempdb..#pricesUpdated') IS NOT NULL
+	DROP TABLE #pricesUpdated
+CREATE TABLE #pricesUpdated
+(
+	Item_Key INT NOT NULL,
+	Store_No INT NOT NULL,
+	PriceChgTypeID INT NOT NULL,
+	Price smallmoney NOT NULL,
+	Multiple tinyint NOT NULL,
+	PricingMethod_ID INT NOT NULL
+)
+
+INSERT INTO #pricesUpdated
+(
+	Item_Key,
+	Store_No,
+	PriceChgTypeID,
+	Price,
+	Multiple,
+	PricingMethod_ID
+)
+SELECT
+	ii.Item_Key,
+	s.Store_No,
+	@RegPriceTypeID,
+	c.PRICE,
+	c.MULTIPLE,
+	@RegularPromoPricingMethodId
+FROM infor.GpmConversionTprCancellations c
+JOIN dbo.ValidatedScanCode v on c.ITEM_ID = v.InforItemId
+JOIN dbo.Store s on c.STORE_NUMBER = s.BusinessUnit_ID
+JOIN dbo.ItemIdentifier ii on v.ScanCode = ii.Identifier
+WHERE ii.Default_Identifier = 1
+AND ii.Deleted_Identifier = 0
+AND c.PRICE_TYPE_CODE = 'REG'
+
+CREATE NONCLUSTERED INDEX IX_ItemKey_StoreNo on #pricesUpdated (Store_No ASC, Item_Key ASC);
+
+UPDATE dbo.Price
+SET
+	PriceChgTypeId = u.PriceChgTypeID,
+	Price = u.Price,
+	POSPrice = u.Price,
+	Multiple = u.Multiple,
+	PricingMethod_ID = u.PricingMethod_ID
+FROM dbo.Price p
+INNER JOIN #pricesUpdated u on p.Item_Key = u.Item_Key
+	AND p.Store_No = u.Store_No
