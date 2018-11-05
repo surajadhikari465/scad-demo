870a57e4ea793d3f5ccd2d67c2b4c039bd9e4662
Tom Lux <tom.lux@hotmail.com>
2018-11-05 15:19:40 +0000

    Merged PR 1305: Release-doc generation script and template updates
    PS Script:
    - Added error check for MS-Word spell tags that corrupt placeholder vars.
    - Added var substitution for contents of var-input file so as long as another var is defined above it, a var can use that var inside its value.
    - Get env and release ID values from input file now.
    - Create local temp folder to house the generated rel doc.
    - Output URL to file uploaded to web server.
    Other:
    - To HTM template: added placeholders to support: special release branch info, task after IRMA DB update, manual mammoth deploy.
    - Prod values for input sheet.
    - Removing QA doc I'd checked in.
    
    Related work items: #29937

--- DevOps/Release Documentation/Apply Release Doc Values to HTML Template.ps1
+++ DevOps/Release Documentation/Apply Release Doc Values to HTML Template.ps1
@@ -1,52 +1,79 @@
 ﻿$gitRootFolder = "c:\tlux\dev\git\IRMA"
 
+########################################################################################################################################
+
+$relDocInValuesFile = "$gitRootFolder\DevOps\Release Documentation\ReleaseDoc.in.tsv"
+if(!(Test-Path $relDocInValuesFile)){
+    Write-Host -ForegroundColor Magenta "**ERROR** -- Doc template not found: [$relDocInValuesFile]"
+    Read-Host "Press <ENTER> to exit..."
+    return
+}
+$relDocInValuesTxt = Get-Content -Encoding String $relDocInValuesFile
+$relDocInValues = $relDocInValuesTxt.split("`n") # Each line in rel-doc input file.
+
+########################################################################################################################################
+########################################################################################################################################
+########################################################################################################################################
+
 $relTmpl = "$gitRootFolder\DevOps\Release Documentation\IRMA Apps Release Template.htm"
 if(!(Test-Path $relTmpl)){
-    Write-Host -ForegroundColor Yellow "**ERROR** -- Doc template not found: [$relTmpl]"
+    Write-Host -ForegroundColor Magenta "**ERROR** -- Doc template not found: [$relTmpl]"
     Read-Host "Press <ENTER> to exit..."
     return
 }
-$relDocTxt = Get-Content $relTmpl
+$relDocTxt = Get-Content -Encoding String $relTmpl
 
-$relDocValuesFile = "$gitRootFolder\DevOps\Release Documentation\ReleaseDoc.in.tsv"
-if(!(Test-Path $relDocValuesFile)){
-    Write-Host -ForegroundColor Yellow "**ERROR** -- Doc template not found: [$relDocValuesFile]"
+# Check MS-Word breaking up placeholder strings.
+$mswordSpellTag = "<span class=SpellE>"
+if($relDocTxt.Contains("__$mswordSpellTag")){
+    Write-Host -ForegroundColor Magenta "**ERROR** -- Doc template contains MS-Word spelling formatting, which corrupts placeholders: [$mswordSpellTag].  Edit & save in Word, then retry."
     Read-Host "Press <ENTER> to exit..."
     return
 }
-$relDocValuesTxt = Get-Content $relDocValuesFile
-$relDocValues = $relDocValuesTxt.split("`n") # Each line in rel-doc input file.
 
-##################### TODO: GET THIS FROM INPUT FILE
-$targetEnv = "QA"
-$targetEnvShort = "QA"
-$relDoc = "$gitRootFolder\DevOps\Release Documentation\IRMA Apps Release 2018-11.$targetEnvShort.htm"
-
-$relDocHash = @{}
-foreach($valueDef in $relDocValues){
+# Replace var refs inside input data.  We could add a loop here that could run this replacement a few times to allow more complex/iterative replacement.
+foreach($valueDef in $relDocInValues){
     $values = $valueDef.split("`t") # Each field in a line.
     $varName = $values[0]
     $varDesc = $values[1]
     $value = $values[2]
-    $relDocHash.add($varName, $value)
-    $relDocTxt = $relDocTxt.Replace(("__" + $varName + "__"), $value)
-    <#
-    $relDocTxt = $relDocTxt.Replace("__IrmaVer__", "10.6")
-    $relDocTxt = $relDocTxt.Replace("__MammothVer__", "3.7")
-    $relDocTxt = $relDocTxt.Replace("__IconVer__", "9.8")
-    $relDocTxt = $relDocTxt.Replace("__TibcoVer__", "1.9")
-    $relDocTxt = $relDocTxt.Replace("__IconReleaseBranchName__", "RELEASE_Icon98_Mammoth37_Sprints127-128")
-    $relDocTxt = $relDocTxt.Replace("__IconBuildNum__", "??")
-    $relDocTxt = $relDocTxt.Replace("__IconBuildId__", "??")
-    $relDocTxt = $relDocTxt.Replace("__IrmaReleaseBranchName__", "RELEASE_IRMA106_Sprints127-128")
-    $relDocTxt = $relDocTxt.Replace("__IrmaBuildNum__", "??")
-    $relDocTxt = $relDocTxt.Replace("__IrmaBuildId__", "??")
-    #>
+    $relDocInValuesTxt = $relDocInValuesTxt.Replace(("__" + $varName + "__"), $value)
 }
 
+# Reset input-values array to updated value that were just replaced.
+$relDocInValues = $relDocInValuesTxt.split("`n") # Each line in rel-doc input file.
 
+# Build input-values hash so we can use values inside this script (for output filename, etc.).
+$relValuesInHash = @{}
+foreach($valueDef in $relDocInValues){
+    $values = $valueDef.split("`t") # Each field in a line.
+    $varName = $values[0]
+    $varDesc = $values[1]
+    $value = $values[2]
+    $relValuesInHash.add($varName, $value)
+}
+
+$targetEnv = $relValuesInHash.TargetEnv
+$targetEnvShort = $relValuesInHash.TargetEnvShort
+$relYear = $relValuesInHash.ReleaseYear
+$relId = $relValuesInHash.ReleaseId
+
+# Create local release-doc folder, if needed.
+$relDocFolder = "c:\temp\reldoc\"
+if(-not (Test-Path $relDocFolder)){
+    New-Item -Verbose -ItemType Directory $relDocFolder
+}
+$relDoc = "$relDocFolder\IRMA Apps Release $relYear-$relId.$targetEnvShort.htm"
+
+# Replace values in HTML file with input values.
+foreach($keyName in $relValuesInHash.keys){
+    $relDocTxt = $relDocTxt.Replace(("__" + $keyName + "__"), $relValuesInHash.Item($keyName))
+}
+
+########################################################################################################################################
 
 #function GetHtmlTemplate(
+# Hard-coded here, but we could get from input-variables file or other source.
 $tibcoPropsHtmTmpl =
 "
 <h2>__TargetEnv__</h2>
@@ -69,7 +96,7 @@ function BuildHtmlListItems(
     return $htm
 }
 
-$appListTxt = $relDocHash.TibcoAppsUpdated
+$appListTxt = $relValuesInHash.TibcoAppsUpdated
 if(-not $appListTxt.tolower().Contains("none")){
     $propsListTxt = ""
     $appList = $appListTxt.Split("`n")
@@ -93,7 +120,7 @@ if(-not $appListTxt.tolower().Contains("none")){
     }
     $propsListTxt = $propsListTxt.trim()
 
-    $propsHtm = BuildHtmlListItems $propsListTxt # Remove any extra line so we dont create empty HTML list-item tags.
+    $propsHtm = BuildHtmlListItems $propsListTxt # Remove any extra lines so we dont create empty HTML list-item tags.
     $propFilesHtm = ""
     if($targetEnv -like "QA"){
         $tibcoEnv = "QAF"
@@ -107,11 +134,14 @@ if(-not $appListTxt.tolower().Contains("none")){
     $propFilesHtm = "N/A"
 }
 
-    $relDocTxt = $relDocTxt.Replace("__PropFilesListSection__", $propFilesHtm)
+$relDocTxt = $relDocTxt.Replace("__PropFilesListSection__", $propFilesHtm)
 
 ##################################################################################################################
 ##################################################################################################################
 
 Set-Content $relDoc -Value $relDocTxt
+$relDocFileObj = Get-ChildItem $relDoc
+$docDest = "\\irmaqaapp1\e$\inetpub\wwwroot\reldoc"
+Copy-Item $relDoc -Destination $docDest
 
-Copy-Item $relDoc -Destination "\\irmaqaapp1\e$\inetpub\wwwroot\reldoc"
+"URL --> " + "http://irmaqaapp1/reldoc/" + $relDocFileObj.name
