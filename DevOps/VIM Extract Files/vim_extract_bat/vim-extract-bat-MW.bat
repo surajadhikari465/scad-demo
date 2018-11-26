d1bc59e03b9d21494c355074fbfaf75c919f9521
Alpesh Patel <alpesh.patel@wholefoods.com>
2018-11-26 16:45:39 +0000

    Merged PR 1389: created batch files to execute VIM extract SSIS packages from CMD line.
    created batch files to execute VIM extract SSIS packages from CMD line.
    
    Related work items: #30279

---
+++ DevOps/VIM Extract Files/vim_extract_bat/vim-extract-bat-MW.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+CLS
+ECHO You are about to execute the TestPackage SSIS package
+"C:\Program Files\Microsoft SQL Server\140\DTS\Binn\DTEXEC.exe" /File "E:\inetpub\ftproot\vimfiles\vim_extract.dtsx" /config "E:\inetpub\ftproot\vimfiles\vim_config\VIMConfigFile_MW.dtsConfig"
