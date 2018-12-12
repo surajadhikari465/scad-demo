b136098d93d71f64b531d87e6b955c5384f47039
Min Zhao <min.zhao@wholefoods.com>
2018-12-12 17:46:00 +0000

    Merged PR 1437: Updated the Item Vendor Lane layout
    Updated the Item Vendor Lane layout
    
    Related work items: #30676

--- IRMA/IRMA.DB/IRMA.DB/Schema/dbo/Tables/Tables1/VendorLaneExtract.sql
+++ IRMA/IRMA.DB/IRMA.DB/Schema/dbo/Tables/Tables1/VendorLaneExtract.sql
@@ -1,14 +1,17 @@
 ﻿CREATE TABLE [dbo].[VendorLaneExtract](
 	[UPC] [varchar](13) NULL,
+    [ITEM_KEY] [int] NULL,
+    [VIN] [varchar](20) NULL,
 	[STORE_NUMBER] [varchar](20) NULL,
 	[VENDOR_NUMBER] [varchar](10) NULL,
 	[VENDOR_NAME] [varchar](50) NULL,
-	[CASE_SIZE] [varchar](15) NULL,
-	[CASE_UOM] [varchar](25) NULL,
+	[VENDOR_CASE_SIZE] [varchar](15) NULL,
+	[VENDOR_CASE_UOM] [varchar](25) NULL,
 	[VENDOR_COST_UOM] [varchar](25) NULL,
 	[REG_COST] [varchar](25) NULL,
 	[RETAIL_UOM] [varchar](25) NULL,
-	[RETAIL_PACK] [varchar](15) NULL
+	[RETAIL_PACK] [varchar](15) NULL,
+    [PRIMARY_VENDOR] [bit] NULL
 ) ON [PRIMARY];
 
 GO
