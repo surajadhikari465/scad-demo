5075dc897ef929c17d484e9dd25727a49e3539cc
Amit Uppal <amit.uppal@wholefoods.com>
2019-01-14 19:49:52 +0000

    Merged PR 1514: create model for assignKitToLocaleUI
    create model for assignKitToLocaleUI
    
    Related work items: #26461

---
+++ KitBuilder/KitBuilder.DataAccess/Dto/AssignKitToLocaleDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KitBuilder.DataAccess.Dto
+{
+    public class AssignKitToLocaleDto
+    {
+        public int LocaleId { get; set; }
+        public int LocaleTypeId { get; set; }
+        public string LocaleName { get; set; }
+        public string LocaleAbbreviation { get; set; }
+        public int ParentLocaleId { get; set; }
+        public bool IsAssigned { get; set; }
+        public bool IsExcluded { get; set; }
+    }
+}
