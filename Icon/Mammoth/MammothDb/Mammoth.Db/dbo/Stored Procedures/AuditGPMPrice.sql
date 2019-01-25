f9d4f4f4495c3595bf6bee58a34bca9951e804fc
Lakshmi Priyanka <lakshmi.priyanka@avanade.com>
2019-01-25 16:47:56 +0000

    Merged PR 1541: Added AuditGpmPrice storedProc
    Added AuditGpmPrice storedProc
    
    Related work items: #30735

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/dbo/Stored Procedures/AuditGPMPrice.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE [dbo].[AuditGPMPrice]
+  @region varchar(2)
+AS
+BEGIN
+  SET NOCOUNT ON;
+ 
+  DECLARE @msg VARCHAR(500) = null,
+          @query NVARCHAR(Max) = null;
+ 
+  IF(NOT Exists(SELECT 1 FROM Regions WHERE Region = @region))
+  BEGIN
+    SET @msg = 'Invalid region specified ' + @region + '.';
+    RAISERROR (@msg, 16, 1);
+    RETURN;
+  END
+ 
+  SET @query = '
+    SELECT p.Region,
+               i.ScanCode,
+               l.ItemID,
+               l.BusinessUnitID,
+               Cast(p.StartDate as Date) StartDate,
+               p.EndDate,
+               p.Price,
+               p.PercentOff,
+               p.PriceType,
+               p.PriceTypeAttribute AS PriceReasonCode ,--price reason type modify
+               p.CurrencyCode,
+               p.Multiple,
+               p.TagExpirationDate,
+               p.InsertDateUtc,
+               p.ModifiedDateUtc,
+               l.Authorized,
+               p.SellableUOM
+      FROM [gpm].[Price_'+ @Region+ '] as p
+      JOIN [dbo].[ItemAttributes_Locale_'+ @Region+'] l ON  p.ItemID = l.ItemID 
+      JOIN [dbo].[Items] i ON    p.ItemID = i.ItemID' 
+ 
+  EXEC sp_executesql @query;
+  SET NOCOUNT OFF;
+END
