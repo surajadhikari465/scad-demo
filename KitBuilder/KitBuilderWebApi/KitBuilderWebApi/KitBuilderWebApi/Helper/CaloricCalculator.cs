56c6feaae5dc68107b277da992562e742f9369d8
Amit Uppal <2096654@wholefoods.com>
2019-01-28 15:39:54 -0600

    latest code

--- KitBuilder/KitBuilderWebApi/KitBuilderWebApi/KitBuilderWebApi/Helper/CaloricCalculator.cs
+++ KitBuilder/KitBuilderWebApi/KitBuilderWebApi/KitBuilderWebApi/Helper/CaloricCalculator.cs
@@ -9,54 +9,86 @@ using System.Net.Http;
 using KitBuilder.DataAccess.Repository;
 using KitBuilder.DataAccess.DatabaseModels;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace KitBuilderWebApi.Helper
 {
-	public class CaloricCalculator 
-	{
-		private int kitLocaleId;
-		private int storeLocaleId;
-		private IRepository<KitLocale> kitLocaleRepository;
-		private ILogger<CaloricCalculator> logger;
+    public class CaloricCalculator
+    {
+        private int kitLocaleId;
+        private int storeLocaleId;
+        private IRepository<KitLocale> kitLocaleRepository;
+        private ILogger<CaloricCalculator> logger;
 
-		public CaloricCalculator(int kitLocaleId, int storeLocaleId, IRepository<KitLocale> kitLocaleRepository, ILogger<CaloricCalculator> logger)
-		{
-			this.logger = logger;
-			this.kitLocaleId = kitLocaleId;
-			this.storeLocaleId = storeLocaleId;
-			this.kitLocaleRepository =  kitLocaleRepository;
+        public CaloricCalculator(int kitLocaleId, int storeLocaleId, IRepository<KitLocale> kitLocaleRepository, ILogger<CaloricCalculator> logger)
+        {
+            this.logger = logger;
+            this.kitLocaleId = kitLocaleId;
+            this.storeLocaleId = storeLocaleId;
+            this.kitLocaleRepository = kitLocaleRepository;
+        }
 
-	}
+        public async Task<KitLocaleDto> Run()
+        {
+            KitLocaleDto kitLocaledto = GetKitByKitLocaleId(kitLocaleId);
+            var scanCode = from k in kitLocaledto.KitLinkGroupLocale
+                          .SelectMany(s => s.KitLinkGroupItemLocales.ToList())
+                          .Select(r => r.KitLinkGroupItem.LinkGroupItem.Item.ScanCode)
+                          select k;
+            int businessUnitID = 10;
+            List<StoreItem> storeItemsList = new List<StoreItem>();
 
-		public KitLocale Run()
-		{
-			KitLocale kitLocale = GetKitByKitLocaleId(kitLocaleId);
-			kitLocale.KitLinkGroupLocale
-			return  GetKitByKitLocaleId(kitLocaleId);
-		}
+            foreach (string scancode in scanCode)
+            {
+                storeItemsList.Add(new StoreItem()
+                {
+                    BusinessUnitId = businessUnitID,
+                    ScanCode = scancode
+                }
 
-		internal KitLocale GetKitByKitLocaleId(int kitLocaleId)
-		{
-			return (kitLocaleRepository.UnitOfWork.Context.KitLocale.Where(kl => kl.KitLocaleId == kitLocaleId)
-					.Include(kll => kll.KitLinkGroupLocale).ThenInclude(k => k.KitLinkGroupItemLocale)).FirstOrDefault(); 
-		}
+                );
+            }
 
-		internal async Task<IEnumerable<ItemStorePriceModel>> GetAuthorizedStatus(ICollection<StoreItem> storeItems)
-		{
-			string url = "http://mammoth-test/api/price/";
+            var result = GetAuthorizedStatus(storeItemsList);
+            var itemStorePriceModelList = await result;
+            
+            //update kit locale
+            foreach(ItemStorePriceModel itemStorePriceModel in itemStorePriceModelList)
+            {
+                var kitLinkGroupItemDtos = kitLocaledto.KitLinkGroupItemLocale.Where(s => s.KitLinkGroupItem.LinkGroupItem.ItemId == itemStorePriceModel.ItemId).Select(s=>s.KitLinkGroupItem;
+                foreach(KitLinkGroupItemDto kitLinkGroupItemDto in kitLinkGroupItemDtos)
+                {
+                    // kitLinkGroupItemDto.   itemStorePriceModel.Price;
+                }
+            }
+        }
 
-			using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
-			{
-				if (response.IsSuccessStatusCode)
-				{
-					IEnumerable<ItemStorePriceModel> itemStorePrices = await response.Content.ReadAsAsync<IEnumerable<ItemStorePriceModel>>();
-					return itemStorePrices;
-				}
-				else
-				{
-					throw new Exception(response.ReasonPhrase);
-				}
-			}
-		}
-	}
+        internal KitLocaleDto GetKitByKitLocaleId(int kitLocaleId)
+        {
+           var kitLocale= ((kitLocaleRepository.UnitOfWork.Context.KitLocale.Where(kl => kl.KitLocaleId == kitLocaleId)
+                    .Include(kll => kll.KitLinkGroupLocale).ThenInclude(k => k.KitLinkGroupItemLocale)
+                    .ThenInclude(i => i.KitLinkGroupItem).ThenInclude(i => i.LinkGroupItem)
+                    .ThenInclude(i => i.Item)).FirstOrDefault());
+
+            return Mapper.Map<KitLocaleDto>(kitLocale);
+        }
+
+        internal async Task<IEnumerable<ItemStorePriceModel>> GetAuthorizedStatus(ICollection<StoreItem> storeItems)
+        {
+            string url = "http://mammoth-test/api/price/";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    IEnumerable<ItemStorePriceModel> itemStorePrices = await response.Content.ReadAsAsync<IEnumerable<ItemStorePriceModel>>();
+                    return itemStorePrices;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
 }
