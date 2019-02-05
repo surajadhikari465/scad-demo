f121f3d0c854da8e39025099d38488cb08a2bdae
Min Zhao <Min.Zhao@wholefoods.com>
2019-02-05 14:53:18 -0600

    Added/Updated KitBuilder data models for caloric calculation.

---
+++ KitBuilder/KitBuilder.DataAccess/Dto/ItemNutritionAttributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KitBuilder.DataAccess.Dto
+{
+	public class ItemNutritionAttributes
+	{
+		public int ItemId { get; set; }
+		public int Calories { get; set; }
+		public decimal ServingsPerPortion { get; set; }
+		public string ServingSizeDesc { get; set; }
+		public string ServingPerContainer { get; set; }
+	}
+}
