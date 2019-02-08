7abdfd5b704a17a3ffb3217a3331565b542a86b4
Victor Lusenko <2172772@wholefoods.com>
2019-02-08 15:02:57 +0000

    Merged PR 42: Added predeployment script to dedup ItemAttributes_Locale_xx tables.
    Added predeployment script to dedup ItemAttributes_Locale_xx tables.
    
    Related work items: #4530

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PreDeploymentScripts/Deduplicate_ItemAttributesLocale_Tables.sql
@@ -0,0 +1,35 @@
+﻿SET NOCOUNT ON;
+DECLARE @sql nvarchar(max),
+        @region nvarchar(2),
+        @name varchar(255),
+        @msg varchar(255);
+
+DECLARE cur CURSOR FOR
+	SELECT DISTINCT Region FROM Regions
+OPEN cur   
+FETCH NEXT FROM cur INTO @region
+
+WHILE @@FETCH_STATUS = 0  
+BEGIN
+  SET @name = 'ItemAttributes_Locale_' + @region;
+  if(object_id(@name) is not null)
+  BEGIN
+    SET @msg = 'Processing ' + @name + ': Deduplicate ItemID-BusinessUnitID records...';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
+
+    SET @sql = ';with cte as(select ItemAttributeLocaleID, Row_Number()over(partition by ItemID, BusinessUnitID order by ItemAttributeLocaleID) rowID FROM ' + @name +')
+                DELETE A FROM ' + @name + ' A INNER JOIN cte B on B.ItemAttributeLocaleID = A.ItemAttributeLocaleID WHERE B.rowID > 1;';
+    EXEC(@sql);
+  END
+  ELSE
+  BEGIN
+    SET @msg = 'Table ' + @name + ' does not exist!';
+    RAISERROR(@msg, 0, 1) WITH NOWAIT;
+  END
+
+  FETCH NEXT FROM cur INTO @region;
+END
+
+DEALLOCATE cur;
+
+PRINT 'Deduplication completed.';
--- Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PreDeploymentScripts/Script.PreDeployment.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PreDeploymentScripts/Script.PreDeployment.sql
@@ -13,3 +13,4 @@
 -- Please add a date-added comment to the end of your line, to help with tracking, maintenance, and archiving.
 
 -- EXAMPLE :r .\TruncateItemLocaleStagingTable.sql -- 2018.02.27
+:r .\Deduplicate_ItemAttributesLocale_Tables.sql  --PBI 4530 2019-02-08
