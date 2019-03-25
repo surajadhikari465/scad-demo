c7865757d72a3635470443d66dd43d97270a3fd7
Victor Lusenko <2172772@wholefoods.com>
2019-03-25 15:03:22 +0000

    Merged PR 255: Refactored DeleteNutriFactsFromIcon procedure.
    Refactored DeleteNutriFactsFromIcon procedure.
    - Set values in FK columns to NULL before removing records.
    - Insert deleted nutrition records into PLUMCorpChgQueue
    
    Related work items: #4817

--- IRMA/IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs1/DeleteNutriFactsFromIcon.sql
+++ IRMA/IRMA.DB/IRMA.DB/Schema/dbo/Stored Procedures/Procs1/DeleteNutriFactsFromIcon.sql
@@ -4,23 +4,31 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	IF (object_id('tempdb..#scanCodes') IS NOT NULL)
+	IF (object_id('tempdb..#itemKeys') IS NOT NULL)
+		DROP TABLE #itemKeys;
+
+  IF (object_id('tempdb..#scanCodes') IS NOT NULL)
 		DROP TABLE #scanCodes;
 
 	SELECT DISTINCT Identifier
 	INTO #ScanCodes
 	FROM @Identifiers;
 
+  SELECT DISTINCT A.Item_Key
+  INTO #itemKeys
+	FROM ItemIdentifier A
+	INNER JOIN #ScanCodes B ON B.Identifier = A.Identifier
+
 	BEGIN TRY
 		BEGIN TRANSACTION;
 
-		DELETE
-		FROM dbo.ItemNutrition
-		WHERE ItemKey IN (
-				SELECT DISTINCT A.Item_Key
-				FROM ItemIdentifier A
-				INNER JOIN #ScanCodes B ON B.Identifier = A.Identifier
-				);
+    UPDATE A SET Scale_Allergen_ID = NULL, Scale_Ingredient_ID = NULL
+    FROM dbo.ItemScale A
+    INNER JOIN #itemKeys B on B.Item_Key = A.Item_Key;
+
+		DELETE A
+		FROM dbo.ItemNutrition A
+    INNER JOIN #itemKeys B ON B.Item_Key = A.ItemKey
 
 		DELETE A
 		FROM dbo.NutriFactsChgQueue A
@@ -30,7 +38,7 @@ BEGIN
 		DELETE A
 		FROM dbo.NutriFacts A
 		INNER JOIN #ScanCodes B ON B.Identifier = A.Description;
-
+    
 		DELETE A
 		FROM dbo.Scale_Allergen A
 		INNER JOIN #ScanCodes B ON B.Identifier = A.Description;
@@ -38,6 +46,15 @@ BEGIN
 		DELETE A
 		FROM dbo.Scale_Ingredient A
 		INNER JOIN #ScanCodes B ON B.Identifier = A.Description;
+
+    INSERT INTO PLUMCorpChgQueue(Item_Key, ActionCode, Store_No)
+      SELECT DISTINCT C.Item_Key, 'C', D.Store_No
+      FROM #ScanCodes A 
+      INNER JOIN dbo.ItemIdentifier B ON B.Identifier = A.Identifier
+      INNER JOIN dbo.Item C ON C.Item_Key = B.Item_Key
+      INNER JOIN dbo.StoreItem D ON D.Item_Key = C.Item_Key
+      LEFT JOIN PLUMCorpChgQueue E ON E.Item_Key = C.Item_Key AND E.Store_No = D.Store_No AND E.ActionCode = 'C'
+      WHERE E.Item_Key IS NULL;
 
 		COMMIT TRANSACTION;
 	END TRY
@@ -47,14 +64,13 @@ BEGIN
 
 		ROLLBACK TRANSACTION;
 
-		RAISERROR (
-				@errorMessage
-				,16
-				,1
-				);
+		RAISERROR(@errorMessage, 16, 1);
 	END CATCH
 
-	IF (object_id('tempdb..#scanCodes') IS NOT NULL)
+	IF (object_id('tempdb..#itemKeys') IS NOT NULL)
+		DROP TABLE #itemKeys;
+
+  IF (object_id('tempdb..#scanCodes') IS NOT NULL)
 		DROP TABLE #scanCodes;
 
 	SET NOCOUNT OFF;
