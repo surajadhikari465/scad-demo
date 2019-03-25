0719bda9454ca9b0930149ccf0ca7bd6d187d45e
Min Zhao <1235868@wholefoods.com>
2019-03-25 22:51:15 +0000

    Merged PR 262: Remove obsolete SQL objects referenced in the Honeycrisp project.
    Related work items: #4883

---
+++ IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/PBI4883_RemoveObsoleteHCDBObjects.sql
@@ -0,0 +1,55 @@
+﻿IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[GetMessagesWithError]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[GetMessagesWithError]
+END
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[LockInventoryMessages]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[LockInventoryMessages]
+END
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[LockPurchaseOrderEvents]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[LockPurchaseOrderEvents]
+END
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[LockReceiptEvents]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[LockReceiptEvents]
+END
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[LockInventorySpoilage]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[LockInventorySpoilage]
+END
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[LockTransferOrderEvents]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[LockInventorySpoilage]
+END
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[amz].[SendErrorNotificationEmail]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [amz].[SendErrorNotificationEmail]
+END
