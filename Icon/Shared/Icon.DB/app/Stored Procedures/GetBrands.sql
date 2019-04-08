74bbf65119dfa991127282012a7dd4793d5010eb
Victor Lusenko <2172772@wholefoods.com>
2019-04-08 19:21:52 +0000

    Merged PR 345: Refactore app.Brands proc to return brand's new traits.
    Refactore app.Brands proc to return brand's new traits.
    
    Related work items: #14464

--- Icon/Shared/Icon.DB/app/Stored Procedures/GetBrands.sql
+++ Icon/Shared/Icon.DB/app/Stored Procedures/GetBrands.sql
@@ -1,32 +1,78 @@
-﻿CREATE PROCEDURE [app].[GetBrands]
+﻿--Formatted with PoorSQL
+
+CREATE PROCEDURE [app].[GetBrands]
 AS
 BEGIN
--- ==================================================
--- Variables
--- ==================================================
-DECLARE @brandHierarchyId int;
-DECLARE @brandAbbreviationTraitId int;
+	DECLARE @brandHierarchyId INT
+		,@brandAbbreviationTraitId INT
+		,@desegnationID INT
+		,@parentCompanyID INT
+		,@zipCodeID INT
+		,@localityID INT
 
-SET @brandHierarchyId = (SELECT hierarchyID FROM Hierarchy WHERE hierarchyName = 'Brands');
-SET @brandAbbreviationTraitId = (SELECT traitID FROM Trait WHERE traitDesc = 'Brand Abbreviation');
+	IF (object_id('tempdb..#brands') IS NOT NULL)
+		DROP TABLE #brands;
 
--- ==================================================
--- Main
--- ==================================================
-SELECT
-	hc.hierarchyClassID			as HierarchyClassID,
-	hc.hierarchyClassName		as HierarchyClassName,
-	hc.hierarchyID				as HierarchyID,
-	hc.hierarchyLevel			as HierarchyLevel,
-	hc.hierarchyParentClassId	as HierarchyParentClassId,
-	hct.traitValue				as BrandAbbreviation
-FROM
-	HierarchyClass hc
-	LEFT JOIN HierarchyClassTrait hct on hc.hierarchyClassID = hct.hierarchyClassID
-										 AND hct.traitID = @brandAbbreviationTraitId
-WHERE
-	hc.hierarchyID = @brandHierarchyId
-ORDER BY
-	hc.hierarchyClassName
+	SET @brandHierarchyId = (SELECT [hierarchyID]	FROM Hierarchy WHERE hierarchyName = 'Brands');
+	SET @brandAbbreviationTraitId = (SELECT traitID	FROM Trait WHERE traitDesc = 'Brand Abbreviation');
+	SET @desegnationID = (SELECT traitID FROM Trait	WHERE traitDesc = 'Designation');
+	SET @parentCompanyID = (SELECT traitID FROM Trait WHERE traitDesc = 'Parent Company');
+	SET @zipCodeID = (SELECT traitID FROM Trait	WHERE traitDesc = 'Zip Code');
+	SET @localityID = (SELECT traitID	FROM Trait WHERE traitDesc = 'Locality');
+
+	SELECT A.hierarchyClassID
+		,B.traitValue AS BrandAbbreviation
+		,cast(NULL AS VARCHAR(255)) AS Designation
+		,cast(NULL AS VARCHAR(255)) AS ParentCompany
+		,cast(NULL AS VARCHAR(255)) AS ZipCode
+		,cast(NULL AS VARCHAR(255)) AS Locality
+	INTO #brands
+	FROM HierarchyClass A
+	LEFT JOIN HierarchyClassTrait B ON B.hierarchyClassID = A.hierarchyClassID
+		AND B.traitID = @brandAbbreviationTraitId
+	WHERE A.HIERARCHYID = @brandHierarchyId;
+
+	CREATE INDEX ix_id ON #brands (HierarchyClassID);
+
+	UPDATE A
+	SET Designation = B.traitValue
+	FROM #brands A
+	LEFT JOIN HierarchyClassTrait B ON B.hierarchyClassID = A.hierarchyClassID
+	WHERE B.traitID = @desegnationID;
+
+	UPDATE A
+	SET ParentCompany = B.traitValue
+	FROM #brands A
+	LEFT JOIN HierarchyClassTrait B ON B.hierarchyClassID = A.hierarchyClassID
+	WHERE B.traitID = @parentCompanyID;
+
+	UPDATE A
+	SET ZipCode = B.traitValue
+	FROM #brands A
+	LEFT JOIN HierarchyClassTrait B ON B.hierarchyClassID = A.hierarchyClassID
+	WHERE B.traitID = @zipCodeID;
+
+	UPDATE A
+	SET Locality = B.traitValue
+	FROM #brands A
+	LEFT JOIN HierarchyClassTrait B ON B.hierarchyClassID = A.hierarchyClassID
+	WHERE B.traitID = @localityID;
+
+	SELECT A.hierarchyClassID AS HierarchyClassID
+		,A.hierarchyClassName AS HierarchyClassName
+		,A.hierarchyID AS [HierarchyID]
+		,A.hierarchyLevel AS HierarchyLevel
+		,A.hierarchyParentClassId AS HierarchyParentClassId
+		,B.BrandAbbreviation
+		,B.Designation
+		,B.Locality
+		,B.ParentCompany
+		,B.ZipCode
+	FROM HierarchyClass A
+	INNER JOIN #brands B ON B.hierarchyClassID = A.hierarchyClassID
+	ORDER BY A.hierarchyClassName;
+
+	IF (object_id('tempdb..#brands') IS NOT NULL)
+		DROP TABLE #brands;
 END
 GO
