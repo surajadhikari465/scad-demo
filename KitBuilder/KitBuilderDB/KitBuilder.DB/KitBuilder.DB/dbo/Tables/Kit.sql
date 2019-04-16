b4bde88611a650bc18e7f683a2c6b7653a08e239
Richard Fornario <2195794@wholefoods.com>
2019-04-16 15:14:51 +0000

    Merged PR 368: Update KitBuilder DB, add fields to Kit table, and add KitType Table
    Related work items: #14437

--- KitBuilder/KitBuilderDB/KitBuilder.DB/KitBuilder.DB/dbo/Tables/Kit.sql
+++ KitBuilder/KitBuilderDB/KitBuilder.DB/KitBuilder.DB/dbo/Tables/Kit.sql
@@ -4,8 +4,12 @@
     [Description]        NVARCHAR (255) NULL,
     [InsertDateUtc]      DATETIME2 (7)  CONSTRAINT [DF_Kit_InsertDateUtc] DEFAULT (sysutcdatetime()) NOT NULL,
     [LastUpdatedDateUtc] DATETIME2 (7)  NULL,
+	[KitType]			 INT			NOT NULL,
+    [IsDisplayMandatory] BIT			CONSTRAINT [DF_Kit_IsDisplayMandatory] DEFAULT 0 NOT NULL,
+    [ShowRecipe]		 BIT			CONSTRAINT [DF_Kit_ShowRecipe] DEFAULT 0 NOT NULL,
     CONSTRAINT [PK_Kit] PRIMARY KEY CLUSTERED ([KitId] ASC),
-    CONSTRAINT [FK_Kit_Items] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Items] ([ItemId])
+    CONSTRAINT [FK_Kit_Items] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Items] ([ItemId]),
+	CONSTRAINT [FK_Kit_KitType] FOREIGN KEY ([KitType]) REFERENCES [dbo].[KitTypes] ([KitType])
 );
 
 
---
+++ KitBuilder/KitBuilderDB/KitBuilder.DB/KitBuilder.DB/dbo/Tables/KitTypes.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[KitTypes]
+(
+	[KitType] INT NOT NULL PRIMARY KEY, 
+    [Type] VARCHAR(50) NOT NULL
+)
