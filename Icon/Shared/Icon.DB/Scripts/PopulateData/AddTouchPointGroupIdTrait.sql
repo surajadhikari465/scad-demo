cf477680adef380b0fd703b1d2289c4cb142b38d
Richard Fornario <2195794@wholefoods.com>
2019-05-08 18:42:58 +0000

    Merged PR 549: Set Identity Insert on Trait not TraitGroup ** 1 LINER :P
    Related work items: #14513

--- Icon/Shared/Icon.DB/Scripts/PopulateData/AddTouchPointGroupIdTrait.sql
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/AddTouchPointGroupIdTrait.sql
@@ -1,6 +1,6 @@
 ﻿USE [IconDev]
 GO
-SET IDENTITY_INSERT [dbo].[TraitGroup] ON
+SET IDENTITY_INSERT [dbo].[Trait] ON
 GO
 IF NOT EXISTS(SELECT 1 FROM dbo.Trait WHERE traitCode= 'TPG')
 	INSERT [dbo].[Trait] ([traitID], [traitCode], [traitPattern], [traitDesc], [traitGroupID]) VALUES (215, N'TPG', N'', N'TouchPoint Group Id', 5)
