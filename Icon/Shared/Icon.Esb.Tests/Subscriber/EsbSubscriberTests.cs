c3c35579bd207d72bcd0042328ea98b983177943
Ed Mcnab <ed.mcnab@wholefoods.com>
2019-07-02 19:37:54 +0000

    Merged PR 806: PBI 8222 Permanent TEST ESB configuration update and test by July 2nd
    updating Test ESB server in configs
    updated ESB certificate info in configs
    
    Related work items: #8222

--- Icon/Shared/Icon.Esb.Tests/Subscriber/EsbSubscriberTests.cs
+++ Icon/Shared/Icon.Esb.Tests/Subscriber/EsbSubscriberTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
