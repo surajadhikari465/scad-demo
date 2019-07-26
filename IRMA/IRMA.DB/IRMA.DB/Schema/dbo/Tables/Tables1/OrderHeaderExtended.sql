ce5e4e1b0f0806cc0ab3c024698684b6fd4230d0
Min Zhao <1235868@wholefoods.com>
2019-07-26 16:49:03 +0000

    Merged PR 964: Added a permission grant statement for the HC Publish Receipt message.
    Added a permission grant statement for the HC Publish Receipt message.
    
    Related work items: #5467

--- IRMA/IRMA.DB/IRMA.DB/Schema/dbo/Tables/Tables1/OrderHeaderExtended.sql
+++ IRMA/IRMA.DB/IRMA.DB/Schema/dbo/Tables/Tables1/OrderHeaderExtended.sql
@@ -5,4 +5,10 @@
     [InsertDate] DATETIME2 NULL CONSTRAINT DF_OrderHeaderExtended_InsertDate DEFAULT (GetDate()),
     [LastUpdatedDate] DATETIME2 NULL, 
     CONSTRAINT [FK_OrderHeaderExtended_OrderHeader] FOREIGN KEY (OrderHeader_ID) REFERENCES [OrderHeader]([OrderHeader_ID])
-)
+);
+
+GO
+
+GRANT SELECT
+    ON OBJECT::[dbo].[OrderHeaderExtended] TO TibcoDataWriter
+    AS [dbo];
