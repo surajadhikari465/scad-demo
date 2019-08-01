b7a8b8cc72f2b4a64d63fc49c7f3a431db9dd3d0
Robin Eudy <Robin.Eudy@wholefoods.com>
2019-08-01 15:19:25 +0000

    Merged PR 998: shrinktype: rename RECALL to PRIME NOW RETURNS
    shrinktype: rename RECALL to PRIME NOW RETURNS
    
    Related work items: #23075

---
+++ IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/PBI23075_UpdateShrinkReasonCode_PrimeNowReturns.sql
@@ -0,0 +1,6 @@
+﻿RAISERROR('Updating ''Recall'' ShrinkSubType to be ''Prime Now Returns''', 0, 1) WITH NOWAIT
+
+UPDATE	dbo.ShrinkSubType
+SET		ReasonCodeDescription = 'Prime Now Returns',
+		LastUpdateDateTime = getdate()
+WHERE	ReasonCodeDescription = 'Recall'
