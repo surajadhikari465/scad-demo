f85665c530ae103dc7860c5aa1bc59a3aae7316e
Tom Lux <7005541@wholefoods.com>
2019-08-23 15:04:58 +0000

    Merged PR 1144: Release 2019.09 Setup: IRMA DB archive & version, client version, reldoc
    - IRMA DB: Archived 2019.08 pop-data.  Incremented version.
    - QA reldoc values
    - Increment IRMA Client version to 11.5 for fast-track that came in.  Added client and DBs to azure-deploy list in reldoc.
    
    Cherry picked from !1143
    
    Related work items: #5481

--- IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
+++ IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
@@ -1,9 +1,8 @@
 print '[' + convert(nvarchar, getdate(), 121) + '] ' + 'Start: [UpdateVersion.sql]'
 go
 
-RAISERROR ('Updating Version table to 11.4.0', 10,1) with nowait
+RAISERROR ('Updating Version table to 11.5.0', 10,1) with nowait
 UPDATE [Version]
-SET [Version] = '11.4.0'
---where applicationname in ('IRMA Client', 'DATABASE', 'SYSTEM')
-where applicationname in ('DATABASE', 'SYSTEM')
+SET [Version] = '11.5.0'
+where applicationname in ('IRMA Client', 'DATABASE', 'SYSTEM')
 GO
