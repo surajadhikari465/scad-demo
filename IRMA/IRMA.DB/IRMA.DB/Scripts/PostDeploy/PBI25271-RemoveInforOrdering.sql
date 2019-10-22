531d27ba423dfc23d3dd28c52d6acea76f819938
Min Zhao <1235868@wholefoods.com>
2019-10-22 19:44:38 +0000

    Merged PR 1686: Updated the data pop script to fix a bug.
    Updated the data pop script to fix a bug.
    
    Related work items: #25271

--- IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/PBI25271-RemoveInforOrdering.sql
+++ IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/PBI25271-RemoveInforOrdering.sql
@@ -1,5 +1,6 @@
 DECLARE @app_name varchar(50)		= 'IRMA CLIENT'
 DECLARE @ConfigKey varchar(150)     = 'WFMBannerStoresForOrdering'
+DECLARE @ConfigKeyId varchar(150)   = 'WFMBannerStoresForOrdering'
 DECLARE @IDFKey varchar(150)        = 'Include365StoresForInforOrdering'
 DECLARE @User_ID  int               = 0
 
@@ -10,10 +11,12 @@ FROM [dbo].[AppConfigValue] acv
 	WHERE aca.[Name]=@app_name
 		  AND (ack.[Name] = @ConfigKey)
 
+SELECT @ConfigKeyId = (SELECT KeyID from AppConfigKey where Name = @ConfigKey)
+
 SELECT @User_ID = ISNULL((SELECT user_id FROM Users WHERE UserName = 'System'),0)
 
 IF EXISTS(SELECT * FROM AppConfigKey WHERE Name = @ConfigKey AND Deleted = 0)
-    EXEC AppConfig_RemoveKey null, null, @ConfigKey, @User_ID
+    EXEC AppConfig_RemoveKey null, null, @ConfigKeyId, @User_ID
 
 DELETE dbo.InstanceDataFlags
 	WHERE FlagKey = @IDFKey
