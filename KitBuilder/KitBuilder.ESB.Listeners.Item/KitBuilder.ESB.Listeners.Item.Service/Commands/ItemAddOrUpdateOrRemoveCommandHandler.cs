b82059bf40c874e16be1c956e418baceb56b469e
Min Zhao <1235868@wholefoods.com>
2019-11-07 19:32:41 +0000

    Merged PR 1886: Updated the sequeunce of parameters mapping when calling stored proc
    Updated the sequeunce of parameters mapping when calling stored proc dbo.ItemAddOrUpdate.
    
    Related work items: #27920

--- KitBuilder/KitBuilder.ESB.Listeners.Item/KitBuilder.ESB.Listeners.Item.Service/Commands/ItemAddOrUpdateOrRemoveCommandHandler.cs
+++ KitBuilder/KitBuilder.ESB.Listeners.Item/KitBuilder.ESB.Listeners.Item.Service/Commands/ItemAddOrUpdateOrRemoveCommandHandler.cs
@@ -80,9 +80,9 @@ namespace KitBuilder.ESB.Listeners.Item.Service.Commands
                     CustomerFriendlyDesc = i.CustomerFriendlyDescription,
                     KitchenDesc = i.KitchenDescription,
                     BrandName = i.BrandsHierarchyName,
-					PosDesc = i.PosDescription,
                     ImageUrl = i.ImageUrl,
-					FlexibleText = i.FlexibleText
+					FlexibleText = i.FlexibleText,
+					PosDesc = i.PosDescription
 				}).ToTvp("items", "dbo.ItemAddOrUpdateType");
 
             repo.UnitOfWork.Context.Database.ExecuteSqlCommand("exec dbo.ItemAddOrUpdate @items", items);
