2785c3defac53931fae0134cc42ffad34851160e
Min Zhao <1235868@wholefoods.com>
2019-12-03 15:26:04 +0000

    Merged PR 2254: Added a script to clean up the AvailablePluNumber table.
    Added a script to clean up the AvailablePluNumber table.
    
    Related work items: #29024

---
+++ KitBuilder/KitBuilderDB/KitBuilder.DB/KitBuilder.DB/Scripts/PostDeploymentScripts/CleanUpAvailablePluNumberTable.sql
@@ -0,0 +1,12 @@
+﻿declare @PluTable as table (PluNumber int)
+insert into @PluTable
+select PluNumber 
+from [dbo].[InstructionList] il
+join [dbo].[InstructionListMember] ilm on il.InstructionListId = ilm.InstructionListId
+
+update apn
+set apn.InUse = 0
+from [dbo].[AvailablePluNumber] apn
+left join @PluTable pt on apn.PluNumber = pt.PluNumber
+where pt.PluNumber is null
+and InUse = 1
