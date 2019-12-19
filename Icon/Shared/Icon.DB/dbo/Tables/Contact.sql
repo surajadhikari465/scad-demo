62bf69d343697d466b7f3c96092c4d253b6d38f2
Vic Lusenko <2172772@wholefoods.com>
2019-12-19 20:56:45 +0000

    Merged PR 2540: 24672: Added 3 new tables in Icon DB
    Added new tables to Icon DB:
      dbo.Contact
      dbo.ContactType
      dbo.HierarchyContact
    
    Related work items: #24672

---
+++ Icon/Shared/Icon.DB/dbo/Tables/Contact.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Contact]
+(
+    [ContactId] INT NOT NULL CONSTRAINT pk_Contact PRIMARY KEY, 
+    [ContactTypeId] INT NOT NULL CONSTRAINT fk_Contact_ContactType FOREIGN KEY REFERENCES dbo.ContactType(ContactTypeId), 
+    [ContactName] NVARCHAR(255) NOT NULL,
+    [Email] NVARCHAR(255) NOT NULL,
+    [Title] NVARCHAR(255) NULL,
+    [AddressLine1] NVARCHAR(255) NULL,
+    [AddressLine2] NVARCHAR(255) NULL,
+    [City] NVARCHAR(255) NULL,
+    [State] NVARCHAR(255) NULL,
+    [ZipCode] NVARCHAR(15) NULL,
+    [Country] NVARCHAR(255) NULL,
+    [PhoneNumber1] NVARCHAR(30) NULL,
+    [PhoneNumber2] NVARCHAR(30) NULL,
+    [WebsiteURL] NVARCHAR(30) NULL,
+)
---
+++ Icon/Shared/Icon.DB/dbo/Tables/ContactType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ContactType]
+(
+    [ContactTypeId] INT NOT NULL CONSTRAINT pk_ContactType PRIMARY KEY, 
+    [ContactTypeName] NVARCHAR(255) NOT NULL CONSTRAINT ix_ContactTypeName UNIQUE,
+    [Archived] BIT NOT NULL CONSTRAINT df_HierarchyContact_IsActive DEFAULT 1
+)
