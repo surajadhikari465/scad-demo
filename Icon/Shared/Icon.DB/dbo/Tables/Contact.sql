5e0cdb9e09c49d227a6231c4523ec7e178c9e3b9
Vic Lusenko <2172772@wholefoods.com>
2019-12-26 21:01:05 +0000

    Merged PR 2610: Addede columns to dbo.Contact: HierarchyClassID, InsertedDate,ModifiedDate
    Addede columns to dbo.Contact:
      HierarchyClassID,
      InsertedDate,
      ModifiedDate
    
    Related work items: #24672

--- Icon/Shared/Icon.DB/dbo/Tables/Contact.sql
+++ Icon/Shared/Icon.DB/dbo/Tables/Contact.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Contact]
 (
-    [ContactId] INT NOT NULL IDENTITY CONSTRAINT pk_Contact PRIMARY KEY, 
+    [ContactId] INT NOT NULL IDENTITY CONSTRAINT pk_Contact PRIMARY KEY,
+    [HierarchyClassID] INT NOT NULL CONSTRAINT fk_Contact_HierarchyClass FOREIGN KEY REFERENCES dbo.HierarchyClass(hierarchyClassID), 
     [ContactTypeId] INT NOT NULL CONSTRAINT fk_Contact_ContactType FOREIGN KEY REFERENCES dbo.ContactType(ContactTypeId), 
     [ContactName] NVARCHAR(255) NOT NULL,
     [Email] NVARCHAR(255) NOT NULL,
@@ -14,4 +15,8 @@
     [PhoneNumber1] NVARCHAR(30) NULL,
     [PhoneNumber2] NVARCHAR(30) NULL,
     [WebsiteURL] NVARCHAR(255) NULL,
+    [InsertedDate] DateTime2(7) NOT NULL CONSTRAINT df_Contact_InsertDate DEFAULT GetDate(),
+    [ModifiedDate] DateTime2(7) NULL,
+
+    CONSTRAINT [ak_Contact] UNIQUE NONCLUSTERED(HierarchyClassID, ContactTypeID, Email ASC) WITH (FILLFACTOR = 80)
 )
