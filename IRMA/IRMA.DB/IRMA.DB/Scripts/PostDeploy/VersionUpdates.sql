678d1aa088508d6b353c8424bf40512351e691f6
Tom Lux <7005541@wholefoods.com>
2019-12-30 16:49:25 +0000

    Merged PR 2631: IRMA DB ver 11.8.
    IRMA DB ver 11.8.
    
    Cherry picked from !2630
    
    Related work items: #5531

--- IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
+++ IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/VersionUpdates.sql
@@ -1,8 +1,8 @@
 print '[' + convert(nvarchar, getdate(), 121) + '] ' + 'Start: [UpdateVersion.sql]'
 go
 
-RAISERROR ('Updating Version table to 11.7.0', 10,1) with nowait
+RAISERROR ('Updating Version table to 11.8.0', 10,1) with nowait
 UPDATE [Version]
-SET [Version] = '11.7.0'
+SET [Version] = '11.8.0'
 where applicationname in ('IRMA Client', 'DATABASE', 'SYSTEM')
 GO
