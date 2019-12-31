a529e8f81152e94b82dffd188f387bc8c5ec2d68
Blake Jones <2208158@wholefoods.com>
2019-12-31 21:13:28 +0000

    Merged PR 2648: 30057 Added Attributes and legacy Items audit scripts
    Added Attributes and legacy Items audit scripts
    
    Related work items: #30057

---
+++ Icon/Shared/Icon.DB/Scripts/Projects/IconReboot/Audit/Attributes.sql
@@ -0,0 +1,115 @@
+﻿SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+--Create temp tables for picklists and character sets
+IF OBJECT_ID('tempdb..#pickLists') IS NOT NULL DROP TABLE #pickLists
+CREATE TABLE #pickLists
+(
+	AttributeId INT,
+	PickListValues NVARCHAR(4000)
+)
+
+IF OBJECT_ID('tempdb..#characterSets') IS NOT NULL DROP TABLE #characterSets
+CREATE TABLE #characterSets
+(
+	AttributeId INT,
+	CharacterSets NVARCHAR(4000)
+)
+
+INSERT INTO #pickLists(AttributeId, PickListValues)
+SELECT a.AttributeId, ''
+FROM AttributesView a
+WHERE EXISTS (SELECT 1 FROM PickListData pld WHERE pld.AttributeId = a.AttributeId)
+
+INSERT INTO #characterSets(AttributeId, CharacterSets)
+SELECT a.AttributeId, ''
+FROM AttributesView a
+WHERE EXISTS (SELECT 1 FROM AttributeCharacterSets acs WHERE acs.AttributeId = a.AttributeId)
+
+--Populate temp tables
+DECLARE @AttributeId INT,
+		@PickListValue NVARCHAR(50),
+		@CharacterSet NVARCHAR(50)
+
+DECLARE pick_list_cursor CURSOR
+	FOR SELECT 
+			AttributeId, 
+			PickListValue 
+		FROM dbo.PickListData 
+		ORDER BY AttributeId, PickListValue
+OPEN pick_list_cursor
+
+FETCH NEXT FROM pick_list_cursor INTO @AttributeId, @PickListValue
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	UPDATE #pickLists
+	SET PickListValues = PickListValues + ', ' + '''' + REPLACE(@PickListValue, '''', '\''') + ''''
+	WHERE AttributeId = @AttributeId
+
+	FETCH NEXT FROM pick_list_cursor INTO @AttributeId, @PickListValue
+END
+
+CLOSE pick_list_cursor
+
+UPDATE #pickLists
+SET PickListValues = '[' + SUBSTRING(PickListValues, 3, LEN(PickListValues)) + ']'
+
+DECLARE character_set_cursor CURSOR
+	FOR SELECT 
+			AttributeId, 
+			cs.Name 
+		FROM dbo.AttributeCharacterSets acs
+		JOIN dbo.CharacterSets cs ON acs.CharacterSetId = cs.CharacterSetId
+OPEN character_set_cursor
+
+FETCH NEXT FROM character_set_cursor INTO @AttributeId, @CharacterSet
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	UPDATE #characterSets
+	SET CharacterSets = CharacterSets + ', ' + '''' + @CharacterSet + ''''
+	WHERE AttributeId = @AttributeId
+		
+
+	FETCH NEXT FROM character_set_cursor INTO @AttributeId, @CharacterSet
+END
+
+CLOSE character_set_cursor
+
+UPDATE ch
+SET CharacterSets = CharacterSets + ', ''SPECIAL'''
+FROM  #characterSets ch
+WHERE EXISTS (
+	SELECT 1 
+	FROM AttributesView a 
+	WHERE a.AttributeId = ch.AttributeId 
+		AND a.SpecialCharactersAllowed IS NOT NULL
+		)
+
+UPDATE #characterSets
+SET CharacterSets = '[' + SUBSTRING(CharacterSets, 3, LEN(CharacterSets)) + ']'
+
+select 
+	TraitCode,
+	AttributeName,
+	DisplayName,
+	dt.DataType,
+	a.Description,
+	a.DefaultValue,
+	ag.AttributeGroupName,
+	a.MaxLengthAllowed,
+	a.MinimumNumber,
+	a.MaximumNumber,
+	a.NumberOfDecimals,
+	a.IsRequired,
+	a.SpecialCharactersAllowed,
+	p.PickListValues,
+	c.CharacterSets,
+	a.DisplayOrder
+from Attributes a
+join AttributeGroup ag on a.AttributeGroupId = ag.AttributeGroupId
+join DataType dt on a.DataTypeId = dt.DataTypeId
+LEFT JOIN #pickLists p ON a.AttributeId = p.AttributeId
+LEFT JOIN #characterSets c ON a.AttributeId = c.AttributeId
+--where a.TraitCode = 'FSE'
+order by AttributeGroupName, a.AttributeId
---
+++ Icon/Shared/Icon.DB/Scripts/Projects/IconReboot/Audit/IconLegacy_Items.sql
@@ -0,0 +1,100 @@
+﻿set transaction isolation level read uncommitted
+
+declare @selectSql nvarchar(max) = '
+	SELECT i.itemId,
+	sc.scanCode,
+	brands.hierarchyClassID AS BrandHierarchyClassID,
+	brands.hierarchyClassName AS BrandHierarchyClassName,
+	merchandise.hierarchyClassID AS MerchandiseHierarchyClassID,
+	merchandise.hierarchyClassName AS MerchandiseHierarchyClassName,
+	tax.hierarchyClassID AS TaxHierarchyClassID,
+	tax.hierarchyClassName AS TaxHierarchyClassName,
+	nat.hierarchyClassID AS NationalHierarchyClassID,
+	nat.hierarchyClassName AS NationalHierarchyClassName,
+	financial.hierarchyClassID AS FinancialHierarchyClassID,
+	financial.hierarchyClassName AS FinancialHierarchyClassName,
+	isa.AnimalWelfareRating,
+	isa.Biodynamic,
+	isa.MilkType,
+	isa.CheeseRaw,
+	isa.EcoScaleRating,
+	isa.GlutenFreeAgencyName,
+	isa.KosherAgencyName,
+	isa.Msc,
+	isa.NonGmoAgencyName,
+	isa.OrganicAgencyName,
+	isa.PremiumBodyCare,
+	isa.FreshOrFrozen,
+	isa.SeafoodCatchType,
+	isa.VeganAgencyName,
+	isa.Vegetarian,
+	isa.WholeTrade,
+	isa.GrassFed,
+	isa.PastureRaised,
+	isa.FreeRange,
+	isa.DryAged,
+	isa.AirChilled,
+	isa.MadeInHouse,
+	isa.CustomerFriendlyDescription, '
+declare @fromSql nvarchar(max) = ' 
+	FROM item i
+JOIN ScanCode sc ON i.itemID = sc.itemID
+JOIN (
+	SELECT ihc.itemID
+		,hc.hierarchyClassID
+		,hc.hierarchyClassName
+	FROM Hierarchy h
+	JOIN HierarchyClass hc ON h.HIERARCHYID = hc.HIERARCHYID
+	JOIN ItemHierarchyClass ihc ON hc.hierarchyClassID = ihc.hierarchyClassID
+	WHERE h.hierarchyName = ''Brands''
+	) brands ON i.itemID = brands.itemID
+JOIN (
+	SELECT ihc.itemID
+		,hc.hierarchyClassID
+		,hc.hierarchyClassName
+	FROM Hierarchy h
+	JOIN HierarchyClass hc ON h.HIERARCHYID = hc.HIERARCHYID
+	JOIN ItemHierarchyClass ihc ON hc.hierarchyClassID = ihc.hierarchyClassID
+	WHERE h.hierarchyName = ''Merchandise''
+	) merchandise ON i.itemID = merchandise.itemID
+JOIN (
+	SELECT ihc.itemID
+		,hc.hierarchyClassID
+		,hc.hierarchyClassName
+	FROM Hierarchy h
+	JOIN HierarchyClass hc ON h.HIERARCHYID = hc.HIERARCHYID
+	JOIN ItemHierarchyClass ihc ON hc.hierarchyClassID = ihc.hierarchyClassID
+	WHERE h.hierarchyName = ''Tax''
+	) tax ON i.itemID = tax.itemID
+JOIN (
+	SELECT ihc.itemID
+		,hc.hierarchyClassID
+		,hc.hierarchyClassName
+	FROM Hierarchy h
+	JOIN HierarchyClass hc ON h.HIERARCHYID = hc.HIERARCHYID
+	JOIN ItemHierarchyClass ihc ON hc.hierarchyClassID = ihc.hierarchyClassID
+	WHERE h.hierarchyName = ''National''
+	) nat ON i.itemID = nat.itemID
+JOIN (
+	SELECT ihc.itemID
+		,hc.hierarchyClassID
+		,hc.hierarchyClassName
+	FROM Hierarchy h
+	JOIN HierarchyClass hc ON h.HIERARCHYID = hc.HIERARCHYID
+	JOIN ItemHierarchyClass ihc ON hc.hierarchyClassID = ihc.hierarchyClassID
+	WHERE h.hierarchyName = ''Financial''
+	) financial ON i.itemID = financial.itemID
+	LEFT JOIN ItemSignAttribute isa ON i.ItemID = isa.ItemId'
+	
+select @selectSql += (' ' + LOWER(t.traitCode) + '.traitValue AS [' + t.traitDesc + '],'),
+	@fromSql += (' left join dbo.ItemTrait ' + LOWER(t.traitCode) + ' on i.itemID = ' + LOWER(t.traitCode) + '.itemID and ' + LOWER(t.traitCode) + '.traitID = ' + cast(t.traitID as nvarchar(10)) + ' ')
+from Trait t
+where exists (
+		select top 1 1 from ItemTrait it
+		where it.traitID = t.traitID
+	)
+
+declare @sql nvarchar(max) = SUBSTRING(@selectSql, 1, LEN(@selectSql) - 1) + @fromSql
+
+select @sql
+--exec sp_executesql  @sql
