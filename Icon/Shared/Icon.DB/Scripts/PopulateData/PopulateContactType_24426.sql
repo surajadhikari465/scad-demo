e33c6ec8e0bdec633bb071d6f411528f4bce130a
Robin Eudy <Robin.Eudy@wholefoods.com>
2020-01-09 19:21:58 +0000

    Merged PR 2754: 30508 -- PopulateContactType_24426 datapopulate script was causing Icon DB build to fail
    corrected a data pop script that  was causing IconDB build to fail.
    - insert @scriptkey into history on completion.
    refactor data pop so values arent inserted if they already exist.
    
    Related work items: #30508

--- Icon/Shared/Icon.DB/Scripts/PopulateData/PopulateContactType_24426.sql
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/PopulateContactType_24426.sql
@@ -2,15 +2,25 @@
 
 IF (NOT EXISTS (SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @scriptKey))
 BEGIN
-    INSERT INTO dbo.ContactType(ContactTypeName)
-        VALUES('AR/AP'),
-              ('Chief Executive'),
-              ('Marketing'),
-              ('Operations Management'),
-              ('Product Safety Emergency Contact'),
-              ('QA'),
-              ('Sales Management'),
-              ('WFM Account Manager (Primary)')
+
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'AR/AP')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('AR/AP')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'Chief Executive')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('Chief Executive')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'Marketing')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('Marketing')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'Operations Management')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('Operations Management')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'Product Safety Emergency Contact')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('Product Safety Emergency Contact')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'QA')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('QA')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'Sales Management')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('Sales Management')
+IF NOT EXISTS (select 1 from dbo.ContactType where ContactTypeName = 'WFM Account Manager (Primary)')  
+	INSERT INTO dbo.ContactType ( ContactTypeName ) Values ('WFM Account Manager (Primary)')
+
+    INSERT INTO app.PostDeploymentScriptHistory (ScriptKey, RunTime) values (@scriptKey, GETDATE())
 END
 ELSE
 BEGIN
