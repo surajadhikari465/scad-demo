4d8192dfde0e8926bfbda36d40227914461aca6e
David Powers <david.powers@wholefoods.com>
2020-01-23 17:04:50 +0000

    Merged PR 2936: 31316 - Disable receiving document link in home page
    - Disable receiving document link in home page
    
    Related work items: #31316

--- IRMA/Projects/IrmaMobile/IrmaMobile/ClientApp/src/pages/Receive/Index/index.tsx
+++ IRMA/Projects/IrmaMobile/IrmaMobile/ClientApp/src/pages/Receive/Index/index.tsx
@@ -1,24 +1,35 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useContext } from 'react'
 import { Header, Segment } from "semantic-ui-react";
 import './styles.scss';
 import { Link, useHistory } from 'react-router-dom';
+import { types, AppContext } from '../../../store';
 
 const Receive: React.FC = () => {
+    //@ts-ignore
+    const { dispatch } = useContext(AppContext);
+
     let history = useHistory();
+
+    useEffect(() => {
+        dispatch({ type: types.SETTITLE, Title: 'IRMA Mobile' });
+        return () => {
+          dispatch({ type: types.SETTITLE, Title: 'IRMA Mobile' });
+        };
+      }, [dispatch]);
 
     return (
         <Fragment>
-            <Segment vertical>
+            <Segment vertical basic>
                 <Header as='h2' textAlign="center">Receive PO or Receiving Document</Header>
             </Segment>
-            <Segment vertical>
-                <button className='wfmButton receive-button'><Link to='/receive/PurchaseOrder'>Receive PO</Link></button>
+            <Segment vertical basic>
+                <button style={{height: '80px', width: '100%'}} className='wfmButton receive-button'><Link to='/receive/PurchaseOrder'>Receive PO</Link></button>
             </Segment>
-            <Segment vertical>
-                <button className='wfmButton receive-button'><Link to='/receive/Document'>Receiving Document</Link></button>
+            <Segment vertical basic>
+                <button disabled style={{height: '80px', backgroundColor: 'grey', width: '100%'}} className='wfmButton receive-button'>Receiving Document</button>
             </Segment>
-            <Segment vertical>
-                <button className='wfmButton receive-button' onClick={() => history.goBack()}>Cancel</button>
+            <Segment vertical basic>
+                <button style={{height: '80px', width: '100%'}} className='wfmButton receive-button' onClick={() => history.goBack()}>Cancel</button>
             </Segment>
         </Fragment>
     ) 
