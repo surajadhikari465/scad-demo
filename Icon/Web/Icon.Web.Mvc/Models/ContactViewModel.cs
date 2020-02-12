8fd32c14bc2d340eef13726121870e67f21ec3c5
Vic Lusenko <2172772@wholefoods.com>
2020-02-12 15:01:57 +0000

    Merged PR 3228: Updated labels.
    31417_Updated labels.
    
    Related work items: #31417

--- Icon/Web/Icon.Web.Mvc/Models/ContactViewModel.cs
+++ Icon/Web/Icon.Web.Mvc/Models/ContactViewModel.cs
@@ -40,7 +40,7 @@ namespace Icon.Web.Mvc.Models
         [Display(Name = "State")]
         public string State { get; set; }
 
-        [Display(Name = "ZIP Code")]
+        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
 
         [Display(Name = "Country")]
