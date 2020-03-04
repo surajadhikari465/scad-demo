c43bebb30f763aaa968f79a20c952d7ba7b9705d
Vic Lusenko <2172772@wholefoods.com>
2020-03-04 20:04:04 +0000

    Merged PR 3620: Fixed PopulateBulkItemUploadStatus script
    Fixed PopulateBulkItemUploadStatus script
    
    Related work items: #31793

--- Icon/Shared/Icon.DB/Scripts/PopulateData/PopulateBulkItemUploadStatus.sql
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/PopulateBulkItemUploadStatus.sql
@@ -1,7 +1,5 @@
 ﻿DECLARE @populateBulkItemUploadStatusScriptKey VARCHAR(128) = 'PopulateBulkItemUploadStatusKey'
 
-IF(NOT EXISTS(SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @populateBulkItemUploadStatusScriptKey))
-BEGIN
 	PRINT '[' + convert(nvarchar, getdate(), 121) + '] ' + @populateBulkItemUploadStatusScriptKey;	
 
 	set identity_insert dbo.BulkUploadStatus on
@@ -15,15 +13,10 @@ BEGIN
 	if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Complete')
 		insert into dbo.BulkUploadStatus (Id,Status) values (2,'Complete')
 
-	if not exists (select * from dbo.BulkItemUploadStatus where [Status] = 'Error')
+	if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Error')
 		insert into dbo.BulkUploadStatus (Id,Status) values (3,'Error')
 
 	set identity_insert dbo.BulkUploadStatus off
 
-	INSERT INTO app.PostDeploymentScriptHistory (ScriptKey, RunTime) values (@populateBulkItemUploadStatusScriptKey, GETDATE())
-END
-ELSE
-BEGIN
-	PRINT '[' + convert(nvarchar, getdate(), 121) + '] ' + 'Pop-data already applied: ' + @populateBulkItemUploadStatusScriptKey
-END
-GO
+    IF(NOT EXISTS(SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @populateBulkItemUploadStatusScriptKey))
+	  INSERT INTO app.PostDeploymentScriptHistory (ScriptKey, RunTime) values (@populateBulkItemUploadStatusScriptKey, GETDATE())
