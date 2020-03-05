d864ca682a27d32c7be53319db42b929716e45f3
Vic Lusenko <2172772@wholefoods.com>
2020-03-05 19:32:20 +0000

    Merged PR 3642: Added comment to fixed script
    33024 Added comment to fixed script
    
    Related work items: #33024

--- Icon/Shared/Icon.DB/Scripts/PopulateData/PopulateBulkItemUploadStatus.sql
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/PopulateBulkItemUploadStatus.sql
@@ -1,22 +1,23 @@
-﻿DECLARE @populateBulkItemUploadStatusScriptKey VARCHAR(128) = 'PopulateBulkItemUploadStatusKey'
+﻿--The script didn't populate 'Error' status due to incorrect table name.
+DECLARE @populateBulkItemUploadStatusScriptKey VARCHAR(128) = 'PopulateBulkItemUploadStatusKey'
 
-	PRINT '[' + convert(nvarchar, getdate(), 121) + '] ' + @populateBulkItemUploadStatusScriptKey;	
+PRINT '[' + convert(nvarchar, getdate(), 121) + '] ' + @populateBulkItemUploadStatusScriptKey;	
 
-	set identity_insert dbo.BulkUploadStatus on
+set identity_insert dbo.BulkUploadStatus on
 
-	if not exists (select * from dbo.BulkUploadStatus where [Status] = 'New')
-		insert into dbo.BulkUploadStatus (Id,Status) values (0,'New')
+if not exists (select * from dbo.BulkUploadStatus where [Status] = 'New')
+	insert into dbo.BulkUploadStatus (Id,Status) values (0,'New')
 
-	if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Processing')
-		insert into dbo.BulkUploadStatus (Id,Status) values (1,'Processing')
+if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Processing')
+	insert into dbo.BulkUploadStatus (Id,Status) values (1,'Processing')
 
-	if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Complete')
-		insert into dbo.BulkUploadStatus (Id,Status) values (2,'Complete')
+if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Complete')
+	insert into dbo.BulkUploadStatus (Id,Status) values (2,'Complete')
 
-	if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Error')
-		insert into dbo.BulkUploadStatus (Id,Status) values (3,'Error')
+if not exists (select * from dbo.BulkUploadStatus where [Status] = 'Error')
+	insert into dbo.BulkUploadStatus (Id,Status) values (3,'Error')
 
-	set identity_insert dbo.BulkUploadStatus off
+set identity_insert dbo.BulkUploadStatus off
 
-    IF(NOT EXISTS(SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @populateBulkItemUploadStatusScriptKey))
-	  INSERT INTO app.PostDeploymentScriptHistory (ScriptKey, RunTime) values (@populateBulkItemUploadStatusScriptKey, GETDATE())
+IF(NOT EXISTS(SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @populateBulkItemUploadStatusScriptKey))
+  INSERT INTO app.PostDeploymentScriptHistory (ScriptKey, RunTime) values (@populateBulkItemUploadStatusScriptKey, GETDATE())
