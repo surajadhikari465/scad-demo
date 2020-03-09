8ef0b57e642d13bb28325167557cb3de7b0f4cd1
Blake Jones <2208158@wholefoods.com>
2020-03-09 17:36:31 +0000

    Merged PR 3689: Changed Transfer to accept decimals for QuantityOrdered
    Changed Transfer to accept decimals for QuantityOrdered
    
    Related work items: #31966

--- IRMA/Projects/IrmaMobile/IrmaMobile/Domain/Models/TransferOrderItemModel.cs
+++ IRMA/Projects/IrmaMobile/IrmaMobile/Domain/Models/TransferOrderItemModel.cs
@@ -7,7 +7,7 @@ namespace IrmaMobile.Domain.Models
 {
     public class TransferOrderItemModel
     {
-        public int QuantityOrdered { get; set; }
+        public decimal QuantityOrdered { get; set; }
         public int ItemKey { get; set; }
         public int QuantityUnit { get; set; }
         public decimal AdjustedCost { get; set; }
