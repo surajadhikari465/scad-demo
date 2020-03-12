dccac94d637772c56227f2235b6948cf336a9a2e
Amit Uppal <2096654@wholefoods.com>
2020-03-12 16:17:19 +0000

    Merged PR 3741: 33385-scripts for kitbuilder metro move
    scripts for kitbuilder metro move
    
    Related work items: #33385

---
+++ KitBuilder/KitBuilderDB/KitBuilder.DB/KitBuilder.DB/Scripts/MoveBoiseAndMetro/MoveMetroToNC_RollBack.sql
@@ -0,0 +1,30 @@
+﻿USE KitBuilder
+
+DECLARE @metroNameToMove VARCHAR(20)
+DECLARE @destinationRegionCode VARCHAR(20)
+DECLARE @metroId INT
+DECLARE @destinationRegionId INT
+DECLARE @RegionLocaleTypeId INT
+
+SET @metroNameToMove = 'Met_Id'
+SET @destinationRegionCode = 'RM'
+SET @RegionLocaleTypeId = (
+		SELECT localeTypeId
+		FROM localeType
+		WHERE localeTypeCode = 'RG'
+		)
+SET @metroId = (
+		SELECT LocaleId
+		FROM Locale
+		WHERE Localename = @metroNameToMove
+		)
+SET @destinationRegionId = (
+		SELECT LocaleId
+		FROM Locale
+		WHERE RegionCode = @destinationRegionCode
+			AND LocaleTypeId = @RegionLocaleTypeId
+		)
+
+UPDATE locale
+SET RegionId = @destinationRegionId
+WHERE LocaleId = @metroId
