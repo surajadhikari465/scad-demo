b57f9ea8dd5c4a724beb91712ab3a4d9b6c2eebd
Shruthi Konudula <2196295@wholefoods.com>
2020-03-19 21:48:56 +0000

    Merged PR 3864: Update Attribute Model with a Flag
    Update Attribute Model with a Flag
    
    Related work items: #33672

--- Icon/Shared/Icon.Common/Models/AttributeModel.cs
+++ Icon/Shared/Icon.Common/Models/AttributeModel.cs
@@ -34,5 +34,6 @@ namespace Icon.Common.Models
         public string XmlTraitDescription { get; set; }
         public int? ItemCount { get; set; }
         public IEnumerable<AttributeItemCountModel> AttributeItemCounts { get; set; }
+        public bool IsItemCountOnAttributes { get; set; } 
     }
 }
