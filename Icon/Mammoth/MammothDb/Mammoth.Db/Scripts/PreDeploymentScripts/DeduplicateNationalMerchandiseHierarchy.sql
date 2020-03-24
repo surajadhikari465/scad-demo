b6605feaa3be4b08b883723611f1820425d64b68
Vic Lusenko <2172772@wholefoods.com>
2020-03-24 13:52:19 +0000

    Merged PR 3909: 32842 Refactored script by Tom's request: Include transaction/rollback and output
    Refactored script by Tom's request: Include transaction/rollback and output deleted records.
    
    Related work items: #32842

--- Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PreDeploymentScripts/DeduplicateNationalMerchandiseHierarchy.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PreDeploymentScripts/DeduplicateNationalMerchandiseHierarchy.sql
@@ -1,44 +1,85 @@
-﻿--32371_Deduplicate_Hierarchy_Merchandise
-SET NOCOUNT ON;
-DECLARE @ids AS TABLE(HierarchyClassID INT);
+﻿DECLARE @key VARCHAR(128) = 'DeduplicateNationalMerchandiseHierarchy';
 
---32842: Deduplicate Hierarchy_NationalClass
---Delete invalid records
-DELETE FROM dbo.Hierarchy_NationalClass
-WHERE FamilyHCID IS NULL
-  OR (CategoryHCID IS NULL AND SubcategoryHCID IS NULL AND ClassHCID IS NULL)
-  OR (CategoryHCID IS NULL AND SubcategoryHCID IS NOT NULL)
-  OR (SubcategoryHCID IS NULL AND ClassHCID IS NOT NULL);
+IF(NOT EXISTS(SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @key))
+BEGIN
+  SET NOCOUNT ON;
+  DECLARE @ids AS TABLE(HierarchyClassID INT);
+  
+  if(object_id('tempdb..#tmpNational') IS NOT NULL) drop table #tmpNational;
+  if(object_id('tempdb..#tmpMerchandise') IS NOT NULL) drop table #tmpMerchandise;
+  
+  BEGIN TRY
+    BEGIN TRANSACTION;
 
---Delete duplicate records
-;WITH cte AS(
-  SELECT HierarchyNationalClassID, Row_Number() OVER(PARTITION BY FamilyHCID, CategoryHCID, SubcategoryHCID, ClassHCID ORDER BY HierarchyNationalClassID ) rowID
-  FROM dbo.Hierarchy_NationalClass)
-    DELETE FROM cte where rowID > 1;
+    --32371: Deduplicate Hierarchy_NationalClass
+    --Delete invalid records
+    DELETE FROM dbo.Hierarchy_NationalClass
+    OUTPUT deleted.*
+    WHERE FamilyHCID IS NULL
+      OR (CategoryHCID IS NULL AND SubcategoryHCID IS NULL AND ClassHCID IS NULL)
+      OR (CategoryHCID IS NULL AND SubcategoryHCID IS NOT NULL)
+      OR (SubcategoryHCID IS NULL AND ClassHCID IS NOT NULL);
+    
+    --Delete duplicate records
+    ;WITH cte AS(
+      SELECT HierarchyNationalClassID, Row_Number() OVER(PARTITION BY FamilyHCID, CategoryHCID, SubcategoryHCID, ClassHCID ORDER BY HierarchyNationalClassID ) rowID
+      FROM dbo.Hierarchy_NationalClass)
+        SELECT * INTO #tmpNational FROM cte WHERE rowID > 1;
 
---Reset invalid Items.HierarchyNationalClassID
-UPDATE i SET HierarchyNationalClassID = NULL
-FROM dbo.Items i
-LEFT JOIN dbo.Hierarchy_NationalClass h ON h.HierarchyNationalClassID = i.HierarchyNationalClassID
-WHERE i.HierarchyNationalClassID IS NOT NULL AND h.HierarchyNationalClassID IS NULL;
+    DELETE hn
+    OUTPUT deleted.*
+    FROM dbo.Hierarchy_NationalClass hn
+    JOIN #tmpNational n on n.HierarchyNationalClassID = hn.HierarchyNationalClassID;
+    
+    --Reset invalid Items.HierarchyNationalClassID
+    UPDATE i SET HierarchyNationalClassID = NULL
+    FROM dbo.Items i
+    LEFT JOIN dbo.Hierarchy_NationalClass h ON h.HierarchyNationalClassID = i.HierarchyNationalClassID
+    WHERE i.HierarchyNationalClassID IS NOT NULL AND h.HierarchyNationalClassID IS NULL;
+    
+    
+    --32842_Deduplicate_Hierarchy_Merchandise
+    --Delete invalid records
+    DELETE FROM dbo.Hierarchy_Merchandise
+    OUTPUT deleted.*
+    WHERE SegmentHCID IS NULL
+      OR FamilyHCID IS NULL
+      OR ClassHCID IS NULL
+      OR BrickHCID IS NULL; --SubBrickHCID IS NULL; SubBrickHCID can be NULL. Verified with Ben and Kevin
+    
+    --Delete duplicate records
+    ;WITH cte AS(
+      SELECT HierarchyMerchandiseID, Row_Number() OVER(PARTITION BY SegmentHCID, FamilyHCID, ClassHCID, BrickHCID, SubBrickHCID ORDER BY HierarchyMerchandiseID) rowID
+      FROM dbo.Hierarchy_Merchandise)
+      SELECT * INTO #tmpMerchandise FROM cte WHERE rowID > 1;
+    
+    DELETE hm
+    OUTPUT deleted.*
+    FROM dbo.Hierarchy_Merchandise hm
+    JOIN #tmpMerchandise m on m.HierarchyMerchandiseID = hm.HierarchyMerchandiseID;
 
+    UPDATE i SET HierarchyMerchandiseID = NULL
+    FROM dbo.Items i
+    LEFT JOIN dbo.Hierarchy_Merchandise h ON h.HierarchyMerchandiseID = i.HierarchyMerchandiseID
+    WHERE i.HierarchyMerchandiseID IS NOT NULL AND h.HierarchyMerchandiseID IS NULL;
 
+    INSERT INTO app.PostDeploymentScriptHistory (ScriptKey, RunTime) VALUES (@key ,GETDATE());
+    COMMIT TRAN;
+  END TRY
+  BEGIN CATCH
+	  ROLLBACK TRAN;
 
---32842_Deduplicate_Hierarchy_Merchandise
---Delete invalid records
-DELETE FROM dbo.Hierarchy_Merchandise
-WHERE SegmentHCID IS NULL
-  OR FamilyHCID IS NULL
-  OR ClassHCID IS NULL
-  OR BrickHCID IS NULL; --SubBrickHCID IS NULL; SubBrickHCID can be NULL. Verified with Ben and Kevin
+      DECLARE @ErrorMessage NVARCHAR(4000);
+	  DECLARE @ErrorSeverity INT;
+	  DECLARE @ErrorState INT;
 
---Delete duplicate records
-;WITH cte AS(
-  SELECT HierarchyMerchandiseID, Row_Number() OVER(PARTITION BY SegmentHCID, FamilyHCID, ClassHCID, BrickHCID, SubBrickHCID ORDER BY HierarchyMerchandiseID) rowID
-  FROM dbo.Hierarchy_Merchandise)
-  DELETE FROM cte where rowID > 1;
-
-UPDATE i SET HierarchyMerchandiseID = NULL
-FROM dbo.Items i
-LEFT JOIN dbo.Hierarchy_Merchandise h ON h.HierarchyMerchandiseID = i.HierarchyMerchandiseID
-WHERE i.HierarchyMerchandiseID IS NOT NULL AND h.HierarchyMerchandiseID IS NULL;
+	 SELECT 
+	    @ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+  END CATCH
+END
+ELSE
+BEGIN
+	PRINT '[' + convert(NVARCHAR, GetDate(), 121) + '] Script ' + @key + ' already applied.'
+END
