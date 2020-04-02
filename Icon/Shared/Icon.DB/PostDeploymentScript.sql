82094b89ae4d2a4f36e171c58653017611bc4e3c
Amit Uppal <2096654@wholefoods.com>
2020-04-02 01:09:27 +0000

    Merged PR 4041: add appid, nlog configuration. tested it in dev and it logs in applog table.
    add appid, nlog configuration. tested it in dev .It logs in applog table.
    
    Related work items: #32993

--- Icon/Shared/Icon.DB/PostDeploymentScript.sql
+++ Icon/Shared/Icon.DB/PostDeploymentScript.sql
@@ -15,6 +15,6 @@ EXAMPLE (showing Icon-DB scripts path):
 
 :r .\Scripts\PopulateData\32769_UpdateSubTeamName.sql -- 2020-03-12 32769
 :r .\Scripts\PopulateData\RetentionPolicyContactUpload.sql --2020-03-20 PBI: 32330
-
+:r .\Scripts\PopulateData\AddBulkItemUploadToAppTable.sql --2020-03-20 PBI: 32330
 -- Always run this security script and make it the last entry in this script.
 :r .\Security\Icon.Security.sql
