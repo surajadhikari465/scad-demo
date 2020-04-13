039fb63dac317675a782c5cf8a5276d23084d639
Robin Eudy <Robin.Eudy@wholefoods.com>
2020-04-13 16:20:01 +0000

    Merged PR 4177: 34783 handle tibco illegalstateexception on Stop()
    handle tibco illegalstateexception on Stop()
    
    Related work items: #34783

--- Icon/Services/Services.Extract/Services.Extract/ExtractService.cs
+++ Icon/Services/Services.Extract/Services.Extract/ExtractService.cs
@@ -1,13 +1,18 @@
-﻿namespace Services.Extract
+﻿using System;
+using Icon.Logging;
+
+namespace Services.Extract
 {
     public class ExtractService
     {
 
         private readonly ExtractServiceListener ListenerApplication;
+        private ILogger<ExtractService> Logger;
 
-        public ExtractService(ExtractServiceListener listenerApplication)
+        public ExtractService(ExtractServiceListener listenerApplication, ILogger<ExtractService> logger)
         {
             ListenerApplication = listenerApplication;
+            Logger = logger;
         }
 
         public void Start()
@@ -19,7 +24,18 @@
 
         public void Stop()
         {
-            ListenerApplication.Close();
+            try
+            {
+                ListenerApplication.Close();
+            }
+            catch (TIBCO.EMS.IllegalStateException tibcoEx)
+            {
+                Logger.Warn($"tibco: {tibcoEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Warn(ex.Message);
+            }
         }
     }
 }
