7ac63d01b3d5ca0d727bd17913ffffe223288e9a
Blake Jones <2208158@wholefoods.com>
2020-05-05 18:52:17 +0000

    Merged PR 4417: Updated MammothFailedEvent.cs
    Updated MammothFailedEvent.cs
    
    Related work items: #34567

--- Icon/Monitoring/Icon.Monitoring/Icon.Monitoring.DataAccess/Model/MammothFailedEvent.cs
+++ Icon/Monitoring/Icon.Monitoring/Icon.Monitoring.DataAccess/Model/MammothFailedEvent.cs
@@ -4,7 +4,7 @@ namespace Icon.Monitoring.DataAccess.Model
 {
     public class MammothFailedEvent
     {
-        public int Identifier { get; set; }
+        public string Identifier { get; set; }
         public int ItemKey { get; set; }
         public int StoreNo { get; set; }
         public DateTime InsertDate { get; set; }
