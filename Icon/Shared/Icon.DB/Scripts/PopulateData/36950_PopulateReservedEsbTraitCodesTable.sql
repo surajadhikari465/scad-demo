11242511dd89521453f3635f73b9325eb990bc60
Shruthi Konudula <2196295@wholefoods.com>
2020-06-12 13:53:26 +0000

    Merged PR 4780: Create and populate ReservedEsbTraitCode Table
    Create and populate ReservedEsbTraitCode Table
    
    Related work items: #36950

---
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/36950_PopulateReservedEsbTraitCodesTable.sql
@@ -0,0 +1,51 @@
+﻿DECLARE @key VARCHAR(128) = '36950_PopulateReservedEsbTraitCodesTable';
+
+IF(Not Exists(SELECT 1 FROM app.PostDeploymentScriptHistory WHERE ScriptKey = @key))
+BEGIN
+
+IF (object_id('tempdb..#reservedEsb') IS NOT NULL)
+	DROP TABLE #reservedEsb;
+
+INSERT INTO [dbo].[ReservedEsbTraitCodes] (
+	[TraitCode]
+	,[Description]
+	,[AttributeGroupId]
+	)
+SELECT a.TraitCode
+	,a.Description
+	,a.AttributeGroupId
+FROM Attributes a
+
+SELECT ta.traitCode
+	,ta.traitDesc
+	,ta.traitGroupID
+INTO #reservedEsb
+FROM trait ta
+
+MERGE dbo.ReservedEsbTraitCodes WITH (
+		UPDLOCK
+		,ROWLOCK
+		) r
+USING #reservedEsb r2
+	ON r2.traitcode = r.Traitcode
+WHEN NOT MATCHED
+	THEN
+		INSERT (
+			[TraitCode]
+			,[Description]
+			,[TraitGroupId]
+			)
+		VALUES (
+			r2.traitcode
+			,r2.traitdesc
+			,r2.traitGroupId
+			);
+
+IF (object_id('tempdb..#reservedEsb') IS NOT NULL)
+	DROP TABLE #reservedEsb;
+END
+ELSE
+BEGIN
+	print '[' + Convert(nvarchar, getdate(), 121) + '] ' + 'Pop-data already applied: ' + @key;
+END
+GO
---
+++ Icon/Shared/Icon.DB/dbo/Tables/ReservedEsbTraitCodes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ReservedEsbTraitCodes] (
+[ReservedTraitCodesId] INT  NOT NULL IDENTITY,
+[TraitCode] NVARCHAR(3)  NOT NULL,
+[Description] NVARCHAR(Max)  NULL,
+[AttributeGroupId] INT NULL,
+[TraitGroupId] INT  NULL,
+CONSTRAINT [PK_ReservedEsbTraitCodes] PRIMARY KEY CLUSTERED ([ReservedTraitCodesId] ASC),
+CONSTRAINT [FK_ReservedEsbTraitCodes_AttributeGroup_AttributeGroupId] FOREIGN KEY ([AttributeGroupId]) REFERENCES [dbo].[AttributeGroup] ([AttributeGroupId]),
+CONSTRAINT [FK_ReservedEsbTraitCodes_TraitGroup_traitGroupID] FOREIGN KEY ([traitGroupID]) REFERENCES [dbo].TraitGroup (traitGroupID)
+)
