ec80349660640521da9e7186268ac354a8908d19
Amit Uppal <2096654@wholefoods.com>
2020-06-23 19:47:28 +0000

    Merged PR 4918: add feature flag table, remove one script from getting auto deployed as its throwing error
    add feature flag, remove one script from getting auto deployed as its throwing error
    
    Related work items: #36947

---
+++ Icon/Shared/Icon.DB/dbo/Tables/FeatureFlag.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[FeatureFlag] (
+	 FeatureFlagId INT NOT NULL IDENTITY
+	,FlagName NVARCHAR(255) NOT NULL
+	,Enabled BIT NOT NULL
+	,Description NVARCHAR(255) NOT NULL
+	,CreatedDate DATETIME NOT NULL
+	,LastModifiedDate DATETIME NULL
+	,LastModifiedBy NVARCHAR(100) NULL
+	,CONSTRAINT [PK_FeatureFlag] PRIMARY KEY CLUSTERED (FeatureFlagId ASC),
+	 CONSTRAINT UC_FeatureFlag_FlagName UNIQUE (FlagName)
+	)
+GO
