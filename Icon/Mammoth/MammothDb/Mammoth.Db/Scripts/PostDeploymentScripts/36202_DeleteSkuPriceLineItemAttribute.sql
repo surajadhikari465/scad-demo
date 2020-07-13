92b5b838f4b5cb7335352c7962cfd53c1b699c7a
Amit Uppal <2096654@wholefoods.com>
2020-07-13 21:50:23 +0000

    Merged PR 5142: 37202 Delete Scripts for SKU and PRiceLine item attributes
    37202 Delete Scripts for SKU and PRiceLine item attributes
    
    Related work items: #37202

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/36202_DeleteSkuPriceLineItemAttribute.sql
@@ -0,0 +1,46 @@
+﻿DECLARE @key VARCHAR(128) = 'DeleteSkuPriceLineItemAttribute';
+
+IF (
+		NOT EXISTS (
+			SELECT 1
+			FROM app.PostDeploymentScriptHistory
+			WHERE ScriptKey = @key
+			)
+		)
+BEGIN
+
+DELETE
+FROM ItemAttributes_Ext
+WHERE attributeid IN (
+		SELECT attributeid
+		FROM Attributes
+		WHERE AttributeCode IN (
+				'Pl'
+				,'pld'
+				,'SKU'
+				)
+		)
+
+DELETE
+FROM Attributes
+WHERE AttributeCode IN (
+		'Pl'
+		,'pld'
+		,'SKU'
+		)
+
+INSERT INTO app.PostDeploymentScriptHistory (
+		ScriptKey
+		,RunTime
+		)
+	VALUES (
+		@key
+		,GETDATE()
+		);
+
+END
+ELSE
+BEGIN
+	PRINT '[' + Convert(NVARCHAR, getdate(), 121) + '] ' + 'Pop-data already applied: ' + @key;
+END
+GO
---
+++ Icon/Shared/Icon.DB/Scripts/PopulateData/36202_DeleteSkuPriceLineItemAttribute.sql
@@ -0,0 +1,80 @@
+﻿DECLARE @key VARCHAR(128) = 'DeleteSkuPriceLineItemAttribute';
+
+IF (
+		NOT EXISTS (
+			SELECT 1
+			FROM app.PostDeploymentScriptHistory
+			WHERE ScriptKey = @key
+			)
+		)
+BEGIN
+
+DECLARE @itemAttributeGroupId INT = (
+		SELECT attributegroupid
+		FROM AttributeGroup
+		WHERE AttributeGroupName = 'Global Item'
+		)
+
+UPDATE Item
+SET itemAttributesJson = JSON_MODIFY(itemAttributesJson, '$.PriceLine', NULL)
+WHERE JSON_VALUE(itemAttributesJson, '$.PriceLine') IS NOT NULL
+
+UPDATE Item
+SET itemAttributesJson = JSON_MODIFY(itemAttributesJson, '$.SKU', NULL)
+WHERE JSON_VALUE(itemAttributesJson, '$.SKU') IS NOT NULL
+
+UPDATE Item
+SET itemAttributesJson = JSON_MODIFY(itemAttributesJson, '$.PriceLineDescription', NULL)
+WHERE JSON_VALUE(itemAttributesJson, '$.PriceLineDescription') IS NOT NULL
+
+DELETE
+FROM ItemColumnDisplayOrder
+WHERE ReferenceId IN (
+		SELECT attributeId
+		FROM Attributes
+		WHERE attributegroupid = @itemAttributeGroupId
+			AND attributename IN (
+				'Sku'
+				,'PriceLine'
+				,'PriceLineDescription'
+				)
+		)
+	AND ColumnType = 'Attribute'
+
+DELETE
+FROM AttributesWebConfiguration
+WHERE attributeId IN (
+		SELECT attributeId
+		FROM Attributes
+		WHERE attributegroupid = @itemAttributeGroupId
+			AND attributename IN (
+				'Sku'
+				,'PriceLine'
+				,'PriceLineDescription'
+				)
+		)
+
+DELETE
+FROM Attributes
+WHERE attributegroupid = @itemAttributeGroupId
+	AND attributename IN (
+		'Sku'
+		,'PriceLine'
+		,'PriceLineDescription'
+		)
+
+INSERT INTO app.PostDeploymentScriptHistory (
+		ScriptKey
+		,RunTime
+		)
+	VALUES (
+		@key
+		,GETDATE()
+		);
+
+END
+ELSE
+BEGIN
+	PRINT '[' + Convert(NVARCHAR, getdate(), 121) + '] ' + 'Pop-data already applied: ' + @key;
+END
+GO
