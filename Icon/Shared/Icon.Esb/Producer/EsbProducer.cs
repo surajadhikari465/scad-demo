e90e4f3358ef1cde3cfafd3fdfa4eeb43715d7c9
Ricardo Venegas <2228714@wholefoods.com>
2020-12-07 18:06:55 +0000

    Merged PR 6914: Add retry to Send Method
    Add retry to Send Method
    
    Related work items: #47054

--- Icon/Shared/Icon.Esb/Producer/EsbProducer.cs
+++ Icon/Shared/Icon.Esb/Producer/EsbProducer.cs
@@ -16,7 +16,6 @@ namespace Icon.Esb.Producer
             lastClientId = "Undefined-" + Guid.NewGuid().ToString("N");
          }
         
-
         public void Send(string message, Dictionary<string, string> messageProperties = null)
         {
             TextMessage textMessage = session.CreateTextMessage(message);
@@ -45,9 +44,11 @@ namespace Icon.Esb.Producer
         public override void OpenConnection(string clientId)
         {
             this.lastClientId = clientId;
-            base.OpenConnection(clientId);
-            producer = session.CreateProducer(destination);
-            producer.DeliveryMode = DeliveryMode.PERSISTENT;
+            Retry<EMSException>(() => { 
+                base.OpenConnection(clientId);
+                producer = session.CreateProducer(destination);
+                producer.DeliveryMode = DeliveryMode.PERSISTENT;
+            });
         }
 
         public override void Dispose()
@@ -74,8 +75,8 @@ namespace Icon.Esb.Producer
             // Verify Connection
             VerifyConnectionAndGracefullyReconnect();
 
-            // Send Message
-            producer.Send(textMessage);
+            // Send Message, Retry on TIBCO.EMS.IllegalStateException 10 times with 30s between tries
+            Retry<IllegalStateException>(() => { producer.Send(textMessage); });
         }
 
         private void Send(BytesMessage byteMessage, Dictionary<string, string> messageProperties = null)
@@ -93,28 +94,39 @@ namespace Icon.Esb.Producer
             // Verify Connection
             VerifyConnectionAndGracefullyReconnect();
 
-            // Send Message
-            producer.Send(byteMessage);
+            // Send Message, Retry on TIBCO.EMS.IllegalStateException 10 times with 30s between tries
+            Retry<IllegalStateException>(() => { producer.Send(byteMessage); });
         }
-
 
         private void VerifyConnectionAndGracefullyReconnect()
         {
-            const int maxRetries = 10;
-            const int timeBetweenRetries = 30000;
+            Retry<Exception>(() => {
+                if (this.IsConnected == false)
+                {
+                    this.OpenConnection(this.lastClientId);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Retry an action maxRetries times, and waits timeBetweenRetries milliseconds between retries.
+        /// </summary>
+        /// <typeparam name="TException">Type of exception to retry.</typeparam>
+        /// <param name="action">Action to execute and retry if needed.</param>
+        /// <param name="maxRetries">Max times to retry: Default 10 times</param>
+        /// <param name="timeBetweenRetries">Time in milliseconds to wait between retries.: Default 30 seconds.</param>
+        private void Retry<TException>(Action action, int maxRetries = 10, int timeBetweenRetries = 30000) where TException: Exception
+        {
             int retryCount = 0;
             bool retry = true;
             while (retry == true)
             {
                 try
                 {
-                    if (base.IsConnected == false)
-                    {
-                        base.OpenConnection(this.lastClientId);
-                    }
+                    action();
                     retry = false;
                 }
-                catch (Exception)
+                catch (TException)
                 {
                     retryCount++;
                     if (retryCount >= maxRetries)
@@ -125,6 +137,5 @@ namespace Icon.Esb.Producer
                 }
             }
         }
-
     }
 }
