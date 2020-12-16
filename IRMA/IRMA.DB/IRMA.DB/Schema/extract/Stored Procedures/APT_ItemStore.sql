76d24e7f535bc6d216693f154a6d6ac80beba662
Robin Eudy <Robin.Eudy@wholefoods.com>
2020-12-16 20:09:40 +0000

    Merged PR 7125: SCM4-1059 refactor itemstore query for performance
    refactor itemstore query for performance
    now 8M+ rows in less than 3 minutes in dev.
    [SCM4-1059](https://jira.wholefoods.com/browse/SCM4-1059)

--- IRMA/IRMA.DB/IRMA.DB/Schema/extract/Stored Procedures/APT_ItemStore.sql
+++ IRMA/IRMA.DB/IRMA.DB/Schema/extract/Stored Procedures/APT_ItemStore.sql
@@ -8,27 +8,38 @@ BEGIN
 	SELECT @region = runmode
 	FROM conversion_runmode
 
+	SELECT store_no
+	INTO #validStores
+	FROM store s
+	WHERE (
+			s.WFM_Store = 1
+			OR s.Mega_Store = 1
+			)
+		AND s.Internal = 1
+		AND s.Manufacturer = 0
+		AND s.Store_No NOT IN (
+			SELECT Key_Value
+			FROM [dbo].[fn_Parse_List]([dbo].[fn_GetAppConfigValue]('LabAndClosedStoreNo', 'IRMA CLIENT'), '|')
+			)
+
+	CREATE NONCLUSTERED INDEX IX_ValidStores ON #validStores (Store_no)
+
 	SELECT @region AS Region
 		,ii.Identifier
 		,s.BusinessUnit_ID
 		,si.Authorized
 		,siv.DiscontinueItem
-		,i.Deleted_Item 
+		,i.Deleted_Item
 	FROM Item i
 	INNER JOIN ItemIdentifier ii ON ii.Item_Key = i.Item_Key
 	INNER JOIN StoreItem si ON si.Item_Key = i.Item_Key
 	INNER JOIN Store s ON s.Store_No = si.Store_No
+	INNER JOIN #validStores vs ON s.store_no = vs.store_no
 	INNER JOIN StoreItemVendor siv ON siv.Item_Key = i.Item_Key
 		AND siv.Store_No = si.Store_No
-	WHERE ii.Remove_Identifier = 0 and ii.Deleted_Identifier = 0
-		AND (
-			s.WFM_Store = 1
-			OR s.Mega_Store = 1
-			)
-		AND s.Internal = 1
+	WHERE ii.Remove_Identifier = 0
+		AND ii.Deleted_Identifier = 0
 		AND siv.PrimaryVendor = 1
-		AND s.Manufacturer = 0
-		AND s.Store_No not in (SELECT Key_Value FROM [dbo].[fn_Parse_List]([dbo].[fn_GetAppConfigValue]('LabAndClosedStoreNo', 'IRMA CLIENT'), '|') ) 
 
 END
 GO
