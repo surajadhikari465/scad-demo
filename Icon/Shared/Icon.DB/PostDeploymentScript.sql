3c36e59c52e0261e4f01c6e33befc9af06b3da8f
Aaron Deane <2230791@wholefoods.com>
2021-01-05 22:19:21 +0000

    Merged PR 7367: Adding coffee subteam scripts to post deployment file
    Adding coffee subteam scripts to post deployment file

--- Icon/Shared/Icon.DB/PostDeploymentScript.sql
+++ Icon/Shared/Icon.DB/PostDeploymentScript.sql
@@ -13,15 +13,15 @@ EXAMPLE (showing Icon-DB scripts path):
 :r .\Scripts\PopulateData\Release\__YOUR__SCRIPT__NAME.sql -- 2018.01.01 PBI 12345
 */
 
-:r .\Scripts\PopulateData\36704_PopulateItemGroupTypes.sql --2020-06-11 36704
-:r .\Scripts\PopulateData\36950_PopulateReservedEsbTraitCodesTable.sql --2020-06-09 36950
-:r .\Scripts\PopulateData\36709_PopulateSkuPriceLineAttributeGroups.sql --2020-06-17 36709
+--:r .\Scripts\PopulateData\36704_PopulateItemGroupTypes.sql --2020-06-11 36704
+--:r .\Scripts\PopulateData\36950_PopulateReservedEsbTraitCodesTable.sql --2020-06-09 36950
+--:r .\Scripts\PopulateData\36709_PopulateSkuPriceLineAttributeGroups.sql --2020-06-17 36709
 -- :r .\Scripts\PopulateData\36709_AddSkuPriceLineAttributes.sql --2020-06-18 36709
 -- Always run this security script and make it the last entry in this script.
 -- :r .\Scripts\PopulateData\36949_AddItemGroups.sql --2020-06-29 36949
-:r .\Scripts\PopulateData\36882_PopulateFeatureFlags_Sku_andPriceline.sql -- 2020-07-08 36882
-:r .\Scripts\PopulateData\40795_Populate_ItemGroup_Keywords.sql -- 2020-07-27 40795
-:r .\Scripts\PopulateData\37766-FixSkuPriceLinePattern.sql -- 2020-08-27 37766
+--:r .\Scripts\PopulateData\36882_PopulateFeatureFlags_Sku_andPriceline.sql -- 2020-07-08 36882
+--:r .\Scripts\PopulateData\40795_Populate_ItemGroup_Keywords.sql -- 2020-07-27 40795
+--:r .\Scripts\PopulateData\37766-FixSkuPriceLinePattern.sql -- 2020-08-27 37766
 :r .\Scripts\PopulateData\PopulateHealthCheck.sql
 :r .\Scripts\PopulateData\SCM2-292_UpdateCoffeeSubteamFinancialHierarchy.sql -- 2020-12-11
 :r .\Security\Icon.Security.sql
