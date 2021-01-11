10e67bf0d25d80945cf6282e0246486134273a5a
Aaron Deane <2230791@wholefoods.com>
2021-01-11 15:35:45 +0000

    Merged PR 7412: [Bug Fix] Updated Coffee SubTeam Script
    updated script and rollback to address issue StoreSubTeam table update and added additional error handling and rollback

--- IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/SCM2-190_MoveCoffeeSubteamToGroceryTeam_rollback.sql
+++ IRMA/IRMA.DB/IRMA.DB/Scripts/PostDeploy/SCM2-190_MoveCoffeeSubteamToGroceryTeam_rollback.sql
@@ -1,19 +1,42 @@
 USE [ItemCatalog]
+SET NOCOUNT ON;
 BEGIN
-	DECLARE @TeamNo int = (SELECT Team_No FROM Team WHERE Team_Name = 'Specialty')
+	DECLARE @TeamNo INT = (
+	  SELECT Team_No
+	  FROM Team
+	  WHERE Team_Abbreviation = 'Specialty'); --Team_No varies based on region but abbreviation is constant
 
-    -- Update statement here to modify Team_No for Coffee Subteam
-	UPDATE Subteam
-	SET Team_No = @TeamNo
-	FROM SubTeam
-	WHERE SubTeam_Name = 'Coffee'
+	DECLARE @SubTeamNo INT = (
+	  SELECT SubTeam_No
+	  FROM SubTeam 
+	  WHERE SubDept_No = 2400)--Coffee Subteam PS
 
-	-- Update statement here to modify Team_No for Coffee Subteam in each store
-	UPDATE sst
-	SET 
-	  sst.Team_No = st.Team_No,
-	  sst.PS_Team_No = 140
-	FROM StoreSubTeam sst
-	INNER JOIN SubTeam st ON sst.SubTeam_No = st.SubTeam_No
-	WHERE st.SubTeam_Name = 'Coffee'
+	BEGIN TRY
+		BEGIN TRANSACTION;
+		-- Update statement here to modify Team_No for Coffee Subteam
+		UPDATE Subteam
+		SET Team_No = @TeamNo
+		FROM SubTeam
+		WHERE SubDept_No = 2400 --Coffee Subteam PS
+
+		-- Update statement here to modify Team_No for Coffee Subteam in each store
+		UPDATE StoreSubTeam
+		SET 
+			Team_No = @TeamNo,
+			PS_Team_No = 140 --Specialty PS_Team_No
+		FROM StoreSubTeam
+		WHERE SubTeam_No = @SubTeamNo AND PS_Team_No = 100 --This condition grabs the Coffee subteams for stores only
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		PRINT 'Error:';
+    SELECT ERROR_NUMBER() AS ErrorNumber
+	  ,ERROR_SEVERITY() AS ErrorSeverity
+	  ,ERROR_STATE() AS ErrorState
+	  ,ERROR_PROCEDURE() AS ErrorProcedure
+	  ,ERROR_LINE() AS ErrorLine
+	  ,ERROR_MESSAGE() AS ErrorMessage;
+    ROLLBACK TRANSACTION;
+  END CATCH
 END
