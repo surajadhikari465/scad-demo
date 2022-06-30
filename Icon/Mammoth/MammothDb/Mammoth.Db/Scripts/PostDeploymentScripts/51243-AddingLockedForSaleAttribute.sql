280a04120efd7e3b5ace5562f84edd7b6e7171a7
Adarsh Pradyut <2312300@wholefoods.com>
2022-06-30 10:43:02 +0000

    Merged PR 12233: Adding LFS in attributes
    * Adding LockedForSale in the attributes list. Needed for adding this to the XML message generated
    
    Related work items: #51243

---
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/51243-AddingLockedForSaleAttribute.sql
@@ -0,0 +1,47 @@
+﻿DECLARE @scriptKey VARCHAR(128) = 'AddingLockedForSaleAttribute';
+
+IF (
+		NOT EXISTS (
+			SELECT *
+			FROM app.PostDeploymentScriptHistory
+			WHERE ScriptKey = @scriptKey
+			)
+		)
+BEGIN
+	PRINT 'running script ' + @scriptKey
+	PRINT '...Adding New Locked For Sale Attribute';
+
+	DECLARE @Today DATETIME;
+
+	SET @Today = GETDATE();
+
+	IF NOT EXISTS (
+			SELECT 1
+			FROM Attributes
+			WHERE AttributeCode = 'LFS'
+			)
+		INSERT INTO Attributes (
+			AttributeGroupID
+			,AttributeCode
+			,AttributeDesc
+			,AddedDate
+			)
+		VALUES (
+			1
+			,'LFS'
+			,'Locked For Sale'
+			,@Today
+			);
+
+	INSERT INTO app.PostDeploymentScriptHistory
+	VALUES (
+		@scriptKey
+		,@Today
+		)
+END
+ELSE
+BEGIN
+	PRINT 'Skipping script ' + @scriptKey
+END
+GO
+
--- Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PostDeploymentScript.sql
+++ Icon/Mammoth/MammothDb/Mammoth.Db/Scripts/PostDeploymentScripts/PostDeploymentScript.sql
@@ -13,7 +13,8 @@ Post-Deployment Script Template
 -- Please add a date-added comment to the end of your line, to help with tracking, maintenance, and archiving.
 
 -- :r .\SCM4-1906-AddTraitCodesForCanadaNutrition.sql --2021-07-23
-:r .\50985_EsbDeprecation_MessageStatus.sql --2022-04-26
+-- :r .\50985_EsbDeprecation_MessageStatus.sql --2022-04-26
+:r .\51243-AddingLockedForSaleAttribute.sql --2022-06-29
 
 -- Run every time (add your script above; keep this at bottom, as last post-deploy script).
 :r ..\..\Security\SecurityGrants.sql
