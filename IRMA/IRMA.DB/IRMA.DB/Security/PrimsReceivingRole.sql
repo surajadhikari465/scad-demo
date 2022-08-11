07c4188db433801396fb960027a6ca2aabdba64e
Tom Lux <7005541@wholefoods.com>
2022-08-11 13:39:39 +0000

    Merged PR 12422: Create PrimsReceivingRole; grant to 4 procs; add prims-receipts SQL user to role
    Create PrimsReceivingRole; grant to 4 procs; add prims-receipts SQL user to role

---
+++ IRMA/IRMA.DB/IRMA.DB/Security/PrimsReceivingRole.sql
@@ -0,0 +1,23 @@
+create role [PrimsReceivingRole]
+	authorization [dbo]
+go
+
+---------------------------------------------------------
+---------------------------------------------------------
+
+-- Existing, separate permission; adding to new role.
+grant exec on [dbo].[ReceiveOrderItem3] to [PrimsReceivingRole]
+
+-- New perms to auto-close POs.
+grant exec on [dbo].[UpdateOrderClosed] to [PrimsReceivingRole]
+grant exec on [dbo].[AutomaticOrderOriginUpdate] to [PrimsReceivingRole]
+go
+
+-- New perm August, 2022.
+grant exec on [dbo].[UpdateOrderHeaderRefuseReceivingAndClose] to [PrimsReceivingRole]
+
+---------------------------------------------------------
+---------------------------------------------------------
+
+ALTER ROLE [PrimsReceivingRole] ADD MEMBER [prims-receipts]
+go
