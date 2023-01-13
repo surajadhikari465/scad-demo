c066ec19339f855fee0dbc0e931d572be32b071d
Adarsh Pradyut <2312300@wholefoods.com>
2023-01-13 17:09:45 +0000

    Merged PR 13292: Fixing ExternalPurchaseOrderReferences namespace
    * Fixing ExternalPurchaseOrderReferences namespace
    
    Related work items: #51282

--- Icon/ESB/Schemas/Icon.Esb.Schemas.Wfm/WfmContracts.cs
+++ Icon/ESB/Schemas/Icon.Esb.Schemas.Wfm/WfmContracts.cs
@@ -15804,15 +15804,28 @@ namespace Icon.Esb.Schemas.Wfm.Contracts
         public ExternalPurchaseOrderReferences externalPurchaseOrderReferences { get; set; }
     }
 
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("MSBuild", "15.8.168+ga8fba1ebd7")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://schemas.wfm.com/Enterprise/InventoryMgmt/CommonRefTypes/V1")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.wfm.com/Enterprise/InventoryMgmt/CommonRefTypes/V1", IsNullable = true)]
     public partial class ExternalPurchaseOrderReference
     {
         public string externalPurchaseOrderNumber { get; set; }
         public string externalSource { get; set; }
     }
 
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("MSBuild", "15.8.168+ga8fba1ebd7")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://schemas.wfm.com/Enterprise/InventoryMgmt/CommonRefTypes/V1")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.wfm.com/Enterprise/InventoryMgmt/CommonRefTypes/V1", IsNullable = true)]
     public partial class ExternalPurchaseOrderReferences
     {
-        public ExternalPurchaseOrderReference externalPurchaseOrderReference { get; set; }
+        [System.Xml.Serialization.XmlArrayItemAttribute("externalPurchaseOrderReference", Namespace = "http://schemas.wfm.com/Enterprise/InventoryMgmt/CommonRefTypes/V1", IsNullable=true)]
+        public ExternalPurchaseOrderReference[] externalPurchaseOrderReference { get; set; }
     }
 
     /// <remarks/>
