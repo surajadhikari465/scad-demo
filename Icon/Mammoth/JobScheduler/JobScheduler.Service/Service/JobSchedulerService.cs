86255204d7a8a47705fdd0a2a22417b57898e8ba
Chitrarath Kumar <2313425@wholefoods.com>
2023-01-20 08:24:27 +0000

    Merged PR 13327: Prevent concurrent runs of JobScheduler
    Prevent concurrent runs of JobScheduler.
    
    As of now, a scheduled job is getting triggered multiple times, because multiple instances of this service gets triggered within milliseconds.
    
    Introduced a delayed start between parallel instances to prevent this.
    
    Next step:
    Configure iterative instance number in release pipeline.
    
    Related work items: #51286

--- Icon/Mammoth/JobScheduler/JobScheduler.Service/Service/JobSchedulerService.cs
+++ Icon/Mammoth/JobScheduler/JobScheduler.Service/Service/JobSchedulerService.cs
@@ -24,7 +24,16 @@ namespace JobScheduler.Service.Service
 
         public void Start()
         {
-            logger.Info("Starting JobScheduler service.");
+            // Introducing delay so that multiple instances don't process the data at the same time.
+            // InstanceNumber iteration starts with 1 in pipeline.
+            int startDelayInSeconds = (jobSchedulerServiceSettings.InstanceNumber - 1) * jobSchedulerServiceSettings.DelayOffsetInSeconds;
+            if (startDelayInSeconds < 0)
+            {
+                logger.Warn($"Invalid value computed for startDelayInSeconds - '{startDelayInSeconds}'. Setting to 0.");
+                startDelayInSeconds = 0;
+            }
+            logger.Info($"Starting JobScheduler service in {startDelayInSeconds} seconds.");
+            Thread.Sleep(startDelayInSeconds * 1000);
             timer.Elapsed += RunService;
             timer.Start();
             logger.Info("Started JobScheduler service.");
--- Icon/Mammoth/JobScheduler/JobScheduler.Service/Settings/JobSchedulerServiceSettings.cs
+++ Icon/Mammoth/JobScheduler/JobScheduler.Service/Settings/JobSchedulerServiceSettings.cs
@@ -7,8 +7,8 @@ namespace JobScheduler.Service.Settings
         public int TimerProcessRunIntervalInMilliseconds { get; set; }
         public int SendMessageRetryCount { get; set; }
         public int SendMessageRetryDelayInMilliseconds { get; set; }
-        public string AwsAccessKey { get; set; }
-        public string AwsSecretKey { get; set; }
+        public int InstanceNumber { get; set; }
+        public int DelayOffsetInSeconds { get; set; }
         public string AwsAccountId { get; set; }
 
         public static JobSchedulerServiceSettings CreateSettings()
@@ -18,8 +18,8 @@ namespace JobScheduler.Service.Settings
                 TimerProcessRunIntervalInMilliseconds = AppSettingsAccessor.GetIntSetting("TimerProcessRunIntervalInMilliseconds", 300000),
                 SendMessageRetryCount = AppSettingsAccessor.GetIntSetting("SendMessageRetryCount", 3),
                 SendMessageRetryDelayInMilliseconds = AppSettingsAccessor.GetIntSetting("SendMessageRetryDelayInMilliseconds", 3000),
-                AwsAccessKey = AppSettingsAccessor.GetStringSetting("AwsAccessKey"),
-                AwsSecretKey = AppSettingsAccessor.GetStringSetting("AwsSecretKey"),
+                InstanceNumber = AppSettingsAccessor.GetIntSetting("InstanceNumber"),
+                DelayOffsetInSeconds = AppSettingsAccessor.GetIntSetting("DelayOffsetInSeconds", 120),
                 AwsAccountId = AppSettingsAccessor.GetStringSetting("AwsAccountId")
             };
         }
