0532fea170a116210dc4645ece5fefd48805d74e
Prashant Kumar <2398017@wholefoods.com>
2023-02-09 11:35:49 +0000

    Merged PR 13427: Config and Injector additions
    - Add service configuration
    - Add db connection string for each stage
    
    Todo -
    - Verify Opsgenie alert being successful
    - Make "SendErrorAlerts" value="false" for all stages except prod

--- Icon/Mammoth/ErrorMessagesMonitor/ErrorMessagesMonitor/SimpleInjectorInitializer.cs
+++ Icon/Mammoth/ErrorMessagesMonitor/ErrorMessagesMonitor/SimpleInjectorInitializer.cs
@@ -1,8 +1,11 @@
-﻿using ErrorMessagesMonitor.Message.Processor;
+﻿using ErrorMessagesMonitor.DataAccess;
+using ErrorMessagesMonitor.Message.Processor;
 using ErrorMessagesMonitor.Service;
 using ErrorMessagesMonitor.Settings;
 using Icon.Common.Email;
+using Icon.DbContextFactory;
 using Icon.Logging;
+using Mammoth.Framework;
 using OpsgenieAlert;
 using Container = SimpleInjector.Container;
 
@@ -16,7 +19,11 @@ namespace ErrorMessagesMonitor
             var serviceSettings = ErrorMessagesMonitorServiceSettings.CreateSettingsFromConfig();
 
             container.RegisterSingleton<ILogger<ErrorMessagesMonitorService>, NLogLogger<ErrorMessagesMonitorService>>();
+            container.RegisterSingleton<IErrorMessagesProcessor, ErrorMessagesProcessor>();
             container.RegisterSingleton<IErrorMessagesMonitorService, ErrorMessagesMonitorService>();
+            container.RegisterSingleton<IErrorMessagesMonitorDAL, ErrorMessagesMonitorDAL>();
+            container.RegisterSingleton<IDbContextFactory<MammothContext>, MammothContextFactory>();
+            container.RegisterSingleton(() => serviceSettings);
             container.RegisterSingleton<IEmailClient>(() => { return EmailClient.CreateFromConfig(); });
             container.RegisterSingleton<IOpsgenieAlert, OpsgenieAlert.OpsgenieAlert>();
             container.RegisterSingleton<ILogger<ErrorMessagesProcessor>, NLogLogger<ErrorMessagesProcessor>>();
